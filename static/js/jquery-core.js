0f2cbb4a9bf8368aaec4279d15ff31aa1c8ce0cb
bluelovers <codelovers@users.sourceforge.net>
2011-06-18 03:28:34 +0800

    調整首頁版面 並且可輪播 圖片

--- static/js/jquery-core.js
+++ static/js/jquery-core.js
@@ -28,12 +28,42 @@ jQuery(function($){
 		}).find('table:first tr').hide().first().fadeIn(400).delay(500, function (){
 			$('#contents').scrollTop($('#contents table').first().height());
 
-			_func = function() {
+			var _func = function() {
 				$(this).next().fadeIn(400).delay(500, _func);
 				$('#contents').scrollTop($('#contents table').first().height());
 			}
 
 			$(this).next().fadeIn(400).delay(500, _func);
+		});
+	}
+
+	// 以首頁的登入表單作為判定
+	if ($('#contents form :submit[name="Login"]').length > 0) {
+		$('#contents > div').width('100%')
+			.children('div:eq(0)').width(270).css({
+				'overflow-x' : 'hidden'
+			})
+			.next('div').width(480)
+				.children('div:eq(0)').width('100%')
+				/*
+					.children('img').css({
+						'margin-left' : (Math.round(Math.random() * 1) ? 0 : -500),
+						'margin-top' : -1 - Math.round(Math.random() * 4) * (200 + 1)
+					})
+				*/
+			;
+
+		$('#contents > div > div:eq(1) div img').css('margin-left', function(){
+			var _this = $(this);
+			var _func = function(){
+				_this.css({
+					'margin-left' : (Math.round(Math.random() * 1) ? 0 : -500),
+					'margin-top' : -1 - Math.round(Math.random() * 4) * (200 + 1)
+				});
+			};
+
+			setInterval(_func, 5000);
+			_func();
 		});
 	}
 
