88e97cf081f2103e0493ddb8793fab843c4a91d3
bluelovers <codelovers@users.sourceforge.net>
2011-08-10 16:38:57 +0800

    $this->members->Set_ID_PASS(); - part.2

--- source/class/game_members.class.php
+++ source/class/game_members.class.php
@@ -15,6 +15,32 @@ class game_members {
 	function game_members($main) {
 		$this->main = &$main;
 	}
+
+	/**
+	 * pass と id を設定する
+	 */
+	function Set_ID_PASS() {
+		$id	= ($_POST["id"])?$_POST["id"]:$_GET["id"];
+		//if($_POST["id"]) {
+		if($id) {
+				$this->main->id	= $id;//$_POST["id"];
+			// ↓ログイン処理した時だけ
+			if (is_registered($_POST["id"])) {
+				$_SESSION["id"]	= $this->main->id;
+			}
+		} else if($_SESSION["id"])
+			$this->main->id	= $_SESSION["id"];
+
+		$pass	= ($_POST["pass"])?$_POST["pass"]:$_GET["pass"];
+		//if($_POST["pass"])
+		if($pass)
+			$this->main->pass	= $pass;//$_POST["pass"];
+		else if($_SESSION["pass"])
+			$this->main->pass	= $_SESSION["pass"];
+
+		if($this->main->pass)
+			$this->main->pass	= $this->main->CryptPassword($this->pass);
+	}
 }
 
 ?>
