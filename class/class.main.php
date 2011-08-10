ac74357d22f38d17d4357e4fdcfcf59f9c7d8c26
bluelovers <codelovers@users.sourceforge.net>
2011-08-10 21:10:21 +0800

    function SettingShow() - 設定表示画面

--- class/class.main.php
+++ class/class.main.php
@@ -87,7 +87,7 @@ class main extends user {
 						$this->SaveData();
 
 					$this->fpCloseAll();
-					$this->SettingShow();
+					$this->member->SettingShow();
 					return 0;
 
 				// オークション
@@ -3009,53 +3009,7 @@ JS_HTML;
 			return true;
 		}
 	}
-//////////////////////////////////////////////////
-//	設定表示画面
-	function SettingShow() {
-		print('<div style="margin:15px">'."\n");
-		if($this->record_btl_log) $record_btl_log	= " checked";
-		if($this->no_JS_itemlist) $no_JS_itemlist	= " checked";
-		?>
-<h4>Setting</h4>
-<form action="?setting" method="post">
-<table><tbody>
-<tr><td><input type="checkbox" name="record_battle_log" value="1" <?=$record_btl_log?>></td><td>戦闘ログの記録</td></tr>
-<tr><td><input type="checkbox" name="no_JS_itemlist" value="1" <?=$no_JS_itemlist?>></td><td>アイテムリストにJavaScriptを使わない</td></tr>
-</tbody></table>
-<!--<tr><td>None</td><td><input type="checkbox" name="none" value="1"></td></tr>-->
-Color : <?php
-		$color	= file(COLOR_FILE);
-		print('<select name="color" class="bgcolor">'."\n");
-		foreach($color as $value) {
-			$value	= trim($value);
-			print("<option value=\"{$value}\" style=\"color:{$value}\" ".($this->UserColor == $value?" selected":"").">");
-			print("SampleColor</option>\n");
-		}
-		print('</select>');
-	?><br />
-<input type="submit" class="btn" name="setting01" value="modify" style="width:100px">
-<input type="hidden" name="setting01" value="1">
-</form>
-<h4>Logout</h4>
-<form action="<?=INDEX?>" method="post">
-<input type="submit" class="btn" name="logout" value="logout" style="width:100px">
-</form>
-<h4>チーム名の変更</h4>
-<form action="?setting" method="post">
-費用 : <?=MoneyFormat(NEW_NAME_COST)?><br />
-16文字まで(全角=2文字)<br />
-新しい名前 : <input type="text" class="text" name="NewName" size="20">
-<input type="submit" class="btn" value="change" style="width:100px">
-</form>
-<h4>脱出口</h4>
-<div class="u">※データの削除</div>
-<form action="?setting" method="post">
-PassWord : <input type="text" class="text" name="deletepass" size="20">
-<input type="submit" class="btn" name="delete" value="delete" style="width:100px">
-</form>
-</div><?php
-		return $Result;
-	}
+
 ////////// Show //////////////////////////////////////////////////////
 /*
  * ShowCharStat
--- source/class/game_member.class.php
+++ source/class/game_member.class.php
@@ -430,6 +430,55 @@ print("</div>\n");
 				return "ID \"{$this->id}\" doesnt exists.";
 		}
 	}
+
+	/**
+	 * 設定表示画面
+	 */
+	function SettingShow() {
+		print('<div style="margin:15px">'."\n");
+		if($this->main->record_btl_log) $record_btl_log	= " checked";
+		if($this->main->no_JS_itemlist) $no_JS_itemlist	= " checked";
+		?>
+<h4>Setting</h4>
+<form action="?setting" method="post">
+<table><tbody>
+<tr><td><input type="checkbox" name="record_battle_log" value="1" <?=$record_btl_log?>></td><td>戦闘ログの記録</td></tr>
+<tr><td><input type="checkbox" name="no_JS_itemlist" value="1" <?=$no_JS_itemlist?>></td><td>アイテムリストにJavaScriptを使わない</td></tr>
+</tbody></table>
+<!--<tr><td>None</td><td><input type="checkbox" name="none" value="1"></td></tr>-->
+Color : <?php
+		$color	= file(COLOR_FILE);
+		print('<select name="color" class="bgcolor">'."\n");
+		foreach($color as $value) {
+			$value	= trim($value);
+			print("<option value=\"{$value}\" style=\"color:{$value}\" ".($this->main->UserColor == $value?" selected":"").">");
+			print("SampleColor</option>\n");
+		}
+		print('</select>');
+	?><br />
+<input type="submit" class="btn" name="setting01" value="modify" style="width:100px">
+<input type="hidden" name="setting01" value="1">
+</form>
+<h4>Logout</h4>
+<form action="<?=INDEX?>" method="post">
+<input type="submit" class="btn" name="logout" value="logout" style="width:100px">
+</form>
+<h4>チーム名の変更</h4>
+<form action="?setting" method="post">
+費用 : <?=MoneyFormat(NEW_NAME_COST)?><br />
+16文字まで(全角=2文字)<br />
+新しい名前 : <input type="text" class="text" name="NewName" size="20">
+<input type="submit" class="btn" value="change" style="width:100px">
+</form>
+<h4>脱出口</h4>
+<div class="u">※データの削除</div>
+<form action="?setting" method="post">
+PassWord : <input type="text" class="text" name="deletepass" size="20">
+<input type="submit" class="btn" name="delete" value="delete" style="width:100px">
+</form>
+</div><?php
+		return $Result;
+	}
 }
 
 ?>
