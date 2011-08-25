2006aea21bc2dcd0b9bb6bd87eaae676ea3e2df4
bluelovers <codelovers@users.sourceforge.net>
2011-08-25 17:23:32 +0800

    move function EnemyNumber($party) - part.2

--- class/class.main.php
+++ class/class.main.php
@@ -366,19 +366,6 @@ class main extends user {
 		}
 	}
 
-	/**
-	 * 敵の数を返す	数～数+2(max:5)
-	 */
-	function EnemyNumber($party) {
-		$min	= count($party);//プレイヤーのPT数
-		if($min == 5)//5人なら5匹
-			return 5;
-		$max	= $min + ENEMY_INCREASE;// つまり、+2なら[1人:1～3匹] [2人:2～4匹] [3:3-5] [4:4-5] [5:5]
-		if($max>5)
-			$max	= 5;
-		mt_srand();
-		return mt_rand($min,$max);
-	}
 //////////////////////////////////////////////////
 //	出現する確率から敵を選んで返す
 	function SelectMonster($monster) {
@@ -1343,7 +1330,7 @@ HTML;
 			include(DATA_LAND);
 			include(DATA_MONSTER);
 			list($Land,$MonsterList)	= LandInformation($_GET["common"]);
-			$EneNum	= $this->EnemyNumber($MyParty);
+			$EneNum	= $this->party->EnemyNumber($MyParty);
 			$EnemyParty	= $this->EnemyParty($EneNum,$MonsterList);
 
 			$this->WasteTime(NORMAL_BATTLE_TIME);//時間の消費
