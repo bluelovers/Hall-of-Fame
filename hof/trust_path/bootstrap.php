35b4ff58223c491a2e8c505d156aa85cd8368a2b
bluelovers <codelovers@users.sourceforge.net>
2012-05-04 00:26:39 +0800

    .
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/bootstrap.php

--- hof/trust_path/bootstrap.php
+++ hof/trust_path/bootstrap.php
@@ -18,9 +18,7 @@ if (file_exists(dirname(__file__) . '/bootstrap.options.php'))
 if (file_exists(dirname(__file__) . '/config/setting.php'))
 {
 	require dirname(__file__) . '/config/setting.php';
-	require dirname(__file__) . '/config/setting.dist.php';
 }
-else
-{
-	require dirname(__file__) . '/config/setting.dist.php';
-}
+
+require dirname(__file__) . '/config/setting.dist.php';
+
