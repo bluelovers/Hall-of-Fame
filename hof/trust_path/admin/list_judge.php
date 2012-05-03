548e475f9609b23e618082221c4458f86bcc7734
bluelovers <codelovers@users.sourceforge.net>
2012-05-04 01:47:33 +0800

    format admin (Step: 18)
    Step: 18
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/admin/list_judge.php

--- hof/trust_path/admin/list_judge.php
+++ hof/trust_path/admin/list_judge.php
@@ -6,22 +6,27 @@
 </head>
 <body>
 <?php
-include("../data/data.judge_setup.php");
-for($i=1000; $i<9999; $i++) {
-	$j	= LoadJudgeData($i);
-	if($j) {
-		print("case {$i}:// {$j[exp]}<br />");
-		$list[]	= $i;
+
+include ("../data/data.judge_setup.php");
+for ($i = 1000; $i < 9999; $i++)
+{
+	$j = LoadJudgeData($i);
+	if ($j)
+	{
+		print ("case {$i}:// {$j[exp]}<br />");
+		$list[] = $i;
 	}
 }
-print("array(<br />\n");
-foreach($list as $var) {
+print ("array(<br />\n");
+foreach ($list as $var)
+{
 	$A++;
-	print("$var, ");
-	if($A%5==0)
-		print("<br />\n");
+	print ("$var, ");
+	if ($A % 5 == 0) print ("<br />\n");
 }
-print("<br />\n);");
+print ("<br />\n);");
+
+
 ?>
 </body>
 </html>
