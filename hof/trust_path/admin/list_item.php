c9e6b58bba03c931374c0c511b6d9532b4281728
bluelovers <codelovers@users.sourceforge.net>
2012-05-04 01:47:31 +0800

    format admin (Step: 16)
    Step: 16
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/admin/list_item.php

--- hof/trust_path/admin/list_item.php
+++ hof/trust_path/admin/list_item.php
@@ -30,10 +30,11 @@ td{
 </style></head>
 <body>
 <?php
-include("../data/data.item.php");
-print("<table cellspacing=\"1\"><tbody>");
-$img_f	= "../image/icon/";
-$des	= '<tr><td class="a">no</td>
+
+include ("../data/data.item.php");
+print ("<table cellspacing=\"1\"><tbody>");
+$img_f = "../image/icon/";
+$des = '<tr><td class="a">no</td>
 <td class="a">img</td>
 <td class="a">name</td>
 <td class="a">type</td>
@@ -42,46 +43,50 @@ $des	= '<tr><td class="a">no</td>
 <td class="a">handle</td>
 <td class="a">buy</td>
 <td class="a">sell</td></tr>';
-$count=0;
-for($i=1000; $i<10000; $i++) {
-	$item	= LoadItemData($i);
-	if(!$item) continue;
+$count = 0;
+for ($i = 1000; $i < 10000; $i++)
+{
+	$item = LoadItemData($i);
+	if (!$item) continue;
 
-	if($count%6==0)
-		print($des);
+	if ($count % 6 == 0) print ($des);
 	$count++;
 
-	print("<tr><td>\n");
-	print($i);
-	print("</td><td>");
-	print("<img src=\"".$img_f.$item[img]."\">");
-	print("</td><td>\n");
-	print($item[name]);
-	print("</td><td>\n");
-	print($item[type]);
-	print("</td><td>\n");
-	print($item[atk][0]."<br />".$item[atk][1]);
-	print("</td><td>\n");
-	print($item[def][0]."+".$item[def][1]."<br />".$item[def][2]."+".$item[def][3]);
-	print("</td><td>\n");
-	print($item[handle]);
-	print("</td><td>\n");
-	print($item[buy]);
-	print("</td><td>\n");
-	print($item[sell]);
-	print("</td></tr>\n");
-	if($item["need"]) {
-		print("<tr><td colspan=\"9\" style=\"text-align:left;padding-left:50px\">\n");
-		foreach($item["need"] as $M_item => $M_amount) {
-			$M	= LoadItemData($M_item);
-			print("$M[name]");
-			print("<img src=\"".$img_f.$M[img]."\">");
-			print("x".$M_amount." / \n");
+	print ("<tr><td>\n");
+	print ($i);
+	print ("</td><td>");
+	print ("<img src=\"" . $img_f . $item[img] . "\">");
+	print ("</td><td>\n");
+	print ($item[name]);
+	print ("</td><td>\n");
+	print ($item[type]);
+	print ("</td><td>\n");
+	print ($item[atk][0] . "<br />" . $item[atk][1]);
+	print ("</td><td>\n");
+	print ($item[def][0] . "+" . $item[def][1] . "<br />" . $item[def][2] . "+" . $item[def][3]);
+	print ("</td><td>\n");
+	print ($item[handle]);
+	print ("</td><td>\n");
+	print ($item[buy]);
+	print ("</td><td>\n");
+	print ($item[sell]);
+	print ("</td></tr>\n");
+	if ($item["need"])
+	{
+		print ("<tr><td colspan=\"9\" style=\"text-align:left;padding-left:50px\">\n");
+		foreach ($item["need"] as $M_item => $M_amount)
+		{
+			$M = LoadItemData($M_item);
+			print ("$M[name]");
+			print ("<img src=\"" . $img_f . $M[img] . "\">");
+			print ("x" . $M_amount . " / \n");
 		}
-		print("</td></tr>\n");
+		print ("</td></tr>\n");
 	}
 }
-print("</tbody></table>");
+print ("</tbody></table>");
+
+
 ?>
 </body>
 </html>
