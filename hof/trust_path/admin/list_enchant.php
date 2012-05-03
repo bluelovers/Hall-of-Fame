84e08d1176797a42974ce7e56ae05a0727c51021
bluelovers <codelovers@users.sourceforge.net>
2012-05-04 01:47:30 +0800

    format admin (Step: 15)
    Step: 15
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/admin/list_enchant.php

--- hof/trust_path/admin/list_enchant.php
+++ hof/trust_path/admin/list_enchant.php
@@ -1,27 +1,33 @@
 <?php
-include("../data/data.create.php");
-include("../data/data.enchant.php");
 
-list($low,$high)	= ItemAbilityPossibility("Sword");
-print("---------------LOW<br />\n");
-foreach($low as $enchant) {
-	$item	= array();
-	AddEnchantData($item,$enchant);
-	print('<span style="width:10em;text-align:right">'.$enchant.'</span>:'.$item["option"]."<br />\n");
+include ("../data/data.create.php");
+include ("../data/data.enchant.php");
+
+list($low, $high) = ItemAbilityPossibility("Sword");
+print ("---------------LOW<br />\n");
+foreach ($low as $enchant)
+{
+	$item = array();
+	AddEnchantData($item, $enchant);
+	print ('<span style="width:10em;text-align:right">' . $enchant . '</span>:' . $item["option"] . "<br />\n");
 }
-print("---------------HIGH<br />\n");
-foreach($high as $enchant) {
-	$item	= array();
-	AddEnchantData($item,$enchant);
-	print('<span style="width:10em;text-align:right">'.$enchant.'</span>:'.$item["option"]."<br />\n");
+print ("---------------HIGH<br />\n");
+foreach ($high as $enchant)
+{
+	$item = array();
+	AddEnchantData($item, $enchant);
+	print ('<span style="width:10em;text-align:right">' . $enchant . '</span>:' . $item["option"] . "<br />\n");
 }
 
 //dump($low);
 //dump($high);
 
-function dump($var){
-	print("<pre>\n");
+function dump($var)
+{
+	print ("<pre>\n");
 	var_dump($var);
-	print("\n</pre>\n");
+	print ("\n</pre>\n");
 }
+
+
 ?>
