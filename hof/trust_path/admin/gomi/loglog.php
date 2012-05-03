ce153d08844c602f229389856448d661a25b12e4
bluelovers <codelovers@users.sourceforge.net>
2012-05-04 01:47:29 +0800

    format admin (Step: 13)
    Step: 13
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/admin/gomi/loglog.php

--- hof/trust_path/admin/gomi/loglog.php
+++ hof/trust_path/admin/gomi/loglog.php
@@ -6,23 +6,29 @@
 </head>
 <body>
 <?php
-include("../trust_path/bootstrap.php");
-$admin	= "../";// index.php からの距離
-$users	= glob($admin.USER."/*");
-print("Users : ".count($users)." / ".MAX_USERS."<br />\n");
 
-print("--- ManageLog<br />\n");
-foreach(file($admin.MANAGE_LOG_FILE) as $string) {
-	print($string."<br />\n");
+include ("../trust_path/bootstrap.php");
+$admin = "../"; // index.php からの距離
+$users = glob($admin . USER . "/*");
+print ("Users : " . count($users) . " / " . MAX_USERS . "<br />\n");
+
+print ("--- ManageLog<br />\n");
+foreach (file($admin . MANAGE_LOG_FILE) as $string)
+{
+	print ($string . "<br />\n");
 }
-print("--- BBS01<br />\n");
-foreach(file($admin.BBS01) as $string) {
-	print($string."<br />\n");
+print ("--- BBS01<br />\n");
+foreach (file($admin . BBS01) as $string)
+{
+	print ($string . "<br />\n");
 }
-print("--- TownBBS<br />\n");
-foreach(file($admin.BBS02) as $string) {
-	print($string."<br />\n");
+print ("--- TownBBS<br />\n");
+foreach (file($admin . BBS02) as $string)
+{
+	print ($string . "<br />\n");
 }
+
+
 ?>
 </body>
 </html>
