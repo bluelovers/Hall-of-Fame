27f33dfdee135a0407ba79fe0556f11d8c4b6754
bluelovers <codelovers@users.sourceforge.net>
2012-05-04 01:47:19 +0800

    format admin (Step: 2)
    Step: 2
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/admin/gomi/graph.dmg.php

--- hof/trust_path/admin/gomi/graph.dmg.php
+++ hof/trust_path/admin/gomi/graph.dmg.php
@@ -1,10 +1,12 @@
 <table><tbody>
 <?php
-for($i=0; $i<5000; $i++) {
-	print("<tr><td>");
+
+for ($i = 0; $i < 5000; $i++)
+{
+	print ("<tr><td>");
 	//$no	= $i-(($i*$i)/1000);
 	//$no	= ceil(pow($i,9/17)*10);
-	$no	= sqrt($i)*10;
+	$no = sqrt($i) * 10;
 	//if($i < 100)
 	//	$no -= ceil($no * $i/100);
 	//if($i < 100)
@@ -14,11 +16,13 @@ for($i=0; $i<5000; $i++) {
 	//$no	= $i - $i * ((sqrt($i)*sqrt($i))/($i*$i));
 	//$no	= $i - $i*(sqrt($i*2)/100);
 	//$no	= pow($i,2) - $i*sqrt($i);
-	echo $i.":".$no."<br>";
-	print("</td><td>");
-	$wid=round($no);
-	print("<img src=\"./bar.gif\" width=\"$no\" height=\"7\"");
-	print("</td></tr>");
+	echo $i . ":" . $no . "<br>";
+	print ("</td><td>");
+	$wid = round($no);
+	print ("<img src=\"./bar.gif\" width=\"$no\" height=\"7\"");
+	print ("</td></tr>");
 }
+
+
 ?>
 </tbody></table>
