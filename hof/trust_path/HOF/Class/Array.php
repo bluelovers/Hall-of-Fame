3a98a068580377027c8e59f83c549d5334e3eaaa
bluelovers <codelovers@users.sourceforge.net>
2012-05-04 01:50:58 +0800

    . (Step: 2)
    Step: 2
    
    Commit ChangedLog:
    
    [A ] new file:
    
    	hof/trust_path/HOF/Class/Array.php

---
+++ hof/trust_path/HOF/Class/Array.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * @author bluelovers
+ * @copyright 2012
+ */
+
+/**
+ *
+ * @example
+ * $a = new Dura_Class_Array();
+
+ * $a->a = 1;
+ * $a['b'] = 2;
+
+ * echo '<pre>';
+ * var_dump($a);
+
+ * var_dump(array(
+ * $a['a'],
+ * $a->b,
+ * (ArrayObject::STD_PROP_LIST | ArrayObject::ARRAY_AS_PROPS),
+ * ));
+ */
+class HOF_Class_Array extends ArrayObject
+{
+
+	/**
+	 * Dura_Class_Array::ARRAY_PROP_BOTH = (ArrayObject::STD_PROP_LIST | ArrayObject::ARRAY_AS_PROPS);
+	 */
+	const ARRAY_PROP_BOTH = 3;
+
+	protected $_data_default_ = array();
+
+	function __construct($input = null)
+	{
+		if ($input === null) $input = $this->_data_default_;
+
+		$this->setFlags(ArrayObject::STD_PROP_LIST | ArrayObject::ARRAY_AS_PROPS);
+		$this->exchangeArray($input);
+	}
+
+	function toArray()
+	{
+		return $this->getArrayCopy();
+	}
+
+}
