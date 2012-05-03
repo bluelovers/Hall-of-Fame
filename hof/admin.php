f7b8c3379a5a253cc55724ece5d9168ff7ddf457
bluelovers <codelovers@users.sourceforge.net>
2012-05-03 23:28:00 +0800

    format (Step: 1)
    Step: 1
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/admin.php

--- hof/admin.php
+++ hof/admin.php
@@ -1,18 +1,22 @@
 <?php
-/*
-* このファイル名は変更してもOK
-* パスワードは変更してください。
-*
-* 管理しないなら
-* admin.php と adminフォルダを
-* サーバーから消してもOK
-*
-*/
-	include("trust_path/bootstrap.php");
-	define("ADMIN_DIR","./admin/");//管理用のファイル場所
-	define("ADMIN_PASSWORD","password");//パスワード
 
-	include_once CLASS_DIR.'class.core.php';
+/**
+ * このファイル名は変更してもOK
+ * パスワードは変更してください。
+ *
+ * 管理しないなら
+ * admin.php と adminフォルダを
+ * サーバーから消してもOK
+ *
+ */
 
-	include(ADMIN_DIR."admin.php");
+include ("trust_path/bootstrap.php");
+define("ADMIN_DIR", "./admin/"); //管理用のファイル場所
+define("ADMIN_PASSWORD", "password"); //パスワード
+
+include_once CLASS_DIR . 'class.core.php';
+
+include (ADMIN_DIR . "admin.php");
+
+
 ?>
