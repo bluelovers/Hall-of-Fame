303a168d3379c6aa1fd33169eb603dfdea092aa2
bluelovers <codelovers@users.sourceforge.net>
2012-05-04 01:47:26 +0800

    format admin (Step: 10)
    Step: 10
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/admin/gomi/list_skill.php

--- hof/trust_path/admin/gomi/list_skill.php
+++ hof/trust_path/admin/gomi/list_skill.php
@@ -1,23 +1,29 @@
 
 <?php
-include("./data.skill.php");
+
+include ("./data.skill.php");
 
 //print("<table><tbody>");
 
-for($no=1000; $no<10000; $no++) {
+for ($no = 1000; $no < 10000; $no++)
+{
 	$skill = LoadSkillData($no);
-	if(!$skill) continue;
+	if (!$skill) continue;
 
-//	print("<tr><td>");
-	print("case $no: // ".$skill[name]."<br>");
-//	print("// target=({$skill[target][0]},{$skill[target][1]},{$skill[target][2]})<br>");
-//	print("// pow={$skill[pow]}<br>");
-//	print("break;<br>");
-//	print("</td><td>");
-	$list[]	= $no;
+	//	print("<tr><td>");
+	print ("case $no: // " . $skill[name] . "<br>");
+	//	print("// target=({$skill[target][0]},{$skill[target][1]},{$skill[target][2]})<br>");
+	//	print("// pow={$skill[pow]}<br>");
+	//	print("break;<br>");
+	//	print("</td><td>");
+	$list[] = $no;
 }
-foreach($list as $var) {
-	print("<>$var");
+foreach ($list as $var)
+{
+	print ("<>$var");
 }
 //print("</tbody></table>");
+
+
+
 ?>
