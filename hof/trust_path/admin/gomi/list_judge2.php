2f8275588428b63a5a04a9b9387d3afaa47dfd20
bluelovers <codelovers@users.sourceforge.net>
2012-05-04 01:47:24 +0800

    format admin (Step: 8)
    Step: 8
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/admin/gomi/list_judge2.php

--- hof/trust_path/admin/gomi/list_judge2.php
+++ hof/trust_path/admin/gomi/list_judge2.php
@@ -1,9 +1,14 @@
 <?php
-include("./data.judge_setup_old.php");
-for($i=1000; $i<9999; $i++) {
-	$j	= LoadJudgeData($i);
-	if($j) {
-		print("case {$i}:// {$j[exp2]}<br>");
+
+include ("./data.judge_setup_old.php");
+for ($i = 1000; $i < 9999; $i++)
+{
+	$j = LoadJudgeData($i);
+	if ($j)
+	{
+		print ("case {$i}:// {$j[exp2]}<br>");
 	}
 }
+
+
 ?>
