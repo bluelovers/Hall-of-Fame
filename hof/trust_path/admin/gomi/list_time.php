aad7540d8f16b247d60f9711ebb396e870a667e6
bluelovers <codelovers@users.sourceforge.net>
2012-05-04 01:47:28 +0800

    format admin (Step: 12)
    Step: 12
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/admin/gomi/list_time.php

--- hof/trust_path/admin/gomi/list_time.php
+++ hof/trust_path/admin/gomi/list_time.php
@@ -1,10 +1,12 @@
 <title>time</title>
 <?php
-$now	= time();
-for($i=-24; $i<24; $i++) {
-	if($i==0)
-		print("Now : ".($now-60*60*$i)."<br />\n");
-	else
-		print("+{$i} : ".($now-60*60*$i)."<br />\n");
+
+$now = time();
+for ($i = -24; $i < 24; $i++)
+{
+	if ($i == 0) print ("Now : " . ($now - 60 * 60 * $i) . "<br />\n");
+	else  print ("+{$i} : " . ($now - 60 * 60 * $i) . "<br />\n");
 }
+
+
 ?>
