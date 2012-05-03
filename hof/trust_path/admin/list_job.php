0a9fc911e9f0253d36b21213f25c8a8233bb9356
bluelovers <codelovers@users.sourceforge.net>
2012-05-04 01:47:32 +0800

    format admin (Step: 17)
    Step: 17
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/admin/list_job.php

--- hof/trust_path/admin/list_job.php
+++ hof/trust_path/admin/list_job.php
@@ -32,10 +32,11 @@ td{
 </style></head>
 <body>
 <?php
-include("../data/data.job.php");
-include("../data/data.skill.php");
 
-$det	= '<tr><td class="a">No</td>
+include ("../data/data.job.php");
+include ("../data/data.skill.php");
+
+$det = '<tr><td class="a">No</td>
 <td class="a">Name</td>
 <td class="a">IMG</td>
 <td class="a">SP</td>
@@ -48,108 +49,110 @@ $det	= '<tr><td class="a">No</td>
 <td class="a">support</td>
 <td class="a">priority</td>
 <td class="a">charge</td>
-<td class="a">exp</td></tr>'."\n";
-$img_f	= "../image/char/";
+<td class="a">exp</td></tr>' . "\n";
+$img_f = "../image/char/";
 
-print('<table border="0" cellspacing="1"><tbody>');
+print ('<table border="0" cellspacing="1"><tbody>');
 //print($det);
-$detcount=0;
-for($no=100; $no<999; $no++) {
+$detcount = 0;
+for ($no = 100; $no < 999; $no++)
+{
 	$j = LoadJobData($no);
-	if(!$j) continue;
+	if (!$j) continue;
 
 	$detcount++;
 	//if($detcount%10==0) print($det);
 
-	print("<tr>");
-	print("<td>{$no}</td>");//no
-	print("<td>{$j[name_male]}</td>");//name
-	print("<td>{$j[coe][0]} : {$j[coe][1]}</td>");//name
-	print("<td><img src=\"{$img_f}{$j[img_male]}\"><img src=\"{$img_f}{$j[img_female]}\"></td>");//no
-	print("</tr>\n");
-	print("<tr>");
-	print("<td colspan=\"4\">");
-	foreach($j[equip] as $i)
-		print("$i, ");
-	print("</td>");
-	print("</tr>\n");
+	print ("<tr>");
+	print ("<td>{$no}</td>"); //no
+	print ("<td>{$j[name_male]}</td>"); //name
+	print ("<td>{$j[coe][0]} : {$j[coe][1]}</td>"); //name
+	print ("<td><img src=\"{$img_f}{$j[img_male]}\"><img src=\"{$img_f}{$j[img_female]}\"></td>"); //no
+	print ("</tr>\n");
+	print ("<tr>");
+	print ("<td colspan=\"4\">");
+	foreach ($j[equip] as $i) print ("$i, ");
+	print ("</td>");
+	print ("</tr>\n");
 	// 習得技
-	if($j[learn]) {
-	print("<tr><td colspan=\"4\">");
-	print('<table><tbody>');
-		foreach($j[learn] as $skill) {
-			print("<tr><td class=\"b\">");
-			$skill	= LoadSKillData($skill);
+	if ($j[learn])
+	{
+		print ("<tr><td colspan=\"4\">");
+		print ('<table><tbody>');
+		foreach ($j[learn] as $skill)
+		{
+			print ("<tr><td class=\"b\">");
+			$skill = LoadSKillData($skill);
 			ShowSkillDetail($skill);
-			print("</td></tr>");
+			print ("</td></tr>");
 		}
-	print("</tbody></table>");
-	print("</div></td></tr>");
+		print ("</tbody></table>");
+		print ("</div></td></tr>");
 	}
 }
 //print($det);
-print("</tbody></table>");
+print ("</tbody></table>");
+
+
 ?>
 </body>
 </html>
 <?php
+
 //////////////////////////////////////////////////
 //	技の詳細を表示
-	function ShowSkillDetail($skill,$radio=false) {
+function ShowSkillDetail($skill, $radio = false)
+{
 
-		define("IMG_ICON","../image/icon/");
+	define("IMG_ICON", "../image/icon/");
 
-		if(!$skill) return false;
+	if (!$skill) return false;
 
-		if($radio)
-			print('<input type="radio" name="newskill" value="'.$skill["no"].'" class="vcent">');
+	if ($radio) print ('<input type="radio" name="newskill" value="' . $skill["no"] . '" class="vcent">');
 
-		print('<img src="'.IMG_ICON.$skill["img"].'" class="vcent">');
-		print("{$skill[name]}");
+	print ('<img src="' . IMG_ICON . $skill["img"] . '" class="vcent">');
+	print ("{$skill[name]}");
 
-		if($radio)
-			print(" / <span class=\"bold\">{$skill[learn]}</span>pt");
+	if ($radio) print (" / <span class=\"bold\">{$skill[learn]}</span>pt");
 
-		if($skill[target][0] == "all")//対象
-			print(" / <span class=\"charge\">{$skill[target][0]}</span>");
-		else if($skill[target][0] == "enemy")
-			print(" / <span class=\"dmg\">{$skill[target][0]}</span>");
-		else if($skill[target][0] == "friend")
-			print(" / <span class=\"recover\">{$skill[target][0]}</span>");
-		else if($skill[target][0] == "self")
-			print(" / <span class=\"support\">{$skill[target][0]}</span>");
-		else if(isset($skill[target][0]))
-			print(" / {$skill[target][0]}");
+	if ($skill[target][0] == "all") //対象
+ 			print (" / <span class=\"charge\">{$skill[target][0]}</span>");
+	else
+		if ($skill[target][0] == "enemy") print (" / <span class=\"dmg\">{$skill[target][0]}</span>");
+		else
+			if ($skill[target][0] == "friend") print (" / <span class=\"recover\">{$skill[target][0]}</span>");
+			else
+				if ($skill[target][0] == "self") print (" / <span class=\"support\">{$skill[target][0]}</span>");
+				else
+					if (isset($skill[target][0])) print (" / {$skill[target][0]}");
 
-		if($skill[target][1] == "all")//単体or複数or全体
-			print(" - <span class=\"charge\">{$skill[target][1]}</span>");
-		else if($skill[target][1] == "individual")
-			print(" - <span class=\"recover\">{$skill[target][1]}</span>");
-		else if($skill[target][1] == "multi")
-			print(" - <span class=\"spdmg\">{$skill[target][1]}</span>");
-		else if(isset($skill[target][1]))
-			print(" - {$skill[target][1]}");
+	if ($skill[target][1] == "all") //単体or複数or全体
+ 			print (" - <span class=\"charge\">{$skill[target][1]}</span>");
+	else
+		if ($skill[target][1] == "individual") print (" - <span class=\"recover\">{$skill[target][1]}</span>");
+		else
+			if ($skill[target][1] == "multi") print (" - <span class=\"spdmg\">{$skill[target][1]}</span>");
+			else
+				if (isset($skill[target][1])) print (" - {$skill[target][1]}");
 
-		if(isset($skill["sp"]))
-			print(" / <span class=\"support\">{$skill[sp]}sp</span>");
-		if($skill["pow"]) {
-			print(" / <span class=\"".($skill["support"]?"recover":"dmg")."\">{$skill[pow]}%</span>x");
-			print(( $skill["target"][2] ? $skill["target"][2] : "1" ) );
-		}
-		if($skill["type"] == 1)
-			print(" / <span class=\"spdmg\">Magic</span>");
-		if($skill["invalid"])
-			print(" / <span class=\"charge\">invalid</span>");
-		if($skill["priority"] == "Back")
-			print(" / <span class=\"support\">BackAttack</span>");
-		if($skill["support"])
-			print(" / <span class=\"charge\">support</span>");
-		if($skill["charge"]["0"] || $skill["charge"]["1"]) {
-			print(" / (".($skill["charge"]["0"]?$skill["charge"]["0"]:"0").":");
-			print(($skill["charge"]["1"]?$skill["charge"]["1"]:"0").")");
-		}
-		if($skill["exp"])
-			print(" / {$skill[exp]}");
-		print("\n");
+	if (isset($skill["sp"])) print (" / <span class=\"support\">{$skill[sp]}sp</span>");
+	if ($skill["pow"])
+	{
+		print (" / <span class=\"" . ($skill["support"] ? "recover" : "dmg") . "\">{$skill[pow]}%</span>x");
+		print (($skill["target"][2] ? $skill["target"][2] : "1"));
 	}
+	if ($skill["type"] == 1) print (" / <span class=\"spdmg\">Magic</span>");
+	if ($skill["invalid"]) print (" / <span class=\"charge\">invalid</span>");
+	if ($skill["priority"] == "Back") print (" / <span class=\"support\">BackAttack</span>");
+	if ($skill["support"]) print (" / <span class=\"charge\">support</span>");
+	if ($skill["charge"]["0"] || $skill["charge"]["1"])
+	{
+		print (" / (" . ($skill["charge"]["0"] ? $skill["charge"]["0"] : "0") . ":");
+		print (($skill["charge"]["1"] ? $skill["charge"]["1"] : "0") . ")");
+	}
+	if ($skill["exp"]) print (" / {$skill[exp]}");
+	print ("\n");
+}
+
+
 ?>
