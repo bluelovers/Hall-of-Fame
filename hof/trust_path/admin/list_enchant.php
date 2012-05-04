71091a6b43b85657fdb368f49de5181b7f8de0d3
bluelovers <codelovers@users.sourceforge.net>
2012-05-04 20:54:39 +0800

    HOF_Model_Item::addEnchantData (Step: 3)
    Step: 3
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/admin/list_enchant.php

--- hof/trust_path/admin/list_enchant.php
+++ hof/trust_path/admin/list_enchant.php
@@ -8,14 +8,14 @@ print ("---------------LOW<br />\n");
 foreach ($low as $enchant)
 {
 	$item = array();
-	AddEnchantData($item, $enchant);
+	HOF_Model_Item::addEnchantData($item, $enchant);
 	print ('<span style="width:10em;text-align:right">' . $enchant . '</span>:' . $item["option"] . "<br />\n");
 }
 print ("---------------HIGH<br />\n");
 foreach ($high as $enchant)
 {
 	$item = array();
-	AddEnchantData($item, $enchant);
+	HOF_Model_Item::addEnchantData($item, $enchant);
 	print ('<span style="width:10em;text-align:right">' . $enchant . '</span>:' . $item["option"] . "<br />\n");
 }
 
