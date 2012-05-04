fc354cbf93d014cdbc709099b1ff7d58d05118c3
bluelovers <codelovers@users.sourceforge.net>
2012-05-05 00:12:06 +0800

    css3 flip-h
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/class/class.main.php

--- hof/trust_path/class/class.main.php
+++ hof/trust_path/class/class.main.php
@@ -4367,6 +4367,21 @@ Users : <?=
 <link rel="stylesheet" href="./static/style/style.css" type="text/css">
 <script type="text/javascript" src="http://code.jquery.com/jquery-latest.pack.js"></script>
 <script type="text/javascript" src="./static/js/jquery-core.js"></script>
+
+
+<style>
+
+.flip-h {
+    -moz-transform: scaleX(-1);
+    -o-transform: scaleX(-1);
+    -webkit-transform: scaleX(-1);
+    transform: scaleX(-1);
+    filter: FlipH;
+    -ms-filter: "FlipH";
+}
+
+</style>
+
 <?php
 
 		}
