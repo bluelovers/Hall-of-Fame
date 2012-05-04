b828107f39c6f09f3e990fca5e5ca7a9829a23b9
bluelovers <codelovers@users.sourceforge.net>
2012-05-04 17:53:54 +0800

    html format (Step: 3)
    Step: 3
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/data/data.tutorial.php

--- hof/trust_path/data/data.tutorial.php
+++ hof/trust_path/data/data.tutorial.php
@@ -1,49 +1,59 @@
 <div style="margin:15px">
-<h4>チュートリアル</h4>
-<p>戦闘の基本的な戦略(イメージ:下図)</p>
-<img src="./static/image/manual/t001.gif" alt="いめーじ" />
-<p>基本は <span class="bold">戦士</span>(Warrior) のような打たれ強いキャラを <span class="bold">前衛</span>(Front) に、<br />
-<span class="bold">魔法使い</span>(Sorecerer) のような打たれ弱いキャラを <span class="bold">後衛</span>(Back) に。</p>
-<p>前衛は後衛を守り、攻撃力の高い後衛が敵を倒し、<br />
-傷ついた前衛を後衛が治してあげる。という具合です。</p>
-<h4>とりあえず やてみろ！</h4>
-<p>戦士(Warrior), 魔法使い(Sorecerer) と 僧侶(Priest) が揃うように<br />
-メニューの <span class="bold">街(Town) → </span><a href="?recruit">人材斡旋所(Recruit)</a> から雇ってみましょ
-<span class="light">(リンクはログインしてる場合のみ有効)</span><br />
-雇った時点で、そこそこ戦えるように設定してあります。</p>
-<img src="<?=
+		<h4>チュートリアル</h4>
+		<p>戦闘の基本的な戦略(イメージ:下図)</p>
+		<img src="./static/image/manual/t001.gif" alt="いめーじ" />
+		<p>基本は <span class="bold">戦士</span>(Warrior) のような打たれ強いキャラを <span class="bold">前衛</span>(Front) に、<br />
+			<span class="bold">魔法使い</span>(Sorecerer) のような打たれ弱いキャラを <span class="bold">後衛</span>(Back) に。</p>
+		<p>前衛は後衛を守り、攻撃力の高い後衛が敵を倒し、<br />
+			傷ついた前衛を後衛が治してあげる。という具合です。</p>
+		<h4>とりあえず やてみろ！</h4>
+		<p>戦士(Warrior), 魔法使い(Sorecerer) と 僧侶(Priest) が揃うように<br />
+			メニューの <span class="bold">街(Town) → </span><a href="?recruit">人材斡旋所(Recruit)</a> から雇ってみましょ <span class="light">(リンクはログインしてる場合のみ有効)</span><br />
+			雇った時点で、そこそこ戦えるように設定してあります。</p>
+		<img src="<?=
 
 IMG_CHAR_REV
 
 
-?>mon_079.gif" alt="守る人" />
-<img src="<?=
+?>mon_079.gif" alt="守る人" /> <img src="<?=
 
 IMG_CHAR_REV
 
 
-?>mon_018.gif" alt="痛い攻撃する人" />
-<img src="<?=
+?>mon_018.gif" alt="痛い攻撃する人" /> <img src="<?=
 
 IMG_CHAR_REV
 
 
 ?>mon_214.gif" alt="治す人" />
-<p>ではさっそく、戦闘です。<br />
-メニューの 狩り(Hunt) → <a href="?common=gb0">ゴブリンと遊ぶ</a><span class="light">(リンクはログインしてる場合のみ有効)</span><br />
-雇った人たちにチェックをつけたら、<br />
-Battle !</p>
-<p>ちゃんとできていれば、<br />戦闘結果が表示されると思います。</p>
-<h4>メニューの構成</h4>
-<img src="./static/image/manual/003.gif">
-<h4>うんぬん</h4>
-<div class="bold u">雇える初期キャラ</div>
-<p><ul>
-<li>戦士(Warrior) - 打たれ強い。</li>
-<li>魔法(Sorcerer) - 攻撃力は高いが、打たれ弱い。</li>
-<li>僧侶(Priest) - 回復役。</li>
-<li>弓使い(Hunter) - 相手の前衛を無視して後衛を攻撃できる等。</li>
-</ul></p>
-<h4>うんぬ2ん</h4>
-編集日(2008/01/28)
-</div>
+		<p>ではさっそく、戦闘です。<br />
+			メニューの 狩り(Hunt) → <a href="?common=gb0">ゴブリンと遊ぶ</a><span class="light">(リンクはログインしてる場合のみ有効)</span><br />
+			雇った人たちにチェックをつけたら、<br />
+			Battle !</p>
+		<p>ちゃんとできていれば、<br />
+			戦闘結果が表示されると思います。</p>
+		<h4>メニューの構成</h4>
+		<img src="./static/image/manual/003.gif">
+		<h4>うんぬん</h4>
+		<div class="bold u">
+			雇える初期キャラ
+		</div>
+		<p>
+		<ul>
+			<li>
+				戦士(Warrior) - 打たれ強い。
+			</li>
+			<li>
+				魔法(Sorcerer) - 攻撃力は高いが、打たれ弱い。
+			</li>
+			<li>
+				僧侶(Priest) - 回復役。
+			</li>
+			<li>
+				弓使い(Hunter) - 相手の前衛を無視して後衛を攻撃できる等。
+			</li>
+		</ul>
+		</p>
+		<h4>うんぬ2ん</h4>
+		編集日(2008/01/28)
+	</div>
