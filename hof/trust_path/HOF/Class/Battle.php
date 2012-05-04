578d7ffe4b44c7403f49b19c9cffba4ae3b12f28
bluelovers <codelovers@users.sourceforge.net>
2012-05-05 00:11:08 +0800

    HOF_Class_Battle_Style (Step: 1)
    Step: 1
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/HOF/Class/Battle.php

--- hof/trust_path/HOF/Class/Battle.php
+++ hof/trust_path/HOF/Class/Battle.php
@@ -8,4 +8,23 @@
 class HOF_Class_Battle extends battle
 {
 
+	function ShowCssImage()
+	{
+		$img = new HOF_Class_Battle_Style();
+
+		$img
+			->SetBackGround($this->BackGround)
+			->SetTeams($this->team1, $this->team0)
+			->SetMagicCircle($this->team1_mc, $this->team0_mc)
+		;
+
+		if (BTL_IMG_TYPE == 2)
+		{
+			// CSS画像反転無し
+			$img->NoFlip();
+		}
+
+		echo $img->exec();
+	}
+
 }
