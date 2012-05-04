3e1d2544597438f89c601c82973c97051597e989
bluelovers <codelovers@users.sourceforge.net>
2012-05-04 18:50:15 +0800

    __construct (Step: 9)
    Step: 9
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/class/class.union.php

--- hof/trust_path/class/class.union.php
+++ hof/trust_path/class/class.union.php
@@ -35,7 +35,7 @@ class union extends char
 
 	//////////////////////////////////////////////////
 	//	コンストラクタ
-	function union($file = false)
+	function __construct($file = false)
 	{
 		$this->LoadData($file);
 	}
