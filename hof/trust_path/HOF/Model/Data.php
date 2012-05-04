9da77b685b75923c8084b703f631869b1b7f06e5
bluelovers <codelovers@users.sourceforge.net>
2012-05-04 19:56:35 +0800

    HOF_Model_Data::getItemData (Step: 182)
    Step: 182
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/HOF/Model/Data.php

--- hof/trust_path/HOF/Model/Data.php
+++ hof/trust_path/HOF/Model/Data.php
@@ -188,6 +188,164 @@ class HOF_Model_Data extends HOF_Class_Data
 	}
 
 	/**
+	 * 1000-1100	片手剣
+	 * 1300-1400	両手槍
+	 * 1400-1500	片手槍
+	 * 1500-1600	両手斧
+	 * 1600-1700	片手斧
+	 * 1700-1800	片手杖
+	 * 1800-1900	両手杖
+	 * 1900-2000	鈍器(片手)
+	 * 2000-2100	弓
+	 * 2100-2199	石弓
+	 * 2200-2299	鞭
+	 * 2210			鞭
+	 * 3000			盾
+	 * 3100			本
+	 * 5000-5100	鎧
+	 * 5100-5200	服
+	 * 5200-5300	衣
+	 * 5500			装飾品
+	 * 6000			素材系 石系
+	 * 6020			木材
+	 * 6040			皮
+	 * 6060			骨
+	 * 6080			牙
+	 * 6100			羽
+	 * 6120			宝石
+	 * 6140			音
+	 * 6160			コイン
+	 * 6180			糸,繊維
+	 * 6200			音
+	 * 6600			ゴミ
+	 * 6800			レア
+	 * 7000			製作強化系
+	 * 7100			製作強化系(モンスターレアドロップ)
+	 * 7500			他消耗品
+	 * 8000			地図,カギ
+	 * 9000			その他
+	 */
+	function getItemData($no)
+	{
+		if (file_exists(DATA_ENCHANT)) include (DATA_ENCHANT);
+
+		//アイテムの種類
+		$base = substr($no, 0, 4);
+
+		//精錬値
+		$refine = (int)substr($no, 4, 2);
+
+		// 付加能力
+		$option0 = substr($no, 6, 3);
+		$option1 = substr($no, 9, 3);
+		$option2 = substr($no, 12, 3);
+
+		/**
+		 * 設定項目
+		 * ---------------------------------------------
+		 * "name"=>"名称",
+		 * "type"=>"種類",
+		 * "buy"=>"買値",
+		 * "img"=>"画像",
+		 * "atk"=>array(物理攻撃,魔法攻撃),
+		 * "def"=>array(物理割?,物理減,魔法割?,魔法減),
+		 * "dh"=> true,//両手武器か否か( "D"ouble"H"and )
+		 * "handle"=>"数値",
+		 * "need" => array("素材番号"=>数, ...),// 製作に必要なアイテム
+		 * ---------------------------------------------
+		 * type
+		 * "Sword"	片手剣
+		 * "TwoHandSword"	両手剣
+		 * "Dagger"	短剣
+		 * "Spear"	両手槍
+		 * "Pike"	片手槍
+		 * "Axe"	両手斧
+		 * "Hatchet"片手斧
+		 * "Wand"	片手杖
+		 * "Staff"	両手杖
+		 * "Mace"	鈍器(片手)
+		 * "Bow"	弓
+		 * "CrossBow"	石弓
+		 *
+		 * "Shield"	盾
+		 * "MainGauche"	防御用短剣
+		 * "Book"	本
+		 *
+		 * "Armor"	鎧
+		 * "Cloth"	服
+		 * "Robe"	衣
+		 *
+		 * "?"
+		 *--------------------------------------------
+		 * 追加オプション
+		 * P_MAXHP
+		 * M_MAXHP
+		 * P_MAXSP
+		 * M_MAXSP
+		 * P_STR
+		 * P_INT
+		 * P_DEX
+		 * P_SPD
+		 * P_LUK
+		 * P_SUMMON = 召還力強化
+		 * P_PIERCE = array(物理,魔法),
+		 *--------------------------------------------
+		 */
+		$data = self::getInstance()->_load('item', $base);
+
+		$data["no"] = $no;
+
+		// 精錬値
+		if ($refine)
+		{
+			$data["refine"] = $refine;
+			$data["name"] = "+" . $refine . " " . $data["name"];
+			//$data["name"]	.= "+".$refine;
+			//$RefineRate	= 1 + 0.5 * ($refine/10);
+
+			if (isset($data["atk"]["0"]))
+			{
+				//$data["atk"]["0"]	= ceil($data["atk"]["0"] * $RefineRate);// 単純式
+				// 1.05*1.05*1.05....
+				/*
+				for($i=0; $i<$refine; $i++) {
+				$data["atk"]["0"]	*= 1.05;
+				}
+				*/
+				$data["atk"]["0"] *= (1 + ($refine * $refine) / 100);
+				$data["atk"]["0"] = ceil($data["atk"]["0"]);
+			}
+
+			if (isset($data["atk"]["1"]))
+			{
+				//$data["atk"]["1"]	= ceil($data["atk"]["1"] * $RefineRate);
+				/*
+				for($i=0; $i<$refine; $i++) {
+				$data["atk"]["1"]	*= 1.05;
+				}
+				*/
+				$data["atk"]["1"] *= (1 + ($refine * $refine) / 100);
+				$data["atk"]["1"] = ceil($data["atk"]["1"]);
+			}
+
+			// 防具の値強化
+			$RefineRate = 1 + 0.3 * ($refine / 10);
+			if (isset($data["def"]["0"])) $data["def"]["0"] = ceil($data["def"]["0"] * $RefineRate);
+			if (isset($data["def"]["1"])) $data["def"]["1"] = ceil($data["def"]["1"] * $RefineRate);
+			if (isset($data["def"]["2"])) $data["def"]["2"] = ceil($data["def"]["2"] * $RefineRate);
+			if (isset($data["def"]["3"])) $data["def"]["3"] = ceil($data["def"]["3"] * $RefineRate);
+
+		}
+
+		// 付加能力
+		if ($option0) AddEnchantData($data, $option0);
+		if ($option1) AddEnchantData($data, $option1);
+		if ($option2) AddEnchantData($data, $option2);
+
+		return $data;
+	}
+
+	/**
 	 * 店に売ってるものデータ
 	 * 店販売リスト
 	 */
