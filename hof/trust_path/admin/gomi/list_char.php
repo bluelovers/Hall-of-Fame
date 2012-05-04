65ff5a711b1a6e7fd66493f7c7962e4b42045b17
bluelovers <codelovers@users.sourceforge.net>
2012-05-04 15:27:45 +0800

    use HOF_Model_Char to handle HOF_Class_Char (Step: 4)
    Step: 4
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/admin/gomi/list_char.php

--- hof/trust_path/admin/gomi/list_char.php
+++ hof/trust_path/admin/gomi/list_char.php
@@ -49,7 +49,7 @@ foreach ($folder as $val)
 print ('<table border="0" cellspacing="1"><tbody>');
 foreach ($chars as $file)
 {
-	$char = new HOF_Class_Char(ParseFile($file));
+	$char = HOF_Model_Char::newCharFromFile(ParseFile($file));
 	print ("<tr><td>");
 	$char->ShowImage();
 	print ("</td><td>");
