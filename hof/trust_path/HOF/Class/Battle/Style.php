d5e59be785aa8aa15136889a361bfc5a59be2d17
bluelovers <codelovers@users.sourceforge.net>
2012-05-05 00:11:09 +0800

    HOF_Class_Battle_Style (Step: 2)
    Step: 2
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/HOF/Class/Battle/Style.php

--- hof/trust_path/HOF/Class/Battle/Style.php
+++ hof/trust_path/HOF/Class/Battle/Style.php
@@ -7,9 +7,185 @@
 
 include_once BTL_IMG_CSS;
 
-class HOF_Class_Battle_Style extends cssimage
+/**
+ * スタイルシートで画像?領域?反転可能だったのを思い出したので
+ * それを用いて戦闘画面を作る。
+ * ただしブラウザによっては上手く表示されないと思う。
+
+ * GDと違って反転済みの画像を用意する必要無し。
+ * IEは表示できる。
+ */
+//class HOF_Class_Battle_Style extends cssimage
+class HOF_Class_Battle_Style extends HOF_Class_Array
 {
 
+	/**
+	 * CSSで image.flip() を使うか使わないか。
+	 */
+	function NoFlip()
+	{
+		$this->NoFlip = true;
 
+		return $this;
+	}
+
+	/**
+	 * 背景画像をセット。
+	 * ついでに大きさも取得する。
+	 */
+	function SetBackGround($bg)
+	{
+		$this->background = IMG_OTHER . "bg_" . $bg . ".gif";
+
+		list($this->img_x, $this->img_y) = getimagesize($this->background);
+		$this->size = "width:{$this->img_x}; height:{$this->img_y};";
+
+		return $this;
+	}
+
+	/**
+	 * チームの情報をセット
+	 * 前衛後衛に分ける
+	 */
+	function SetTeams($team1, $team2)
+	{
+		foreach ($team1 as $char)
+		{
+			// 召喚キャラが死亡している場合は飛ばす
+			if ($char->STATE === DEAD && $char->summon == true) continue;
+			if ($char->POSITION == "front") $this->team1_front[] = $char;
+			else  $this->team1_back[] = $char;
+		}
+
+		foreach ($team2 as $char)
+		{
+			// 召喚キャラが死亡している場合は飛ばす
+			if ($char->STATE === DEAD && $char->summon == true) continue;
+			if ($char->POSITION == "front") $this->team2_front[] = $char;
+			else  $this->team2_back[] = $char;
+		}
+
+		return $this;
+	}
+
+	/**
+	 * 魔方陣の数
+	 */
+	function SetMagicCircle($team1_mc, $team2_mc)
+	{
+		$this->team1_mc = $team2_mc;
+		$this->team2_mc = $team1_mc;
+
+		return $this;
+	}
+
+	/**
+	 * CSS( キャラ画像 ,x座標 ,y座標 )
+	 */
+	function det($url, $x, $y)
+	{
+		return "background-image:url({$url});background-repeat:no-repeat;background-position:{$x}px {$y}px;";
+	}
+
+	/**
+	 * 戦闘画面を表示
+	 */
+	function exec()
+	{
+		$this->output = '';
+
+		//print("<div style=\"postion:relative;height:{$this->img_x}px;\">\n");
+		//$this->div++;
+		// 背景を表示 ( 中央表示の為に左にずらす )
+		$margin = (-1) * round($this->img_x / 2);
+		$this->output .= "<div style=\"position:relative;left:50%;margin-left:{$margin}px;{$this->size}" . $this->det($this->background, 0, 0) . "\">\n";
+		$this->div++;
+
+		// 魔方陣を表示する
+		if (0 < $this->team1_mc)
+		{
+			$this->output .= "<div style=\"{$this->size}" . $this->det(IMG_OTHER . "mc0_" . $this->team1_mc . ".gif", 280, 0) . "\">\n";
+			$this->div++;
+		}
+		if (0 < $this->team2_mc)
+		{
+			$this->output .= "<div style=\"{$this->size}" . $this->det(IMG_OTHER . "mc1_" . $this->team2_mc . ".gif", 0, 0) . "\">\n";
+			$this->div++;
+		}
+
+		$cell_width = ($this->img_x) / 6; //横幅を6分割した長さ
+		$y = $this->img_y / 2; //高さの中心
+
+		// team1 を表示(後列→前列)
+		$this->CopyRow($this->team1_back, 0, $cell_width * 1, $cell_width, $y, $this->img_y);
+		$this->CopyRow($this->team1_front, 0, $cell_width * 2, $cell_width, $y, $this->img_y);
+
+		if (!$this->NoFlip)
+		{
+			// 反転用のCSS
+			$this->output .= "<div class=\"flip-h\">\n";
+			$this->div++;
+			$dir = 0;
+			$backs = 1;
+			$fore = 2;
+		}
+		else
+		{
+			$dir = 1;
+			$backs = 5;
+			$fore = 4;
+		}
+
+		$this->CopyRow($this->team2_back, $dir, $cell_width * $backs, $cell_width, $y, $this->img_y);
+		$this->CopyRow($this->team2_front, $dir, $cell_width * $fore, $cell_width, $y, $this->img_y);
+
+		for ($i = 0; $i < $this->div; $i++) $this->output .= "</div>";
+
+		return $this;
+	}
+
+	/**
+	 * 列のキャラを描き出す
+	 */
+	function CopyRow($teams, $direction, $axis_x, $cell_width, $axis_y, $cell_height)
+	{
+		$number = count($teams);
+		if ($number == 0) return false;
+
+		$axis_x += ($direction ? -$cell_width / 2 : + $cell_width / 2);
+		$axis_y += ($direction ? -$cell_height / 2 : -$cell_height / 2);
+
+		$gap_x = $cell_width / ($number + 1) * ($direction ? 1 : -1);
+		$gap_y = $cell_height / ($number + 1) * ($direction ? 1 : 1);
+
+		$f = $direction ? IMG_CHAR_REV : IMG_CHAR;
+
+		foreach ($teams as $char)
+		{
+			$this->div++;
+			$gap++;
+			$x = $axis_x + ($gap_x * $gap);
+			$y = $axis_y + ($gap_y * $gap);
+
+			$x = floor($x);
+			$y = floor($y);
+			if ($char->STATE === DEAD) $img = $f . DEAD_IMG;
+			else  $img = $char->GetImageUrl($f);
+			list($img_x, $img_y) = getimagesize($img);
+			$x -= round($img_x / 2);
+			$y -= round($img_y / 2);
+			$this->output .= "<div style=\"{$this->size}" . $this->det($img, $x, $y) . "\">\n";
+		}
+	}
+
+	function output()
+	{
+		return (string)$this->output;
+	}
+
+	function __toString()
+	{
+		return $this->output();
+	}
 
 }
