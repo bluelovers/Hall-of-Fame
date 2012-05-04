83285be4754a227c4ffb2b2924c5f89e63eba0d7
bluelovers <codelovers@users.sourceforge.net>
2012-05-04 20:16:27 +0800

    HOF_Model_Data::getTownAppear (Step: 1)
    Step: 1
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/HOF/Model/Data.php

--- hof/trust_path/HOF/Model/Data.php
+++ hof/trust_path/HOF/Model/Data.php
@@ -452,4 +452,28 @@ class HOF_Model_Data extends HOF_Class_Data
 			);
 	}
 
+	/**
+	 * 町でいける店とかの出現条件とか...
+	 * 日付別でいける場所を変えれるとか、
+	 * あるアイテムがないと行けないとかできる
+	 * 別ファイルにする必要があったのかどうか微妙
+	 */
+	function getTownAppear($user)
+	{
+		$place = array();
+
+		// 無条件で行ける
+		$place["Shop"] = true;
+		$place["Recruit"] = true;
+		$place["Smithy"] = true;
+		$place["Auction"] = true;
+		$place["Colosseum"] = true;
+
+		// 特定のアイテムがないと行けない施設
+		//if($user->item[****])
+		//	$place["****"]	= true;
+
+		return $place;
+	}
+
 }
