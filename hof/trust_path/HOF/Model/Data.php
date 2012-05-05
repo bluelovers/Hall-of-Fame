37a989179671f1b4cb088f3bb49c2256261f8a50
bluelovers <codelovers@users.sourceforge.net>
2012-05-06 05:28:44 +0800

    DEBUG_LANDAPPEAR_ALL (Step: 1)
    Step: 1
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/HOF/Model/Data.php

--- hof/trust_path/HOF/Model/Data.php
+++ hof/trust_path/HOF/Model/Data.php
@@ -122,33 +122,34 @@ class HOF_Model_Data extends HOF_Class_Data
 		array_push($land, "gb0", "gb1", "gb2");
 
 		// アイテムがあれば行ける。
-		if ($user->item["8000"]) array_push($land, "ac0");
-		if ($user->item["8001"]) array_push($land, "ac1");
-		if ($user->item["8002"]) array_push($land, "ac2");
-		if ($user->item["8003"]) array_push($land, "ac3");
-		if ($user->item["8004"]) array_push($land, "ac4");
+		if (DEBUG_LANDAPPEAR_ALL || $user->item["8000"]) array_push($land, "ac0");
+		if (DEBUG_LANDAPPEAR_ALL || $user->item["8001"]) array_push($land, "ac1");
+		if (DEBUG_LANDAPPEAR_ALL || $user->item["8002"]) array_push($land, "ac2");
+		if (DEBUG_LANDAPPEAR_ALL || $user->item["8003"]) array_push($land, "ac3");
+		if (DEBUG_LANDAPPEAR_ALL || $user->item["8004"]) array_push($land, "ac4");
 
-		if ($user->item["8009"]) array_push($land, "snow0");
-		if ($user->item["8010"]) array_push($land, "snow1");
-		if ($user->item["8011"]) array_push($land, "snow2");
+		if (DEBUG_LANDAPPEAR_ALL || $user->item["8009"]) array_push($land, "snow0");
+		if (DEBUG_LANDAPPEAR_ALL || $user->item["8010"]) array_push($land, "snow1");
+		if (DEBUG_LANDAPPEAR_ALL || $user->item["8011"]) array_push($land, "snow2");
 
-		/*
-		array_push($land,"sea0");
-		array_push($land,"sea1");
-		array_push($land,"ocean0");
-		array_push($land,"sand0");
-		array_push($land,"swamp0");
-		array_push($land,"swamp1");
-		array_push($land,"mt0");
-		array_push($land,"volc0");
-		array_push($land,"volc1");
+		if (DEBUG_LANDAPPEAR_ALL)
+		{
+			array_push($land,"sea0");
+			array_push($land,"sea1");
+			array_push($land,"ocean0");
+			array_push($land,"sand0");
+			array_push($land,"swamp0");
+			array_push($land,"swamp1");
+			array_push($land,"mt0");
+			array_push($land,"volc0");
+			array_push($land,"volc1");
 
-		array_push($land,"blow01");
-		array_push($land,"plund01");
-		array_push($land,"des01");
-		*/
+			array_push($land,"blow01");
+			array_push($land,"plund01");
+			array_push($land,"des01");
+		}
 
-		if (gc_date("H") == 2 && substr(gc_date("i"), 0, 1) == 5) array_push($land, "horh");
+		if (DEBUG_LANDAPPEAR_ALL || gc_date("H") == 2 && substr(gc_date("i"), 0, 1) == 5) array_push($land, "horh");
 		return $land;
 	}
 
