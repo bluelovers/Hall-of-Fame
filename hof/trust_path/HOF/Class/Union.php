57d3bdc78cffbfad595efdccc136ef6ed793a5a6
bluelovers <codelovers@users.sourceforge.net>
2012-05-05 16:59:35 +0800

    . (Step: 3)
    Step: 3
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/HOF/Class/Union.php

--- hof/trust_path/HOF/Class/Union.php
+++ hof/trust_path/HOF/Class/Union.php
@@ -10,5 +10,33 @@ include_once (CLASS_UNION);
 class HOF_Class_Union extends union
 {
 
+	/**
+	 * 戦闘中のキャラ名,HP,SP を色を分けて表示する
+	 * それ以外にも必要な物があれば表示するようにした。
+	 */
+	function ShowHpSp()
+	{
+		$output = '';
+
+		if ($this->STATE === 1) $sub = " dmg";
+		else
+			if ($this->STATE === 2) $sub = " spdmg";
+		//名前
+		$output .= "<span class=\"bold{$sub}\">{$this->name}</span>\n";
+		// チャージor詠唱
+		if ($this->expect_type === 0) $output .= '<span class="charge">(charging)</span>' . "\n";
+		else
+			if ($this->expect_type === 1) $output .= '<span class="charge">(casting)</span>' . "\n";
+		// HP,SP
+		$output .= "<div class=\"hpsp\">\n";
+		$sub = $this->STATE === 1 ? "dmg" : "recover";
+		//print("<span class=\"{$sub}\">HP : ????/{$this->MAXHP}</span><br />\n");//HP
+		$output .= "<span class=\"{$sub}\">HP : ????/????</span><br />\n"; //HP
+		$sub = $this->STATE === 1 ? "dmg" : "support";
+		$output .= "<span class=\"{$sub}\">SP : ????/????</span>\n";
+		$output .= "</div>\n"; //SP
+
+		return $output;
+	}
 
 }
