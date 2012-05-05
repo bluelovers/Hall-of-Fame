d80b324215138be00225d21e15fd7176fed0929a
bluelovers <codelovers@users.sourceforge.net>
2012-05-05 22:27:32 +0800

    HOF_Class_Icon (Step: 3)
    Step: 3
    
    Commit ChangedLog:
    
    [A ] new file:
    
    	hof/trust_path/HOF/Class/Icon.php

---
+++ hof/trust_path/HOF/Class/Icon.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * @author bluelovers
+ * @copyright 2012
+ */
+
+class HOF_Class_Icon
+{
+	static $cache = array();
+	static $map_imgtype = array(
+		'png',
+		'gif',
+		'jpg',
+		'bmp',
+		);
+
+	/**
+	 * @example HOF_Class_Icon::getIamge('ori_003', IMG_CHAR)
+	 */
+	function getIamge($no, $dir, $return_true = false)
+	{
+		$dir = rtrim($dir, '/').'/';
+
+		if (!isset(self::$cache[$dir][$no]))
+		{
+			$file = false;
+
+			foreach (self::$map_imgtype as $ext)
+			{
+				$_file = $dir . $no . '.' . $ext;
+				if (file_exists($_file))
+				{
+					$file = $_file;
+					break;
+				}
+			}
+
+			self::$cache[$dir][$no] = $file;
+		}
+
+		if (self::$cache[$dir][$no])
+		{
+			$ret = self::$cache[$dir][$no];
+		}
+		else
+		{
+			$ret = $dir . ($return_true ? $no : NO_IMAGE) . '.' . reset(self::$map_imgtype);
+		}
+
+		return $ret;
+	}
+
+}
