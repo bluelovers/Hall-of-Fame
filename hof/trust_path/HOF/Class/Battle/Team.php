67e5a379e13b1e6fd3a7245a6835cecaf9dd5390
bluelovers <codelovers@users.sourceforge.net>
2012-05-05 19:47:33 +0800

    HOF_Class_Battle_Team (Step: 2)
    Step: 2
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/HOF/Class/Battle/Team.php

--- hof/trust_path/HOF/Class/Battle/Team.php
+++ hof/trust_path/HOF/Class/Battle/Team.php
@@ -5,16 +5,129 @@
  * @copyright 2012
  */
 
-class HOF_Class_Battle_Team
+class HOF_Class_Battle_Team extends HOF_Class_Array
 {
+
+	function __construct($team = array())
+	{
+		if ($team instanceof self)
+		{
+			$team = $team->toArray();
+		}
+
+		parent::__construct($team);
+
+		unset($this->_data_default_);
+
+		if (!empty($team))
+		{
+			$this->update();
+		}
+	}
+
+	/**
+	 * @return self
+	 */
+	public static function &newInstance($team = array())
+	{
+		if ($team instanceof self)
+		{
+			return $team;
+		}
+
+		return new self($team);
+	}
+
+	function update($no = null)
+	{
+		foreach($this as $char)
+		{
+			$char->setTeamObj(&$this);
+
+			if (isset($no) && !is_null($no))
+			{
+				$char->SetTeam($no);
+			}
+		}
+	}
+
+	function addChar($char, $no = null)
+	{
+		$char->setTeamObj(&$this);
+		if (isset($no) && !is_null($no))
+		{
+			$char->SetTeam($no);
+		}
+
+		$this[] = $char;
+	}
+
+	function addChars($list, $no = null)
+	{
+		foreach($list as $char)
+		{
+			$this->addChar($char, $no);
+		}
+	}
+
+	protected function _getTeamArray($who)
+	{
+		if ($who instanceof self)
+		{
+			$team = $who->toArray();
+		}
+		elseif ($who instanceof HOF_Class_Char)
+		{
+			$team = $who->getTeamObj()->toArray();
+		}
+		elseif (@isset($this))
+		{
+			$team = $this->toArray();
+		}
+		else
+		{
+			$team = (array)$who;
+		}
+
+		return $team;
+	}
+
+	/**
+	 * 指定キャラのチームの死者数を数える(指定のチーム)ネクロマンサしか使ってない?
+	 */
+	function CountDead($who = null)
+	{
+		$team = self::_getTeamArray($who);
+
+		$count = 0;
+
+		foreach ((array)$team as $char)
+		{
+			if ($char->STATE === DEAD)
+			{
+				$count++;
+			}
+			else
+			{
+				if ($char->SPECIAL["Undead"] == true)
+				{
+					$count++;
+				}
+			}
+		}
+
+		return $count;
+	}
 
 	/**
 	 * 生存者数を数えて返す
 	 */
-	function CountAlive($team)
+	function CountAlive($who = null)
 	{
+		$team = self::_getTeamArray($who);
+
 		$no = 0; //初期化
-		foreach ($team as $char)
+		foreach ((array)$team as $char)
 		{
 			if ($char->STATE !== 1) $no++;
 		}
@@ -24,16 +137,18 @@ class HOF_Class_Battle_Team
 	/**
 	 * 初期キャラ生存数を数えて返す
 	 */
-		function CountAliveChars($team)
+	function CountAliveChars($who = null)
+	{
+		$team = self::_getTeamArray($who);
+
+		$no = 0; //初期化
+		foreach ((array)$team as $char)
 		{
-			$no = 0; //初期化
-			foreach ($team as $char)
-			{
-				if ($char->STATE === 1) continue;
-				if ($char->monster) continue;
-				$no++;
-			}
-			return $no;
+			if ($char->STATE === 1) continue;
+			if ($char->monster) continue;
+			$no++;
 		}
+		return $no;
+	}
 
 }
