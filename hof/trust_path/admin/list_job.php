0098e325e8109d7aef0b5a11afccbc1448e485e0
bluelovers <codelovers@users.sourceforge.net>
2012-05-05 23:09:12 +0800

    . (Step: 5)
    Step: 5
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/admin/list_job.php

--- hof/trust_path/admin/list_job.php
+++ hof/trust_path/admin/list_job.php
@@ -110,7 +110,7 @@ function ShowSkillDetail($skill, $radio = false)
 
 	if ($radio) print ('<input type="radio" name="newskill" value="' . $skill["no"] . '" class="vcent">');
 
-	print ('<img src="' . IMG_ICON . $skill["img"] . '" class="vcent">');
+	print ('<img src="' . HOF_Class_Icon::getIamge($skill["img"], IMG_ICON) . '" class="vcent">');
 	print ("{$skill[name]}");
 
 	if ($radio) print (" / <span class=\"bold\">{$skill[learn]}</span>pt");
