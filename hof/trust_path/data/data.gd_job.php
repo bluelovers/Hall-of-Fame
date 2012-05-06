e88c486c56acec8bb055b566df538557b076c1e5
bluelovers <codelovers@users.sourceforge.net>
2012-05-06 16:50:11 +0800

    HOF_Class_Icon::getIamgeUrl (Step: 12)
    Step: 12
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/data/data.gd_job.php

--- hof/trust_path/data/data.gd_job.php
+++ hof/trust_path/data/data.gd_job.php
@@ -164,8 +164,8 @@ foreach ($job as $No => $exp)
 	if ($JobData["name_male"] !== $JobData["name_female"]) print ("<br />(" . $JobData["name_female"] . ")");
 	print ('</span></td>' . "\n");
 	print ("<td$css>");
-	print ('<img src="' . HOF_Class_Icon::getIamge($JobData["img_male"], IMG_CHAR) . '" />');
-	print ('<img src="' . HOF_Class_Icon::getIamge($JobData["img_female"], IMG_CHAR) . '" />');
+	print ('<img src="' . HOF_Class_Icon::getIamgeUrl($JobData["img_male"], IMG_CHAR) . '" />');
+	print ('<img src="' . HOF_Class_Icon::getIamgeUrl($JobData["img_female"], IMG_CHAR) . '" />');
 	print ("</td>");
 	print ("<td$css>$exp");
 	print ("</td>");
