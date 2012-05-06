dea5b7b66fd924017ab542c66c28c18d25dbdada
bluelovers <codelovers@users.sourceforge.net>
2012-05-06 22:24:11 +0800

    HOF_Class_Controller::newInstance('log', 'log')->main(); (Step: 3)
    Step: 3
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/HOF/Controller/Log.php

--- hof/trust_path/HOF/Controller/Log.php
+++ hof/trust_path/HOF/Controller/Log.php
@@ -38,27 +38,88 @@ class HOF_Controller_Log extends HOF_Class_Controller
 	{
 		$logs = array();
 
+		$idx = $_SERVER["QUERY_STRING"];
+
+		if (in_array($idx, array(
+			'clog',
+			'ulog',
+			'rlog',
+			)))
+		{
+			$this->output->full_log = true;
+
+			$map = array();
+
+			if ($idx == 'ulog')
+			{
+				$map[] = LOG_BATTLE_UNION;
+			}
+			elseif ($idx == 'rlog')
+			{
+				$map[] = LOG_BATTLE_RANK;
+			}
+			else
+			{
+				$map[] = LOG_BATTLE_NORMAL;
+			}
+		}
+		else
+		{
+			$map = array(
+				LOG_BATTLE_NORMAL,
+				LOG_BATTLE_UNION,
+				LOG_BATTLE_RANK,
+				);
+		}
+
 		// common
 
-		foreach(array(
-			LOG_BATTLE_NORMAL,
-			LOG_BATTLE_UNION,
-			LOG_BATTLE_RANK,
-		) as $_k)
+		foreach ($map as $_k)
 		{
 			$log = game_core::glob($_k);
 			foreach (array_reverse($log) as $file)
 			{
 				$logs[$_k][] = $this->BattleLogDetail($file);
 				$limit++;
-				if (30 <= $limit)
+				if (!$this->output->full_log && 30 <= $limit)
 				{
 					break;
 				}
 			}
 		}
 
+		$this->output->idx = $idx;
+
 		$this->output['logs'] = $logs;
+	}
+
+	function _main_action_log()
+	{
+		$map = array(
+			'log' => LOG_BATTLE_NORMAL,
+			'clog' => LOG_BATTLE_NORMAL,
+			'ulog' => LOG_BATTLE_UNION,
+			'rlog' => LOG_BATTLE_RANK,
+			);
+
+		foreach ($map as $_k => $_v)
+		{
+			if ($_GET[$_k])
+			{
+				break;
+			}
+		}
+
+		if ($_k == 'log' || $_k == 'clog')
+		{
+			$_k = 'clog';
+		}
+
+		$this->output->idx = $_k;
+
+		list($this->output->log, $this->output->time) = $this->ShowBattleLog($_GET[$_k], $_v);
+
+		$this->options['escapeHtml'] = false;
 	}
 
 	/**
@@ -90,7 +151,41 @@ class HOF_Controller_Log extends HOF_Class_Controller
 			$act,
 			$date,
 
-		);
+			);
+	}
+
+	/**
+	 * 戦闘ログを回覧する
+	 */
+	function ShowBattleLog($no, $type = false)
+	{
+		if ($type == LOG_BATTLE_RANK)
+		{
+			$file = LOG_BATTLE_RANK . $no . ".dat";
+		}
+		elseif ($type == LOG_BATTLE_UNION)
+		{
+			$file = LOG_BATTLE_UNION . $no . ".dat";
+		}
+		else
+		{
+			$file = LOG_BATTLE_NORMAL . $no . ".dat";
+		}
+
+		if (!file_exists($file))
+		{
+			//ログが無い
+			return "log doesnt exists";
+		}
+
+		$log = file($file);
+		// ログの何行目から書き出すか?
+		$row = 6;
+		$time = substr($log[0], 0, 10);
+
+		//print('<table style="width:100%;text-align:center" class="break"><tr><td>'."\n");
+
+		return array($log, $time);
 	}
 
 }
