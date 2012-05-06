2fa8dc3a8c918fe786719fab92cf75dc4a84a698
bluelovers <codelovers@users.sourceforge.net>
2012-05-07 00:38:24 +0800

    . (Step: 2)
    Step: 2
    
    Commit ChangedLog:
    
    [A ] new file:
    
    	hof/trust_path/tpl/gamedata.item.php

---
+++ hof/trust_path/tpl/gamedata.item.php
@@ -0,0 +1,20 @@
+
+	<?php $this->extend('gamedata.layout') ?>
+
+	<div style="margin:0 15px">
+
+	<?php foreach($this->output->list as $type => $data): ?>
+
+		<h4><?php e($type) ?></h4>
+
+		<?php foreach($data as $v): ?>
+
+			<?php ShowItemDetail($v); ?>
+
+			<br />
+
+		<?php endforeach; ?>
+
+	<?php endforeach; ?>
+
+	</div>
