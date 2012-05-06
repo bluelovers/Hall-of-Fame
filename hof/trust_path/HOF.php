e34daf810483f9a9bed9d2214828806450fd6f98
bluelovers <codelovers@users.sourceforge.net>
2012-05-06 20:40:00 +0800

    . (Step: 8)
    Step: 8
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/HOF.php

--- hof/trust_path/HOF.php
+++ hof/trust_path/HOF.php
@@ -8,4 +8,39 @@
 class HOF
 {
 
+	public static function putintoClassParts($str)
+	{
+		$str = preg_replace('/[^a-z0-9_]/', '', $str);
+		$str = explode('_', $str);
+		$str = array_map('trim', $str);
+		$str = array_diff($str, array(''));
+		$str = array_map('ucfirst', $str);
+		$str = implode('', $str);
+		return $str;
+	}
+
+	public static function putintoPathParts($str)
+	{
+		$str = preg_replace('/[^a-zA-Z0-9]/', '', $str);
+		$str = preg_replace('/([A-Z])/', '_$1', $str);
+		$str = strtolower($str);
+		$str = substr($str, 1, strlen($str));
+		return $str;
+	}
+
+	public static function escapeHtml($string)
+	{
+		if (is_array($string))
+		{
+			foreach ($string as $_k => $_v)
+			{
+				$string[$_k] = self::escapeHtml($_v);
+			}
+
+			return $string;
+		}
+
+		return htmlspecialchars((string )$string, ENT_QUOTES);
+	}
+
 }
