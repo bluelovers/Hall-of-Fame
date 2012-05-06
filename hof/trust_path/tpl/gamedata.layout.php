332529b5449d29db495a4f6a9e7f5a3d579e5f74
bluelovers <codelovers@users.sourceforge.net>
2012-05-07 00:21:34 +0800

    HOF_Controller_Gamedata (Step: 2)
    Step: 2
    
    Commit ChangedLog:
    
    [A ] new file:
    
    	hof/trust_path/tpl/gamedata.layout.php

---
+++ hof/trust_path/tpl/gamedata.layout.php
@@ -0,0 +1,12 @@
+
+	<div style="margin:15px">
+		<h4>GameData</h4>
+		<div style="margin:0 20px">
+			| <a href="?gamedata=job">職(Job)</a> |
+			<a href="?gamedata=item">アイテム(item)</a> |
+			<a href="?gamedata=judge">判定</a> |
+			<a href="?gamedata=monster">モンスター</a> |
+		</div>
+	</div>
+
+	<?php e($this->content) ?>
