a66b75559b2e031496e65bb4166180c0e55f0fd0
bluelovers <codelovers@users.sourceforge.net>
2012-05-06 14:31:45 +0800

    fix land info (Step: 2)
    Step: 2
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/HOF/Class/Yaml.php

--- hof/trust_path/HOF/Class/Yaml.php
+++ hof/trust_path/HOF/Class/Yaml.php
@@ -8,7 +8,9 @@
 class HOF_Class_Yaml extends Symfony_Component_Yaml_Yaml
 {
 
-	function load($file, $enablePhpParsing = false)
+	const INLINE = 6;
+
+	public static function load($file, $enablePhpParsing = false)
 	{
 		HOF_Class_Yaml::$enablePhpParsing = $enablePhpParsing;
 
@@ -26,9 +28,14 @@ class HOF_Class_Yaml extends Symfony_Component_Yaml_Yaml
 		return $yaml;
 	}
 
-	function save($file, $data)
+	public static function save($file, $data, $inline = HOF_Class_Yaml::INLINE)
 	{
-		return file_put_contents($file, self::dump($data), LOCK_EX);
+		return file_put_contents($file, self::dump($data, $inline), LOCK_EX);
+	}
+
+	public static function dump($array, $inline = HOF_Class_Yaml::INLINE)
+	{
+		return parent::dump($array, $inline);
 	}
 
 }
