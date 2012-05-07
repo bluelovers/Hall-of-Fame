bb2ae67e015cc52bf711bf1de30f5c3abdcd707f
bluelovers <codelovers@users.sourceforge.net>
2012-05-07 15:19:31 +0800

    remove include (Step: 7)
    Step: 7
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/admin/gomi/loglog.php

--- hof/trust_path/admin/gomi/loglog.php
+++ hof/trust_path/admin/gomi/loglog.php
@@ -7,7 +7,7 @@
 <body>
 <?php
 
-include ("../trust_path/bootstrap.php");
+include_once ("../trust_path/bootstrap.php");
 $admin = "../"; // index.php からの距離
 $users = glob($admin . USER . "/*");
 print ("Users : " . count($users) . " / " . MAX_USERS . "<br />\n");
