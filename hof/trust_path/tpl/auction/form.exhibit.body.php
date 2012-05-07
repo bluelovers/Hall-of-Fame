3a936960e00c997a22b53913eb243eb1f055b7f6
bluelovers <codelovers@users.sourceforge.net>
2012-05-07 14:32:05 +0800

    HOF_Controller_Auction (Step: 3)
    Step: 3
    
    Commit ChangedLog:
    
    [A ] new file:
    
    	hof/trust_path/tpl/auction/form.exhibit.body.php

---
+++ hof/trust_path/tpl/auction/form.exhibit.body.php
@@ -0,0 +1,50 @@
+
+	<?php e($this->slot('auction/form.exhibit.header')); ?>
+
+	<?php if (empty($this->output->select)): ?>
+
+		<?php HOF_Helper_Global::ShowError($this->get('error', 'No items')) ?>
+
+	<?php else: ?>
+
+		<?php e($this->output->select) ?>
+
+		<form action="?menu=auction" method="post">
+				<div id="list">
+					<?php e($this->output->show) ?>
+				</div>
+				<table>
+					<tr>
+						<td style="text-align:right">数量(Amount) :</td>
+						<td><input type="text" name="Amount" class="text" style="width:60px" value="1" />
+							<br /></td>
+					</tr>
+					<tr>
+						<td style="text-align:right">時間(Time) :</td>
+						<td><select name="ExhibitTime">
+								<option value="24" selected>24 hour</option>
+								<option value="18">18 hour</option>
+								<option value="12">12 hour</option>
+								<option value="6">6 hour</option>
+								<option value="3">3 hour</option>
+								<option value="1">1 hour</option>
+							</select></td>
+					</tr>
+					<tr>
+						<td>開始価格(Start Price) :</td>
+						<td><input type="text" name="StartPrice" class="text" style="width:240px" maxlength="10">
+							<br /></td>
+					</tr>
+					<tr>
+						<td style="text-align:right">コメント(Comment) :</td>
+						<td><input type="text" name="Comment" class="text" style="width:240px" maxlength="40"></td>
+					</tr>
+					<tr>
+						<td></td>
+						<td><input type="submit" class="btn" value="Put Auction" name="PutAuction" style="width:240px"/>
+							<input type="hidden" name="PutAuction" value="1"></td>
+					</tr>
+				</table>
+			</form>
+
+	<?php endif; ?>
