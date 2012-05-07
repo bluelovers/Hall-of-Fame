499e4ec84bb077140799e43b20407f5592204c00
bluelovers <codelovers@users.sourceforge.net>
2012-05-08 05:51:28 +0800

    HOF_Controller_Game (Step: 1)
    Step: 1
    
    Commit ChangedLog:
    
    [A ] new file:
    
    	hof/trust_path/HOF/Controller/Game.php

---
+++ hof/trust_path/HOF/Controller/Game.php
@@ -0,0 +1,584 @@
+<?php
+
+/**
+ * @author bluelovers
+ * @copyright 2012
+ */
+
+class HOF_Controller_Game extends HOF_Class_Controller
+{
+
+	/**
+	 * @var HOF_Class_User
+	 */
+	var $user;
+
+	protected $_cache;
+
+	function _init()
+	{
+		$this->user = &HOF_Model_Main::getInstance();
+	}
+
+	function _input()
+	{
+
+		$this->input->make = HOF::$input->post->Make;
+		$this->input->newid = trim(HOF::$input->post->Newid);
+		$this->input->pass1 = trim(HOF::$input->post->pass1);
+		$this->input->pass2 = trim(HOF::$input->post->pass2);
+
+	}
+
+	function _main_before()
+	{
+		$this->user->fpCloseAll();
+
+		$this->_input();
+	}
+
+	function _main_action_default($message = null)
+	{
+		$this->_main_action_login($message);
+	}
+
+	/**
+	 * 初回ログイン用のフォーム
+	 */
+	function _main_action_first_login()
+	{
+		if (!$this->FirstLogin())
+		{
+			$this->_stop = true;
+		}
+	}
+
+	function _main_action_login($message = null)
+	{
+		$this->LoginForm($message);
+	}
+
+	/**
+	 * 新規ID作成用のフォーム
+	 */
+	function _main_action_newgame()
+	{
+		$this->output->newid = $this->input->newid;
+
+		/**
+		 * 登録者数が限界の場合
+		 */
+		if (MAX_USERS <= count(game_core::glob(USER)))
+		{
+			$this->_error('Maximum users.<br />登録者数が限界に達してしまった様です。');
+
+			/**
+			 * 登録者数が限界の場合
+			 */
+			$this->output->user_full = true;
+		}
+		elseif ($this->input->make)
+		{
+			list($bool, $message) = $this->MakeNewData();
+
+			if ($message) $this->_error($message);
+
+			if (true === $bool)
+			{
+				$this->_main_action_login($message);
+				return false;
+			}
+		}
+	}
+
+	function _error($s, $a = null)
+	{
+		$this->output->error[] = array($s, $a);
+		$this->error[] = $s;
+	}
+
+	/**
+	 * $id が過去登録されたかどうか
+	 */
+	function is_registered($id)
+	{
+		if ($registered = @file(REGISTER))
+		{
+			// 改行記号必須
+			if (array_search($id . "\n", $registered) !== false && !ereg("[\.\/]+", $id))
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+
+	/**
+	 * 入力された情報が型にはまるか判定
+	 * → 新規データを作成。
+	 */
+	function MakeNewData()
+	{
+		if (empty($this->input->newid)) return array(false, "Enter ID.");
+
+		// 正規表現
+		if (!ereg("[0-9a-zA-Z]{4,16}", $this->input->newid) || ereg("[^0-9a-zA-Z]+", $this->input->newid)) return array(false, "Bad ID");
+
+		if (strlen($this->input->newid) < 4 || 16 < strlen($this->input->newid)) //文字制限
+ 				return array(false, "Bad ID");
+
+		if ($this->is_registered($this->input->newid)) return array(false, "This ID has been already used.");
+
+		$file = USER . $this->input->newid . "/" . DATA . '.yml';
+		// PASS
+		//if(isset($this->input->pass1))
+		//	trim($this->input->pass1);
+		if (empty($this->input->pass1) || empty($this->input->pass2)) return array(false, "Enter both Password.");
+
+		if (!ereg("[0-9a-zA-Z]{4,16}", $this->input->pass1) || ereg("[^0-9a-zA-Z]+", $this->input->pass1)) return array(false, "Bad Password 1");
+		if (strlen($this->input->pass1) < 4 || 16 < strlen($this->input->pass1)) //文字制限
+ 				return array(false, "Bad Password 1");
+		if (!ereg("[0-9a-zA-Z]{4,16}", $this->input->pass2) || ereg("[^0-9a-zA-Z]+", $this->input->pass2)) return array(false, "Bad Password 2");
+		if (strlen($this->input->pass2) < 4 || 16 < strlen($this->input->pass2)) //文字制限
+ 				return array(false, "Bad Password 2");
+
+		if ($this->input->pass1 !== $this->input->pass2) return array(false, "Password dismatch.");
+
+		$pass = $this->user->CryptPassword($this->input->pass1);
+		// MAKE
+		if (!file_exists($file))
+		{
+			mkdir(USER . $this->input->newid, 0705);
+			$this->RecordRegister($this->input->newid); //ID記録
+
+			/*
+			$fp = fopen("$file", "w");
+			flock($fp, LOCK_EX);
+			$now = time();
+			fputs($fp, "id=$_POST[Newid]\n");
+			fputs($fp, "pass=$pass\n");
+			fputs($fp, "last=" . $now . "\n");
+			fputs($fp, "login=" . $now . "\n");
+			fputs($fp, "start=" . $now . substr(microtime(), 2, 6) . "\n");
+			fputs($fp, "money=" . START_MONEY . "\n");
+			fputs($fp, "time=" . START_TIME . "\n");
+			fputs($fp, "record_btl_log=1\n");
+			fclose($fp);
+			*/
+
+			$data = array(
+				'id' => $this->input->newid,
+				'pass' => $pass,
+				'last' => $now,
+				'login' => $now,
+				'start' => $now . substr(microtime(), 2, 6),
+				'money' => START_MONEY,
+				'time' => START_TIME,
+				'record_btl_log' => 1,
+				);
+
+			HOF_Class_Yaml::save($file, $data);
+
+			//print("ID:$_POST[Newid] success.<BR>");
+			$_SESSION["id"] = $this->input->newid;
+			setcookie("NO", session_id(), time() + COOKIE_EXPIRE);
+			$success = "<div class=\"recover\">ID : {$this->input->newid} success. Try Login</div>";
+			return array(true, $success); //強引...
+		}
+	}
+
+	/**
+	 * $id を登録済みidとして記録する
+	 */
+	function RecordRegister($id)
+	{
+		$fp = fopen(REGISTER, "a");
+		flock($fp, 2);
+		fputs($fp, "$id\n");
+		fclose($fp);
+	}
+
+	/**
+	 * ログイン用のフォーム
+	 */
+	function LoginForm($message = NULL)
+	{
+
+
+?>
+<div style="width:730px;">
+	<!-- ログイン -->
+	<div style="width:350px;float:right">
+		<h4 style="width:350px">Login</h4>
+		<?=
+
+		$message
+
+
+?>
+		<form action="<?=
+
+		INDEX
+
+
+?>" method="post" style="padding-left:20px">
+			<table>
+				<tbody>
+					<tr>
+						<td><div style="text-align:right">
+								ID:
+							</div></td>
+						<td><input type="text" maxlength="16" class="text" name="id" style="width:160px"<?=
+
+		$_SESSION["id"] ? " value=\"$_SESSION[id]\"" : NULL
+
+
+?>></td>
+					</tr>
+					<tr>
+						<td><div style="text-align:right">
+								PASS:
+							</div></td>
+						<td><input type="password" maxlength="16" class="text" name="pass" style="width:160px"></td>
+					</tr>
+					<tr>
+						<td></td>
+						<td><input type="submit" class="btn" name="Login" value="login" style="width:80px">
+							&nbsp;<a href="?newgame">NewGame?</a></td>
+					</tr>
+				</tbody>
+			</table>
+		</form>
+		<h4 style="width:350px">Ranking</h4>
+		<?php
+
+		include_once (CLASS_RANKING);
+		$Rank = new Ranking();
+		$Rank->ShowRanking(0, 4);
+
+
+?>
+	</div>
+	<!-- 飾 -->
+	<div style="width:350px;padding:15px;float:left;">
+		<div style="width:350px;text-align:center;height: 199px;overflow: hidden; margin-bottom: 20px;">
+			<img src="<?php
+
+		echo HOF_Class_Icon::getImageUrl("hof02", './static/image/');
+
+
+?>" style="margin-top: -1px;margin-left: -70px;" />
+		</div>
+		<div style="margin-left:20px">
+			<div class="u">
+				これってどんなゲーム?
+			</div>
+			<ul>
+				<li>
+					ゲームの目的はランキング1位になり、<br />
+					1位を守る事です。
+				</li>
+				<li>
+					冒険要素はないですが、<br />
+					ちょっと深い戦闘システムが売りです。
+				</li>
+			</ul>
+			<div class="u">
+				戦闘はどんな感じ?
+			</div>
+			<ul>
+				<li>
+					5人のキャラクターでパーティーを編成。
+				</li>
+				<li>
+					各キャラが行動パターンを持ち、<br />
+					戦闘の状況に応じて技を使い分けます。
+				</li>
+				<li>
+					<a href="?log" class="a0">こちら</a>で戦闘ログが回覧できます。
+				</li>
+			</ul>
+		</div>
+	</div>
+	<div class="c-both">
+	</div>
+</div>
+
+<!-- -------------------------------------------------------- -->
+
+<div style="margin:15px">
+<h4>info.</h4>
+Users :
+<?=
+
+		UserAmount()
+
+
+?>
+/
+<?=
+
+		MAX_USERS
+
+
+?>
+<br />
+<?php
+
+		$Abandon = ABANDONED;
+		print (floor($Abandon / (60 * 60 * 24)) . "日データに変化無しでデータ消える。");
+		print ("</div>\n");
+	}
+
+	/**
+	 * 初回ログイン用のフォーム
+	 */
+	function FirstLogin()
+	{
+		// 返値:設定済み=false / 非設定=true
+		if ($this->user->name) return false;
+
+		do
+		{
+			if (!$_POST["Done"]) break;
+			if (is_numeric(strpos($_POST["name"], "\t")))
+			{
+				$error = 'error1';
+				break;
+			}
+			if (is_numeric(strpos($_POST["name"], "\n")))
+			{
+				$error = 'error';
+				break;
+			}
+			$_POST["name"] = trim($_POST["name"]);
+			$_POST["name"] = stripslashes($_POST["name"]);
+			if (!$_POST["name"])
+			{
+				$error = 'Name is blank.';
+				break;
+			}
+			$length = strlen($_POST["name"]);
+			if (0 == $length || 16 < $length)
+			{
+				$error = '1 to 16 letters?';
+				break;
+			}
+			$userName = userNameLoad();
+			if (in_array($_POST["name"], $userName))
+			{
+				$error = 'その名前は使用されています。';
+				break;
+			}
+			// 最初のキャラの名前
+			$_POST["first_name"] = trim($_POST["first_name"]);
+			$_POST["first_name"] = stripslashes($_POST["first_name"]);
+			if (is_numeric(strpos($_POST["first_name"], "\t")))
+			{
+				$error = 'error';
+				break;
+			}
+			if (is_numeric(strpos($_POST["first_name"], "\n")))
+			{
+				$error = 'error';
+				break;
+			}
+			if (!$_POST["first_name"])
+			{
+				$error = 'Character name is blank.';
+				break;
+			}
+			$length = strlen($_POST["first_name"]);
+			if (0 == $length || 16 < $length)
+			{
+				$error = '1 to 16 letters?';
+				break;
+			}
+			if (!$_POST["fjob"])
+			{
+				$error = 'Select characters job.';
+				break;
+			}
+			$_POST["name"] = htmlspecialchars($_POST["name"], ENT_QUOTES);
+			$_POST["first_name"] = htmlspecialchars($_POST["first_name"], ENT_QUOTES);
+
+			$this->user->name = $_POST["name"];
+			userNameAdd($this->user->name);
+			$this->user->SaveData();
+			switch ($_POST["fjob"])
+			{
+				case "1":
+					$job = 1;
+					$gend = 0;
+					break;
+				case "2":
+					$job = 1;
+					$gend = 1;
+					break;
+				case "3":
+					$job = 2;
+					$gend = 0;
+					break;
+				default:
+					$job = 2;
+					$gend = 1;
+			}
+
+			/*
+			include (DATA_BASE_CHAR);
+			$char = new HOF_Class_Char();
+			$char->SetCharData(array_merge(BaseCharStatus($job), array("name" => $_POST[first_name], "gender" => "$gend")));
+			$char->SaveCharData($this->id);
+			*/
+
+			$char = HOF_Model_Char::newBaseChar($job, array("name" => $_POST[first_name], "gender" => $gend));
+			$char->SaveCharData($this->user->id);
+
+			return false;
+		} while (0);
+
+		/*
+		include (DATA_BASE_CHAR);
+		$war_male = new HOF_Class_Char();
+		$war_male->SetCharData(array_merge(BaseCharStatus("1"), array("gender" => "0")));
+		$war_female = new HOF_Class_Char();
+		$war_female->SetCharData(array_merge(BaseCharStatus("1"), array("gender" => "1")));
+		$sor_male = new HOF_Class_Char();
+		$sor_male->SetCharData(array_merge(BaseCharStatus("2"), array("gender" => "0")));
+		$sor_female = new HOF_Class_Char();
+		$sor_female->SetCharData(array_merge(BaseCharStatus("2"), array("gender" => "1")));
+		*/
+
+		// bluelovers
+		$war_male = HOF_Model_Char::newBaseChar(1, array("gender" => 0));
+		$war_female = HOF_Model_Char::newBaseChar(1, array("gender" => 1));
+
+		$sor_male = HOF_Model_Char::newBaseChar(2, array("gender" => 0));
+		$sor_female = HOF_Model_Char::newBaseChar(2, array("gender" => 1));
+		// bluelovers
+
+
+
+?>
+<form action="<?=
+
+		INDEX
+
+
+?>" method="post" style="margin:15px">
+	<?php
+
+		HOF_Helper_Global::ShowError($error);
+
+
+?>
+	<h4>Name of Team</h4>
+	<p>Decide the Name of the team.<br />
+		It should be more than 1 and less than 16 letters.<br />
+		Japanese characters count as 2 letters.</p>
+	<p>1-16文字でチームの名前決めてください。<br />
+		日本語でもOK。<br />
+		日本語は 1文字 = 2 letter</p>
+	<div class="bold u">
+		TeamName
+	</div>
+	<input class="text" style="width:160px" maxlength="16" name="name"<?
+
+		print ($_POST["name"] ? "value=\"$_POST[name]\"" : "")
+
+
+?>>
+	<h4>First Character</h4>
+	<p>Decide the name of Your First Charactor.<br>
+		more than 1 and less than 16 letters.</p>
+	<p>初期キャラの名前。</p>
+	<div class="bold u">
+		CharacterName
+	</div>
+	<input class="text" type="text" name="first_name" maxlength="16" style="width:160px;margin-bottom:10px">
+	<table cellspacing="0" style="width:400px">
+		<tbody>
+			<tr>
+				<td class="td1" valign="bottom"><div style="text-align:center">
+						<?=
+
+		$war_male->ShowImage()
+
+
+?>
+						<br>
+						<input type="radio" name="fjob" value="1" style="margin:3px">
+					</div></td>
+				<td class="td1" valign="bottom"><div style="text-align:center">
+						<?=
+
+		$war_female->ShowImage()
+
+
+?>
+						<br>
+						<input type="radio" name="fjob" value="2" style="margin:3px">
+					</div></td>
+				<td class="td1" valign="bottom"><div style="text-align:center">
+						<?=
+
+		$sor_male->ShowImage()
+
+
+?>
+						<br>
+						<input type="radio" name="fjob" value="3" style="margin:3px">
+					</div></td>
+				<td class="td1" valign="bottom"><div style="text-align:center">
+						<?=
+
+		$sor_female->ShowImage()
+
+
+?>
+						<br>
+						<input type="radio" name="fjob" value="4" style="margin:3px">
+					</div></td>
+			</tr>
+			<tr>
+				<td class="td2"><div style="text-align:center">
+						male
+					</div></td>
+				<td class="td3"><div style="text-align:center">
+						female
+					</div></td>
+				<td class="td2"><div style="text-align:center">
+						male
+					</div></td>
+				<td class="td3"><div style="text-align:center">
+						female
+					</div></td>
+			</tr>
+			<tr>
+				<td colspan="2" class="td4"><div style="text-align:center">
+						Warrior
+					</div></td>
+				<td colspan="2" class="td4"><div style="text-align:center">
+						Socerer
+					</div></td>
+			</tr>
+		</tbody>
+	</table>
+	<p>Choose your first character's job &amp; Gender.</p>
+	<p>最初のキャラの職と性別</p>
+	<input class="btn" style="width:160px" type="submit" value="Done" name="Done">
+	<input type="hidden" value="1" name="Done">
+	<input class="btn" style="width:160px" type="submit" value="logout" name="logout">
+</form>
+<?php
+
+		return true;
+	}
+
+}
+
+
+?>
