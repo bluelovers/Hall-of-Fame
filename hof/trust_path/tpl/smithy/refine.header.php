7393a10bb9fd277473f1e530c3eeef4495a0209d
bluelovers <codelovers@users.sourceforge.net>
2012-05-07 17:10:24 +0800

    HOF_Controller_Smithy (Step: 8)
    Step: 8
    
    Commit ChangedLog:
    
    [A ] new file:
    
    	hof/trust_path/tpl/smithy/refine.header.php

---
+++ hof/trust_path/tpl/smithy/refine.header.php
@@ -0,0 +1,21 @@
+
+
+	<!-- 鍛冶屋 精錬 ヘッダ -->
+
+	<div style="margin:15px">
+		<h4>鍛冶屋(Smithy)<a name="sm"></a></h4>
+		<div style="width:600px">
+			<div style="float:left;width:80px;">
+				<img src="<?php e(HOF_Class_Icon::getImageUrl("mon_053r", IMG_CHAR)) ?>" />
+			</div>
+			<div style="float:right;width:520px;">
+				ここでは&nbsp;アイテムの精錬ができるぜ！<br />
+				精錬する物と精錬回数を選んでくれ。<br />
+				ただし壊れても責任は持てないぜ。<br />
+				弟がやってる<span class="bold">製作工房</span>は<a href="?menu=create">アッチ</a>だ。
+			</div>
+			<div style="clear:both">
+			</div>
+		</div>
+		<h4>アイテムの精錬<a name="refine"></a></h4>
+		<div style="margin:0 15px">
