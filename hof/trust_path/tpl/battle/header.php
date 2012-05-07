0e0db6cc4f080f028f560228f219d2f33d172e96
bluelovers <codelovers@users.sourceforge.net>
2012-05-08 02:52:25 +0800

    HOF_Class_Char_View::ShowCharacters (Step: 7)
    Step: 7
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/tpl/battle/header.php

--- hof/trust_path/tpl/battle/header.php
+++ hof/trust_path/tpl/battle/header.php
@@ -1,4 +1,8 @@
 
-	<?php foreach((array)$this->output->error as $e): ?>
-		<?php HOF_Helper_Global::ShowError($e[0], $e[1]); ?>
-	<?php endforeach; ?>
+	<div style="margin:15px">
+		<h4><?php e($this->output->land['name']) ?></h4>
+
+			<?php foreach((array)$this->output->error as $e): ?>
+				<?php HOF_Helper_Global::ShowError($e[0], $e[1]); ?>
+			<?php endforeach; ?>
+	</div>
