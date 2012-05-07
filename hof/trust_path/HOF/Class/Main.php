8f9651b45a053ac74c0081778261c9e35a5860e3
bluelovers <codelovers@users.sourceforge.net>
2012-05-07 14:32:14 +0800

    HOF_Controller_Auction (Step: 11)
    Step: 11
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/HOF/Class/Main.php

--- hof/trust_path/HOF/Class/Main.php
+++ hof/trust_path/HOF/Class/Main.php
@@ -10,5 +10,30 @@ include_once (CLASS_MAIN);
 class HOF_Class_Main extends main
 {
 
+	protected static $instance;
+
+	function __construct()
+	{
+		if (!isset(self::$instance))
+		{
+			self::$instance = $this;
+		}
+		else
+		{
+			die('error!!');
+		}
+
+		parent::__construct();
+	}
+
+	public static function &getInstance()
+	{
+		if (!isset(self::$instance))
+		{
+			self::$instance = new self();
+		}
+
+		return self::$instance;
+	}
 
 }
