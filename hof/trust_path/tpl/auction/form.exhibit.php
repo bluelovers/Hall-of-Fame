1273ae097149a4d82389a62eb8d62473b282c77d
bluelovers <codelovers@users.sourceforge.net>
2012-05-07 15:19:48 +0800

    remove include (Step: 21)
    Step: 21
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/tpl/auction/form.exhibit.php

--- hof/trust_path/tpl/auction/form.exhibit.php
+++ hof/trust_path/tpl/auction/form.exhibit.php
@@ -2,3 +2,5 @@
 	<form action="?menu=auction" method="post">
 		<input type="submit" value="Put Auction" name="ExhibitItemForm" class="btn" style="width:160px">
 	</form>
+
+	<a href="?menu=auction">更新</a>
