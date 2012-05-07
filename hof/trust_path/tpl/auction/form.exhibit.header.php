d088f324657c0bb1175a5f9ba1c7eac08b8398c5
bluelovers <codelovers@users.sourceforge.net>
2012-05-07 14:32:06 +0800

    HOF_Controller_Auction (Step: 4)
    Step: 4
    
    Commit ChangedLog:
    
    [A ] new file:
    
    	hof/trust_path/tpl/auction/form.exhibit.header.php

---
+++ hof/trust_path/tpl/auction/form.exhibit.header.php
@@ -0,0 +1,49 @@
+
+		<div class="u bold">
+			出品方法
+		</div>
+
+		<ol>
+			<li>
+				出品するアイテムを選択します。
+			</li>
+			<li>
+				2個以上出品する場合、数量を入力します。
+			</li>
+			<li>
+				出品している時間の長さを指定します。
+			</li>
+			<li>
+				開始価格を指定します(記入無し = 0)
+			</li>
+			<li>
+				コメントがあれば入力します。
+			</li>
+			<li>
+				送信する。
+			</li>
+		</ol>
+
+		<div class="u bold">
+			注意事項
+		</div>
+
+		<ul>
+			<li>
+				出品には&nbsp;手数料として$500&nbsp;必要です。
+			</li>
+			<li>
+				ちゃんとうごいてくれなさそう
+			</li>
+		</ul>
+
+		<a href="?menu=auction">一覧に戻る</a>
+	</div>
+
+	<h4>出品する</h4>
+
+	<div style="margin-left:20px">
+
+	<div class="u bold">
+		出品可能な物一覧
+	</div>
