a77344ccdead1d81d8b9fe1f3f042028948fb8e4
bluelovers <codelovers@users.sourceforge.net>
2012-05-08 00:42:36 +0800

    . (Step: 5)
    Step: 5
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/HOF/Syntax/syntax.func.php

--- hof/trust_path/HOF/Syntax/syntax.func.php
+++ hof/trust_path/HOF/Syntax/syntax.func.php
@@ -9,7 +9,15 @@ function e($string, $escape = false)
 {
 	$string = $escape ? HOF::escapeHtml($string) : $string;
 
-	if (is_array($string))
+	if (is_object($string) && method_exists($string, 'toArray'))
+	{
+		print_r($string->toArray());
+	}
+	elseif (is_object($string) && method_exists($string, '__toString'))
+	{
+		$string = (string)$string;
+	}
+	elseif (is_array($string))
 	{
 		print_r($string);
 	}
