145c2a652bcd8e3b2f7b5edd22ff716b711f463d
bluelovers <codelovers@users.sourceforge.net>
2012-05-07 18:41:02 +0800

    HOF_Controller_Shop (Step: 6)
    Step: 6
    
    Commit ChangedLog:
    
    [A ] new file:
    
    	hof/trust_path/tpl/shop/header.php

---
+++ hof/trust_path/tpl/shop/header.php
@@ -0,0 +1,18 @@
+
+	<!-- 店ヘッダ -->
+
+	<div style="margin:15px">
+		<h4>店</h4>
+		<div style="width:600px">
+			<div style="float:left;width:50px;">
+				<img src="<?php e(HOF_Class_Icon::getImageUrl('ori_002', HOF_Class_Icon::IMG_CHAR)); ?>" />
+			</div>
+			<div style="float:right;width:550px;">
+				いらっしゃいませー<br />
+				<a href="?menu=buy">買う</a>/<a href="?menu=sell">売る</a><br />
+				<a href="?menu=work">アルバイト</a>
+			</div>
+			<div style="clear:both">
+			</div>
+		</div>
+	</div>
