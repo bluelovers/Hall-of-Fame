1ea291c0ffd23c934001bdd2b62a1f70484e4f10
bluelovers <codelovers@users.sourceforge.net>
2012-05-07 14:32:18 +0800

    HOF_Controller_Auction (Step: 14)
    Step: 14
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/HOF/Helper/Global.php

--- hof/trust_path/HOF/Helper/Global.php
+++ hof/trust_path/HOF/Helper/Global.php
@@ -16,4 +16,26 @@ class HOF_Helper_Global
 		return $pre . number_format($number);
 	}
 
+	/**
+	 * 赤い警告文でエラー表示
+	 */
+	function ShowError($message, $add = false)
+	{
+		if ($add) $add = " " . $add;
+
+		if (is_object($message) && method_exists($message, '__toString'))
+		{
+			$message = (string)$message;
+		}
+		elseif (is_array($message))
+		{
+			$message = implode('<p>', $message);
+		}
+
+		if ($message)
+		{
+			print ('<div class="error' . $add . '">' . $message . '</div>' . "\n");
+		}
+	}
+
 }
