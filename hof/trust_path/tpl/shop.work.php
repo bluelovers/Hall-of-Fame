920e221d70ac622454d0bfe263aa583fa5043c29
bluelovers <codelovers@users.sourceforge.net>
2012-05-07 18:41:00 +0800

    HOF_Controller_Shop (Step: 4)
    Step: 4
    
    Commit ChangedLog:
    
    [A ] new file:
    
    	hof/trust_path/tpl/shop.work.php

---
+++ hof/trust_path/tpl/shop.work.php
@@ -0,0 +1,27 @@
+
+	<?php $this->extend('shop/layout'); ?>
+
+	<?php $this->callMethod('_work'); ?>
+
+	<div style="margin:15px">
+		<h4>アルバイトする！</h4>
+		<form method="post" action="?menu=work">
+			<p>1回 100Time<br />
+				給与 :
+				<?php e(HOF_Helper_Global::MoneyFormat($this->output->work_each_pay)) ?>/回</p>
+			<select name="amount">
+				<option value="1">1</option>
+				<option value="2">2</option>
+				<option value="3">3</option>
+				<option value="4">4</option>
+				<option value="5">5</option>
+				<option value="6">6</option>
+				<option value="7">7</option>
+				<option value="8">8</option>
+				<option value="9">9</option>
+				<option value="10">10</option>
+			</select>
+			<br />
+			<input type="submit" value="Work" class="btn" />
+		</form>
+	</div>
