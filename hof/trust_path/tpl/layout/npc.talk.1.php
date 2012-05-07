fe209bd657deb154c5f5988789b5c69b1948f7c9
bluelovers <codelovers@users.sourceforge.net>
2012-05-07 19:37:34 +0800

    . (Step: 1)
    Step: 1
    
    Commit ChangedLog:
    
    [A ] new file:
    
    	hof/trust_path/tpl/layout/npc.talk.1.php

---
+++ hof/trust_path/tpl/layout/npc.talk.1.php
@@ -0,0 +1,14 @@
+
+	<div style="margin:15px">
+		<h4><?php e($this->get('npc.talk.title')) ?></h4>
+		<div>
+			<div style="float:left; min-width:70px; margin-right: 1em; text-align: center;">
+				<img src="<?php e(HOF_Class_Icon::getImageUrl($this->get('npc.talk.no'), HOF_Class_Icon::IMG_CHAR)); ?>" />
+			</div>
+			<div style="max-width:550px;display: inline-block;">
+				<?php e($this->content) ?>
+			</div>
+			<div style="clear:both">
+			</div>
+		</div>
+	</div>
