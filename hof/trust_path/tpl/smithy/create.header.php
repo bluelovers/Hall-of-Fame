e71a2516b5d1130aee4a4a38989c0d4b486c13e4
bluelovers <codelovers@users.sourceforge.net>
2012-05-07 19:37:40 +0800

    . (Step: 6)
    Step: 6
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/tpl/smithy/create.header.php

--- hof/trust_path/tpl/smithy/create.header.php
+++ hof/trust_path/tpl/smithy/create.header.php
@@ -2,21 +2,18 @@
 
 	<!-- 鍛冶屋 製作 ヘッダ -->
 
-	<div style="margin:15px">
-		<h4>鍛冶屋(Smithy)<a name="sm"></a></h4>
-		<div style="width:600px">
-			<div style="float:left;width:80px;">
-				<img src="<?php e(HOF_Class_Icon::getImageUrl("mon_053rz", IMG_CHAR)) ?>" />
-			</div>
-			<div style="float:right;width:520px;">
+	<?php $this->set('npc.talk.title', '鍛冶屋(Smithy)<a name="sm"></a>') ?>
+	<?php $this->set('npc.talk.no', $this->get('npc_no', 'mon_053rz')) ?>
+	<?php ob_start(); ?>
 				ここでは&nbsp;アイテムの製作ができるぜ！<br />
 				お前さんが持ってる素材から作れそうな装備を作れるぜ。<br />
 				特別な素材を練り込めば特殊な武器も作れるぜ。<br />
 				兄がやってる<span class="bold">精錬工房</span>は<a href="?menu=refine">コッチ</a>だ。<br />
 				<a href="#mat">所持素材一覧</a>
-			</div>
-			<div style="clear:both">
-			</div>
-		</div>
+	<?php $content = ob_get_clean(); ?>
+
+	<?php e($this->slot('layout/npc.talk.1', $content)) ?>
+
+	<div style="margin:15px">
 		<h4>アイテムの製作<a name="refine"></a></h4>
 		<div style="margin:0 15px">
