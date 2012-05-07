8af3265882090c9f019da6e6370e294db373a98e
bluelovers <codelovers@users.sourceforge.net>
2012-05-07 21:49:08 +0800

    . (Step: 4)
    Step: 4
    
    Commit ChangedLog:
    
    [A ] new file:
    
    	hof/trust_path/tpl/recruit/header.php

---
+++ hof/trust_path/tpl/recruit/header.php
@@ -0,0 +1,14 @@
+
+	<!-- 店ヘッダ -->
+
+	<?php $this->set('npc.talk.title', '人材斡旋所(Recruit)') ?>
+	<?php $this->set('npc.talk.no', $this->get('npc_no', 'ori_002')) ?>
+	<?php ob_start(); ?>
+
+		<?php foreach($this->output->error as $e): ?>
+			<?php HOF_Helper_Global::ShowError($e[0], $e[1]); ?>
+		<?php endforeach; ?>
+
+	<?php $content = ob_get_clean(); ?>
+
+	<?php e($this->slot('layout/npc.talk.1', $content)) ?>
