94524921b47673dcb021b113f237e0b5f96e550e
bluelovers <codelovers@users.sourceforge.net>
2012-05-08 19:03:07 +0800

    . (Step: 2)
    Step: 2
    
    Commit ChangedLog:
    
    [A ] new file:
    
    	hof/trust_path/tpl/game/index.info.php

---
+++ hof/trust_path/tpl/game/index.info.php
@@ -0,0 +1,37 @@
+
+		<div style="width:350px;text-align:center;height: 199px;overflow: hidden; margin-bottom: 20px;">
+				<img src="<?php e(HOF_Class_Icon::getImageUrl("hof02", HOF_Class_Icon::IMG_IMAGE)) ?>" style="margin-top: -1px;margin-left: -70px;" />
+			</div>
+			<div style="margin-left:20px">
+				<div class="u">
+					これってどんなゲーム?
+				</div>
+				<ul>
+					<li>
+						ゲームの目的はランキング1位になり、
+						<br />
+						1位を守る事です。
+					</li>
+					<li>
+						冒険要素はないですが、
+						<br />
+						ちょっと深い戦闘システムが売りです。
+					</li>
+				</ul>
+				<div class="u">
+					戦闘はどんな感じ?
+				</div>
+				<ul>
+					<li>
+						5人のキャラクターでパーティーを編成。
+					</li>
+					<li>
+						各キャラが行動パターンを持ち、
+						<br />
+						戦闘の状況に応じて技を使い分けます。
+					</li>
+					<li>
+						<a href="?log" class="a0">こちら</a>で戦闘ログが回覧できます。
+					</li>
+				</ul>
+			</div>
