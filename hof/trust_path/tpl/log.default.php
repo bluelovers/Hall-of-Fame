7b68f3fa80e96ff681f8ab57c4a8e74b3cd55e65
bluelovers <codelovers@users.sourceforge.net>
2012-05-08 18:10:37 +0800

    log (Step: 7)
    Step: 7
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/tpl/log.default.php

--- hof/trust_path/tpl/log.default.php
+++ hof/trust_path/tpl/log.default.php
@@ -1,10 +1,7 @@
 
-	<div style="margin:15px">
+	<?php $this->extend('log/layout'); ?>
 
-		<a href="?log" <?php if ($this->output->idx == 'log') e(' class="a0"'); ?>>All</a>
-		<a href="?clog" <?php if ($this->output->idx == 'clog') e(' class="a0"'); ?>>Common</a>
-		<a href="?ulog" <?php if ($this->output->idx == 'ulog') e(' class="a0"'); ?>>Union</a>
-		<a href="?rlog" <?php if ($this->output->idx == 'rlog') e(' class="a0"'); ?>>Ranking</a>
+	<div style="margin:15px">
 
 		<!-- common -->
 
