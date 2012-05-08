d1be23c57d3d1068e8a89be912ed199654abc923
bluelovers <codelovers@users.sourceforge.net>
2012-05-09 05:19:09 +0800

    . (Step: 9)
    Step: 9
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/tpl/log.log.php

--- hof/trust_path/tpl/log.log.php
+++ hof/trust_path/tpl/log.log.php
@@ -19,14 +19,14 @@
 
 	</div>
 
-	<div style="padding:15px 0;width:100%;text-align:center" class="break">
-		<h2>battle log*</h2>
-		this battle starts at<br />
-		<?php e(gc_date("m/d H:i:s", substr($this->output->time, 0, 10))) ?>
-	</div>
+	<?php if ($this->output->log): ?>
 
-	<?php $row = 6; ?>
-	<?php while ($this->output->log[$row]): ?>
-		<?php e($this->output->log["$row"]); ?>
-		<?php $row++; ?>
-	<?php endwhile; ?>
+		<div style="padding:15px 0;width:100%;text-align:center" class="break">
+			<h2>battle log*</h2>
+			this battle starts at<br />
+			<?php e(gc_date("m/d H:i:s", substr($this->output->log['time'], 0, 10))) ?>
+		</div>
+
+		<?php e($this->output->log['contents']); ?>
+
+	<?php endif; ?>
