c9039dcf0d4927d76298976339604a0e842c35a4
bluelovers <codelovers@users.sourceforge.net>
2012-05-08 18:10:38 +0800

    log (Step: 8)
    Step: 8
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/tpl/log.log.php

--- hof/trust_path/tpl/log.log.php
+++ hof/trust_path/tpl/log.log.php
@@ -1,3 +1,24 @@
+
+	<?php $this->extend('log/layout'); ?>
+
+	<div style="margin:15px">
+
+		<h4>
+			<?php if ($this->output->idx == 'ulog'): ?>
+				ユニオン戦 - (Union Battle Log)
+			<?php elseif ($this->output->idx == 'rlog'): ?>
+				ランキング戦 - (Rank Battle Log)
+			<?php else: ?>
+				最近の戦闘 - (Recent Battles)
+			<?php endif; ?>
+
+			<?php if (!$this->output->full_log): ?>
+				<a href="?<?php e($this->output->idx) ?>">全表示</a>
+			<?php endif; ?>
+		</h4>
+
+	</div>
+
 	<div style="padding:15px 0;width:100%;text-align:center" class="break">
 		<h2>battle log*</h2>
 		this battle starts at<br />
