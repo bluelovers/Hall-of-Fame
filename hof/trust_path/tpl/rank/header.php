55eb8ab0ee5f9de6360dd85006beaad5eb925c9b
bluelovers <codelovers@users.sourceforge.net>
2012-05-08 21:53:09 +0800

    Revert ". (Step: 8)"
    This reverts commit 2d5358a5ea0215ab6d3c0c5d7179990440db179b.
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/tpl/rank/header.php

--- hof/trust_path/tpl/rank/header.php
+++ hof/trust_path/tpl/rank/header.php
@@ -3,7 +3,7 @@
 		<h4>コロシアム(Colosseum)</h4>
 
 		<?php foreach((array)$this->output->msg_result as $e): ?>
-			<?php HOF_Helper_Global::HOF_Helper_Global::ShowResult($e[0], $e[1]); ?>
+			<?php HOF_Helper_Global::ShowResult($e[0], $e[1]); ?>
 		<?php endforeach; ?>
 
 		<?php foreach((array)$this->output->error as $e): ?>
