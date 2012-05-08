ddafa35facdb97bc0d4cc2144039d1b22cc2018c
bluelovers <codelovers@users.sourceforge.net>
2012-05-08 21:47:59 +0800

    HOF_Controller_Rank (Step: 4)
    Step: 4
    
    Commit ChangedLog:
    
    [A ] new file:
    
    	hof/trust_path/tpl/rank/header.php

---
+++ hof/trust_path/tpl/rank/header.php
@@ -0,0 +1,15 @@
+
+	<div style="margin:15px">
+		<h4>コロシアム(Colosseum)</h4>
+
+		<?php foreach((array)$this->output->msg_result as $e): ?>
+			<?php HOF_Helper_Global::ShowResult($e[0], $e[1]); ?>
+		<?php endforeach; ?>
+
+		<?php foreach((array)$this->output->error as $e): ?>
+			<?php HOF_Helper_Global::ShowError($e[0], $e[1]); ?>
+		<?php endforeach; ?>
+
+	</div>
+
+
