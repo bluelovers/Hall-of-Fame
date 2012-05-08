db8e033f3e8e7d1887cba72466f30d8517d0642e
bluelovers <codelovers@users.sourceforge.net>
2012-05-08 19:03:06 +0800

    . (Step: 1)
    Step: 1
    
    Commit ChangedLog:
    
    [A ] new file:
    
    	hof/trust_path/tpl/game/index.footer.php

---
+++ hof/trust_path/tpl/game/index.footer.php
@@ -0,0 +1,10 @@
+
+	<!-- -------------------------------------------------------- -->
+
+	<div style="margin:15px">
+		<h4>info.</h4>
+		Users :
+		<?php e($this->output->game_users) ?> / <?php e($this->output->game_users_max) ?>
+		<br />
+		<?php e($this->output->game_abandon) ?> 日データに変化無しでデータ消える。
+	</div>
