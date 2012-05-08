d494dc8fef599bdd8a069438debe4f1eca6bf740
bluelovers <codelovers@users.sourceforge.net>
2012-05-08 19:03:13 +0800

    . (Step: 8)
    Step: 8
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/tpl/game.login.php

---
+++ hof/trust_path/tpl/game.login.php
@@ -0,0 +1,32 @@
+
+			<?php $this->extend('game/login.layout') ?>
+
+			<h4 style="width:350px">Login</h4>
+
+			<?php if ($this->output->message): ?>
+				<div class="recover margin15"><?php e($this->output->message) ?></div>
+			<?php endif; ?>
+
+			<form action="<?php e(INDEX) ?>" method="post" style="padding-left:20px">
+				<table>
+					<tbody>
+						<tr>
+							<td><div style="text-align:right">
+									ID:
+								</div></td>
+							<td><input type="text" maxlength="16" class="text" name="id" style="width:160px" value="<?php e($this->output->id) ?>"></td>
+						</tr>
+						<tr>
+							<td><div style="text-align:right">
+									PASS:
+								</div></td>
+							<td><input type="password" maxlength="16" class="text" name="pass" style="width:160px"></td>
+						</tr>
+						<tr>
+							<td></td>
+							<td><input type="submit" class="btn" name="Login" value="login" style="width:80px">
+								&nbsp;<a href="?newgame">NewGame?</a></td>
+						</tr>
+					</tbody>
+				</table>
+			</form>
