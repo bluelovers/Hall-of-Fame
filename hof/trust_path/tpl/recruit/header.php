5cab0628d613e3c177a1722e148d1eff4584bf1d
bluelovers <codelovers@users.sourceforge.net>
2012-05-08 21:48:09 +0800

    HOF_Controller_Rank (Step: 13)
    Step: 13
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/tpl/recruit/header.php

--- hof/trust_path/tpl/recruit/header.php
+++ hof/trust_path/tpl/recruit/header.php
@@ -5,7 +5,7 @@
 	<?php $this->set('npc.talk.no', $this->get('npc_no', 'ori_002')) ?>
 	<?php ob_start(); ?>
 
-		<?php foreach($this->output->error as $e): ?>
+		<?php foreach((array)$this->output->error as $e): ?>
 			<?php HOF_Helper_Global::ShowError($e[0], $e[1]); ?>
 		<?php endforeach; ?>
 
