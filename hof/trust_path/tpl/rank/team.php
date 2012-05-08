ee686b0578e62fd3aa18cc5a11a690ff0b41639e
bluelovers <codelovers@users.sourceforge.net>
2012-05-08 21:48:01 +0800

    HOF_Controller_Rank (Step: 6)
    Step: 6
    
    Commit ChangedLog:
    
    [A ] new file:
    
    	hof/trust_path/tpl/rank/team.php

---
+++ hof/trust_path/tpl/rank/team.php
@@ -0,0 +1,36 @@
+
+	<div style="margin:15px">
+		<h4>チーム設定(Team Setting)</h4>
+		<p>
+			ランキング戦用のチーム設定。
+			<br />
+			ここで設定したチームで戦います。
+		</p>
+	</div>
+
+	<form action="?menu=rank" method="post">
+
+		<?php e(HOF_Class_Char_View::ShowCharacters(HOF_Model_Main::getInstance()->char, "CHECKBOX", HOF_Model_Main::getInstance()->party_memo)); ?>
+
+		<div style="margin:15px;text-align:center;overflow: visible;">
+
+			<?php if ($this->output->left_setting): ?>
+				<div class="bold">
+					<?php e($this->output->left_hour) ?>Hour <?php e($this->output->left_min) ?>minutes left to set again.
+				</div>
+			<?php endif; ?>
+
+			<?php e($this->output->left_mes) ?>
+			<input type="submit" class="btn" style="width:160px" value="SetTeam"<?php e($this->output->disable) ?> />
+			<input type="hidden" name="SetRankTeam" value="1" />
+			<p>
+				設定後、
+				<?php e($this->output->reset) ?>
+				時間は変更できません。
+				<br />
+				Team setting disabled after
+				<?php e($this->output->reset) ?>
+				hours once set.
+			</p>
+		</div>
+	</form>
