c1001021d55b519e1edbebb1e48563818bff9f1b
bluelovers <codelovers@users.sourceforge.net>
2012-05-09 18:51:34 +0800

    . (Step: 1)
    Step: 1
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/HOF/Class/Array.php

--- hof/trust_path/HOF/Class/Array.php
+++ hof/trust_path/HOF/Class/Array.php
@@ -33,19 +33,63 @@ class HOF_Class_Array extends ArrayObject
 	 */
 	const ARRAY_PROP_BOTH = 3;
 
-	protected $_data_default_ = array();
+	//protected $_data_default_ = array();
 
-	function __construct($input = null)
+	/**
+	 *
+	 */
+	const ARRAY_RECURSIVE_ALL = 10;
+
+	static $ARRAYOBJECT = 'HOF_Class_Array';
+
+	function __construct($input = null, $deep = 0)
 	{
-		if ($input === null) $input = $this->_data_default_;
+		if ($input === null) $input = !empty($this->_data_default_) ? $this->_data_default_ : array();
 
-		$this->setFlags(ArrayObject::STD_PROP_LIST | ArrayObject::ARRAY_AS_PROPS);
+		$input = $this->_fixArrayRecursive($input, $deep);
+
+		$this->setFlags(self::ARRAY_PROP_BOTH);
 		$this->exchangeArray($input);
+	}
+
+	function _toArrayObjectRecursive($append, $loop = 1, $ARRAYOBJECT = null)
+	{
+		if (!$ARRAYOBJECT) $ARRAYOBJECT = self::$ARRAYOBJECT;
+
+		if (!($append instanceof $ARRAYOBJECT))
+		{
+			$append = new $ARRAYOBJECT($append);
+		}
+
+		if ($loop > 0 && is_array($append) && count($append) > 0)
+		{
+			foreach($append as $k => $v)
+			{
+				$append[$k] = self::_toArrayObjectRecursive($v, $loop - 1, $ARRAYOBJECT);
+			}
+		}
+
+		return $append;
 	}
 
 	function toArray()
 	{
 		return $this->getArrayCopy();
+	}
+
+	function _fixArrayRecursive($append, $loop = 1)
+	{
+		$append = self::_fixArray($append, 1);
+
+		if ($loop > 0 && is_array($append))
+		{
+			foreach($append as $k => $v)
+			{
+				$append[$k] = self::_fixArrayRecursive($v, $loop - 1);
+			}
+		}
+
+		return $append;
 	}
 
 	/**
