8ae221d48c3458911703e7a30cbc70eef02a6eac
bluelovers <codelovers@users.sourceforge.net>
2012-05-10 01:08:56 +0800

    . (Step: 2)
    Step: 2
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/tpl/rank/header.php

--- hof/trust_path/tpl/rank/header.php
+++ hof/trust_path/tpl/rank/header.php
@@ -1,15 +1,16 @@
 
-	<div style="margin:15px">
-		<h4>コロシアム(Colosseum)</h4>
+	<?php $this->set('npc.talk.title', 'コロシアム(Colosseum)') ?>
+	<?php $this->set('npc.talk.no', $this->get('npc_no', 'ori_002')) ?>
+	<?php ob_start(); ?>
+				<?php foreach((array)$this->output->msg_result as $e): ?>
+					<?php HOF_Helper_Global::ShowResult($e[0], $e[1]); ?>
+				<?php endforeach; ?>
 
-		<?php foreach((array)$this->output->msg_result as $e): ?>
-			<?php HOF_Helper_Global::ShowResult($e[0], $e[1]); ?>
-		<?php endforeach; ?>
+				<?php foreach((array)$this->output->error as $e): ?>
+					<?php HOF_Helper_Global::ShowError($e[0], $e[1]); ?>
+				<?php endforeach; ?>
+	<?php $content = ob_get_clean(); ?>
 
-		<?php foreach((array)$this->output->error as $e): ?>
-			<?php HOF_Helper_Global::ShowError($e[0], $e[1]); ?>
-		<?php endforeach; ?>
-
-	</div>
+	<?php e($this->slot('layout/npc.talk.1', $content)) ?>
 
 
