0e80f2c864c7c8baa328ee3e5d5cda2fa744f4ef
bluelovers <codelovers@users.sourceforge.net>
2012-05-10 01:18:09 +0800

    clean (Step: 1)
    Step: 1
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/HOF/Model/Main.php

--- hof/trust_path/HOF/Model/Main.php
+++ hof/trust_path/HOF/Model/Main.php
@@ -107,64 +107,28 @@ class HOF_Model_Main extends HOF_Class_Main
 		switch (true)
 		{
 			case ($_SERVER["QUERY_STRING"] === "update"):
-				/*
-				ShowUpDate();
-				*/
 				HOF_Class_Controller::newInstance('log', $_SERVER["QUERY_STRING"])->main();
 				return true;
 			case ($_SERVER["QUERY_STRING"] === "bbs"):
-				/*
-				$this->bbs01();
-				*/
 				HOF_Class_Controller::newInstance($_SERVER["QUERY_STRING"])->main();
 				return true;
 			case ($_SERVER["QUERY_STRING"] === "manual"):
 			case ($_SERVER["QUERY_STRING"] === "manual2"):
 			case ($_SERVER["QUERY_STRING"] === "tutorial"):
 				HOF_Class_Controller::newInstance('manual', $_SERVER["QUERY_STRING"])->main();
-				;
 				return true;
-
-				/*
-				case ($_SERVER["QUERY_STRING"] === "log"):
-				ShowLogList();
-				return true;
-				case ($_SERVER["QUERY_STRING"] === "clog"):
-				LogShowCommon();
-				return true;
-				case ($_SERVER["QUERY_STRING"] === "ulog"):
-				LogShowUnion();
-				return true;
-				case ($_SERVER["QUERY_STRING"] === "rlog"):
-				LogShowRanking();
-				return true;
-				case ($_GET["log"]):
-				HOF_Helper_Global::getLogBattle($_GET["log"]);
-				return true;
-				case ($_GET["ulog"]):
-				HOF_Helper_Global::getLogBattle($_GET["ulog"], "UNION");
-				return true;
-				case ($_GET["rlog"]):
-				HOF_Helper_Global::getLogBattle($_GET["rlog"], "RANK");
-				return true;
-				case ($_GET["gamedata"]):
-				ShowGameData();
-				return true;
-				*/
 			case ($_SERVER["QUERY_STRING"] === "log"):
 			case ($_SERVER["QUERY_STRING"] === "clog"):
 			case ($_SERVER["QUERY_STRING"] === "ulog"):
 			case ($_SERVER["QUERY_STRING"] === "rlog"):
 				HOF_Class_Controller::newInstance('log')->main();
 				return true;
-
 			case ($_GET["log"]):
 			case ($_GET["clog"]):
 			case ($_GET["ulog"]):
 			case ($_GET["rlog"]):
 				HOF_Class_Controller::newInstance('log', 'log')->main();
 				return true;
-
 			case ($_GET["gamedata"]):
 				HOF_Class_Controller::newInstance('gamedata', $_GET["gamedata"])->main();
 				return true;
