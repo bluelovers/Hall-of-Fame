65d0cf2dcbd24d776d0da06d593698a7db91a757
bluelovers <codelovers@users.sourceforge.net>
2012-05-09 14:53:22 +0800

    rewrite HOF_Class_Ranking (Step: 3)
    Step: 3
    
    Commit ChangedLog:
    
    [A ] new file:
    
    	hof/trust_path/tpl/rank.rank_all.php

---
+++ hof/trust_path/tpl/rank.rank_all.php
@@ -0,0 +1,8 @@
+
+	<?php $this->extend('rank/layout') ?>
+
+	<div style="margin:15px">
+		<h4>Ranking - <?php e(gc_date("Y年n月j日 G時i分s秒")) ?></h4>
+
+		<?php $this->callMethod('_ShowRanking', 1); ?>
+	</div>
