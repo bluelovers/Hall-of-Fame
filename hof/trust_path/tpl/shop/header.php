bb71b1a3c72a0a5709f3adb62d395b0ef2bb06c8
bluelovers <codelovers@users.sourceforge.net>
2012-05-11 07:49:12 +0800

    . (Step: 6)
    Step: 6
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/tpl/shop/header.php

--- hof/trust_path/tpl/shop/header.php
+++ hof/trust_path/tpl/shop/header.php
@@ -10,3 +10,48 @@
 	<?php $content = ob_get_clean(); ?>
 
 	<?php e($this->slot('layout/npc.talk.1', $content)) ?>
+
+<script type="text/javascript">
+(function($){
+
+	$(function(){
+		$('table[data-item="table"]')
+			.on('change', ':checkbox, :radio', function(){
+				var _this = $(this);
+
+				if (_this.prop('checked'))
+				{
+					_this.parents('tr:first').find('> td').addClass('tdToggleBg');
+				}
+				else
+				{
+					_this.parents('tr:first').find('> td').removeClass('tdToggleBg');
+				}
+
+			})
+			.find('td[data-no]')
+				.on('click', function(event){
+
+					var _this = $(this);
+
+					if (!$(event.target).is(':input'))
+					{
+
+						$(':checkbox[data-no="' + _this.attr('data-no') + '"]')
+							.prop('checked', function(idx, old){
+								return !old;
+							})
+							.trigger('change')
+						;
+
+					}
+
+				})
+			.end()
+			.find(':checkbox')
+				.trigger('change')
+		;
+	});
+
+})(jQuery);
+</script>
