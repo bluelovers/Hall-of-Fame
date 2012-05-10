a35d9ff5420365e53de1caa444f24abdc533a68a
bluelovers <codelovers@users.sourceforge.net>
2012-05-10 22:01:55 +0800

    . (Step: 4)
    Step: 4
    
    Commit ChangedLog:
    
    [A ] new file:
    
    	hof/trust_path/tpl/layout/layout.header.php

---
+++ hof/trust_path/tpl/layout/layout.header.php
@@ -0,0 +1,20 @@
+
+	<title><?= TITLE ?></title>
+
+			<link rel="stylesheet" href="./static/style/basis.css" type="text/css">
+			<link rel="stylesheet" href="./static/style/style.css" type="text/css">
+			<script type="text/javascript" src="http://code.jquery.com/jquery-latest.pack.js"></script>
+			<script type="text/javascript" src="./static/js/jquery-core.js"></script>
+			<style>
+
+.flip-h {
+    -moz-transform: scaleX(-1);
+    -o-transform: scaleX(-1);
+    -webkit-transform: scaleX(-1);
+    transform: scaleX(-1);
+    filter: FlipH;
+    -ms-filter: "FlipH";
+}
+
+</style>
+
