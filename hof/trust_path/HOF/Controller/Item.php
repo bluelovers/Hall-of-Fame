26745f7d83293e6b2bfb2f9421dd7c59a4dcbed3
bluelovers <codelovers@users.sourceforge.net>
2012-05-10 19:53:22 +0800

    . (Step: 1)
    Step: 1
    
    Commit ChangedLog:
    
    [A ] new file:
    
    	hof/trust_path/HOF/Controller/Item.php

---
+++ hof/trust_path/HOF/Controller/Item.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * @author bluelovers
+ * @copyright 2012
+ */
+
+class HOF_Controller_Item extends HOF_Class_Controller
+{
+
+	/**
+	 * @var HOF_Class_User
+	 */
+	var $user;
+
+	function _main_init()
+	{
+		$this->user = &HOF_Model_Main::getInstance();
+	}
+
+	function _main_action_default()
+	{
+		$this->user->LoadUserItem();
+	}
+
+	function _main_after()
+	{
+		parent::_main_view();
+
+		$this->user->fpCloseAll();
+	}
+
+	function _ItemShow()
+	{
+		if ($this->user->item)
+		{
+			$goods = new HOF_Class_Item_Style_List();
+			$goods->SetID("my");
+			$goods->SetName("type");
+			// JSを使用しない。
+			if ($this->no_JS_itemlist) $goods->NoJS();
+			//$goods->ListTable("<table>");
+			//$goods->ListTableInsert("<tr><td>No</td><td>Item</td></tr>");
+			foreach ($this->user->item as $no => $val)
+			{
+				$item = HOF_Model_Data::getItemData($no);
+				$string = HOF_Class_Item::ShowItemDetail($item, $val, 1) . "<br />";
+				//$string	= "<tr><td>".$no."</td><td>".HOF_Class_Item::ShowItemDetail($item,$val,1)."</td></tr>";
+				$goods->AddItem($item, $string);
+			}
+			print ($goods->GetJavaScript("list"));
+			print ($goods->ShowSelect());
+			print ('<div id="list">' . $goods->ShowDefault() . '</div>');
+		}
+		else
+		{
+			print ("No items.");
+		}
+	}
+
+}
+
+
+?>
