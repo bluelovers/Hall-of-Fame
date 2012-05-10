7eaf9f484ee66d5419b0d5e85a86fac9fac60ef1
bluelovers <codelovers@users.sourceforge.net>
2012-05-10 19:22:33 +0800

    HOF_Helper_Battle (Step: 4)
    Step: 4
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/class/class.main.php

--- hof/trust_path/class/class.main.php
+++ hof/trust_path/class/class.main.php
@@ -137,52 +137,7 @@ class main extends HOF_Class_User
 
 
 
-	//	('A`)...
-	function CharTestDoppel()
-	{
-		if (!$_POST["TestBattle"]) return 0;
 
-		$char = $this->char[$_GET["char"]];
-		$this->DoppelBattle(array($char));
-	}
-
-	//	ドッペルゲンガーと戦う。
-	function DoppelBattle($party, $turns = 10)
-	{
-		//$enemy	= $party;
-		//これが無いとPHP4or5 で違う結果になるんです
-		//$enemy	= unserialize(serialize($enemy));
-		// ↓
-
-		$enemy = array();
-
-		foreach ($party as $key => $char)
-		{
-			/*
-			$enemy[$key] = new HOF_Class_Char();
-			$enemy[$key]->SetCharData(get_object_vars($char));
-			*/
-
-			$enemy[$key] = HOF_Model_Char::newChar(get_object_vars($char));
-		}
-		foreach ($enemy as $key => $doppel)
-		{
-			//$doppel->judge	= array();//コメントを取るとドッペルが行動しない。
-			$enemy[$key]->ChangeName("ニセ" . $doppel->name);
-		}
-		//dump($enemy[0]->judge);
-		//dump($party[0]->judge);
-
-		$enemy = HOF_Class_Battle_Team::newInstance($enemy);
-		$party = HOF_Class_Battle_Team::newInstance($party);
-
-		$battle = new HOF_Class_Battle($party, $enemy);
-		$battle->SetTeamName($this->name, "ドッペル");
-		$battle->LimitTurns($turns); //最大ターン数は10
-		$battle->NoResult();
-		$battle->Process(); //戦闘開始
-		return true;
-	}
 
 	//
 
