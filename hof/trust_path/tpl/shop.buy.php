b3c268961d24ba3f61edad1eda7da653deb67d35
bluelovers <codelovers@users.sourceforge.net>
2012-05-11 07:49:10 +0800

    . (Step: 4)
    Step: 4
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/tpl/shop.buy.php

--- hof/trust_path/tpl/shop.buy.php
+++ hof/trust_path/tpl/shop.buy.php
@@ -1,4 +1,34 @@
+<?php $this->extend('shop/layout'); ?>
+<?php $this->callMethod('_buy'); ?>
 
-	<?php $this->extend('shop/layout'); ?>
+	<div style="margin:15px">
+		<h4>買う</h4>
 
-	<?php $this->callMethod('_buy'); ?>
+		<form action="?menu=buy" method="post">
+			<table cellspacing="0" data-item="table">
+
+				<thead>
+					<tr>
+						<td class="td6"></td>
+						<td style="text-align:center" class="td6">値段</td>
+						<td style="text-align:center" class="td6">数</td>
+						<td style="text-align:center" class="td6">アイテム</td>
+					</tr>
+				</thead>
+
+				<tbody>
+				<?php foreach((array)$this->output->shoplist as $no => $item): ?>
+
+					<tr>
+						<td class="td7" data-no="<?php e($no) ?>"><input type="checkbox" name="check_<?php e($no) ?>" value="1" data-no="<?php e($no) ?>"></td>
+						<td class="td7" data-no="<?php e($no) ?>"><?php e($item->price_buy(1)) ?></td>
+						<td class="td7" data-no="<?php e($no) ?>"><input type="text" id="text_<?php e($no) ?>" name="amount_<?php e($no) ?>" value="1" style="width:60px" class="text"></td>
+						<td class="td8" data-no="<?php e($no) ?>"><?php e($item->html()) ?></td>
+					</tr>
+				</tbody>
+
+				<?php endforeach; ?>
+			</table>
+			<input type="submit" name="ItemBuy" value="Buy" class="btn">
+		</form>
+	</div>
