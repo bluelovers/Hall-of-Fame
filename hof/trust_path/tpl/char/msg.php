18cad41770eb462796908ea942cee809a91afffc
bluelovers <codelovers@users.sourceforge.net>
2012-05-10 16:12:16 +0800

    . (Step: 4)
    Step: 4
    
    Commit ChangedLog:
    
    [A ] new file:
    
    	hof/trust_path/tpl/char/msg.php

---
+++ hof/trust_path/tpl/char/msg.php
@@ -0,0 +1,12 @@
+
+	<div class="margin15">
+
+		<?php foreach((array)$this->output->_msg_error as $v): ?>
+			<?php HOF_Helper_Global::ShowError($v[0], $v[1]); ?>
+		<?php endforeach; ?>
+
+		<?php foreach((array)$this->output->_msg_result as $v): ?>
+			<?php HOF_Helper_Global::ShowResult($v[0], $v[1]); ?>
+		<?php endforeach; ?>
+
+	</div>
