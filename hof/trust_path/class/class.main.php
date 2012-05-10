cebc4008f2358ebe37f396bc4938fe1c4c4e86d8
bluelovers <codelovers@users.sourceforge.net>
2012-05-10 20:53:38 +0800

    . (Step: 14)
    Step: 14
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/class/class.main.php

--- hof/trust_path/class/class.main.php
+++ hof/trust_path/class/class.main.php
@@ -5,33 +5,11 @@ if (!defined('DEBUG'))
 	exit('Access Denied');
 }
 
-include (GLOBAL_PHP);
 
-class main extends HOF_Class_User
+
+class main
 {
 
-	var $islogin = false;
-
-
-	//
-	function __construct()
-	{
-		$this->SessionSwitch();
-		$this->Set_ID_PASS();
-		ob_start();
-		$this->Order();
-		$content = ob_get_contents();
-		ob_end_clean();
-
-		$this->Head();
-		print ($content);
-		$this->Debug();
-		//$this->ShowSession();
-		$this->Foot();
-	}
-
-
-	//
 
 
 	//	変数の表示
@@ -39,98 +17,6 @@ class main extends HOF_Class_User
 	{
 		if (DEBUG) print ("<pre>" . print_r(get_object_vars($this), 1) . "</pre>");
 	}
-
-
-	//	セッション情報を表示する。
-	function ShowSession()
-	{
-		echo "this->id:$this->id<br>";
-		echo "this->pass:$this->pass<br>";
-		echo "SES[id]:$_SESSION[id]<br>";
-		echo "SES[pass]:$_SESSION[pass]<br>";
-		echo "SES[pass]:" . $this->CryptPassword($_SESSION[pass]) . "(crypted)<br>";
-		echo "CK[NO]:$_COOKIE[NO]<br>";
-		echo "SES[NO]:" . session_id();
-		dump($_COOKIE);
-		dump($_SESSION);
-	}
-
-
-	//	ログインした時間を設定する
-	function RenewLoginTime()
-	{
-		$this->login = time();
-	}
-
-	//	pass と id を設定する
-	function Set_ID_PASS()
-	{
-		$id = ($_POST["id"]) ? $_POST["id"] : $_GET["id"];
-		//if($_POST["id"]) {
-		if ($id)
-		{
-			$this->id = $id; //$_POST["id"];
-			// ↓ログイン処理した時だけ
-			if (HOF_Controller_Game::is_registered($_POST["id"]))
-			{
-				$_SESSION["id"] = $this->id;
-			}
-		}
-		else
-			if ($_SESSION["id"]) $this->id = $_SESSION["id"];
-
-		$pass = ($_POST["pass"]) ? $_POST["pass"] : $_GET["pass"];
-		//if($_POST["pass"])
-		if ($pass) $this->pass = $pass; //$_POST["pass"];
-		else
-			if ($_SESSION["pass"]) $this->pass = $_SESSION["pass"];
-
-		if ($this->pass) $this->pass = $this->CryptPassword($this->pass);
-	}
-
-
-	//	保存されているセッション番号を変更する。
-	function SessionSwitch()
-	{
-		// session消滅の時間(?)
-		// how about "session_set_cookie_params()"?
-		session_cache_expire(COOKIE_EXPIRE / 60);
-		if ($_COOKIE["NO"]) //クッキーに保存してあるセッションIDのセッションを呼び出す
- 				session_id($_COOKIE["NO"]);
-
-		session_start();
-		if (!SESSION_SWITCH) //switchしないならここで終了
- 				return false;
-		//print_r($_SESSION);
-		//dump($_SESSION);
-		$OldID = session_id();
-		$temp = serialize($_SESSION);
-
-		session_regenerate_id();
-		$NewID = session_id();
-		setcookie("NO", $NewID, time() + COOKIE_EXPIRE);
-		$_COOKIE["NO"] = $NewID;
-
-		session_id($OldID);
-		session_start();
-
-		if ($_SESSION):
-			//	session_destroy();//Sleipnirだとおかしい...?(最初期)
-			//	unset($_SESSION);//こっちは大丈夫(やっぱりこれは駄目かも)(修正後)
-			//結局,セッションをforeachでループして1個づつunset(2007/9/14 再修正)
-			foreach ($_SESSION as $key => $val) unset($_SESSION["$key"]);
-		endif;
-
-		session_id($NewID);
-		session_start();
-		$_SESSION = unserialize($temp);
-	}
-
-
-	//
-
-	//
-
 
 	//	上部に表示されるメニュー。
 	//	ログインしてる人用とそうでない人。
