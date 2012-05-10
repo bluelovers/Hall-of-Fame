2f76cb19846550fe042d58076d971d55d7a0cab2
bluelovers <codelovers@users.sourceforge.net>
2012-05-10 20:53:28 +0800

    . (Step: 4)
    Step: 4
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/HOF/Controller/Battle.php

--- hof/trust_path/HOF/Controller/Battle.php
+++ hof/trust_path/HOF/Controller/Battle.php
@@ -17,7 +17,7 @@ class HOF_Controller_Battle extends HOF_Class_Controller
 
 	function _init()
 	{
-		$this->user = &HOF_Model_Main::getInstance();
+		$this->user = HOF::user();
 
 		$this->options['escapeHtml'] = false;
 	}
