e84263daa412254a0f639b3a555b8ecfb3eee3d1
bluelovers <codelovers@users.sourceforge.net>
2012-05-11 22:26:48 +0800

    . (Step: 2)
    Step: 2
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/class/class.auction.php

--- hof/trust_path/class/class.auction.php
+++ hof/trust_path/class/class.auction.php
@@ -214,68 +214,7 @@ class Auction
 			return $this->UserName["$id"];
 		}
 	}
-	//////////////////////////////////////////////
-	//	最低入札価格を返す
-	function ItemBottomPrice($ArticleNo)
-	{
-		if ($this->Article["$ArticleNo"])
-		{
-			return BottomPrice($this->Article["$ArticleNo"]["price"]);
-		}
-	}
-	//////////////////////////////////////////////
-	//	入札する
-	function ItemBid($ArticleNo, $BidPrice, $Bidder, $BidderName)
-	{
-		if (!$Article = $this->Article["$ArticleNo"]) return false;
-		$BottomPrice = BottomPrice($this->Article["$ArticleNo"]["price"]);
-		// IPが同じ
-		if ($Article["IP"] == $_SERVER[REMOTE_ADDR])
-		{
-			HOF_Helper_Global::ShowError("IP制限.");
-			return false;
-		}
-		// 携帯端末を禁止する。
-		if (isMobile == "i")
-		{
-			HOF_Helper_Global::ShowError("mobile forbid.");
-			return false;
-		}
-		// 最低入札価格を割っている場合
-		if ($BidPrice < $BottomPrice) return false;
 
-		// 誰かが入札していた場合お金を返金する。
-		if ($Article["bidder"])
-		{
-			$this->UserGetMoney($Article["bidder"], $Article["price"]);
-			$this->UserSaveData();
-		}
-
-		// 入札時間が残り少ないなら延長する。
-		$Now = time();
-		$left = AuctionLeftTime($Now, $Article["end"], true);
-		/* // 残り時間1時間以下なら15分延長する
-		if(1 < $left && $left < 3601) {
-		$this->Article["$ArticleNo"]["end"]	+= 900;
-		}
-		*/
-		// 残り時間15分以下なら 15分にする。
-		if (0 < $left && $left < 901)
-		{
-			$dif = 900 - $left;
-			$this->Article["$ArticleNo"]["end"] += $dif;
-		}
-
-
-		$this->Article["$ArticleNo"]["price"] = $BidPrice;
-		$this->Article["$ArticleNo"]["TotalBid"]++;
-		$this->Article["$ArticleNo"]["bidder"] = $Bidder;
-		$this->DataChange = true;
-		$item = HOF_Model_Data::getItemData($Article["item"]);
-		//$this->AddLog("No.".$Article["No"]." <span class=\"bold\">{$item[name]} x{$Article[amount]}</span>個に ".HOF_Helper_Global::MoneyFormat($BidPrice)." で ".$this->LoadUserName($Bidder)." が<span class=\"support\">入札しました。</span>");
-		$this->AddLog("No." . $Article["No"] . " <span class=\"bold\">{$item[name]} x{$Article[amount]}</span>個に " . HOF_Helper_Global::MoneyFormat($BidPrice) . " で " . $BidderName . " が<span class=\"support\">入札しました。</span>");
-		return true;
-	}
 	//////////////////////////////////////////////
 	//	出品物一覧を表示する(その1) 表示の並びが違うだけ
 	function ItemShowArticle($bidding = false)
@@ -713,16 +652,5 @@ function AuctionLeftTime($now, $end, $int = false)
 			return "{$hour}時間{$minutes}分";
 		}
 }
-//////////////////////////////////////////////////
-//	最低価格を返す
-//	価格の5%が最低値。
-//	100以下なら100がそれになる。
-function BottomPrice($price)
-{
-	$bottom = floor($price * 0.10);
-	if ($bottom < 101) return sprintf("%0.0f", $price + 100);
-	else  return sprintf("%0.0f", $price + $bottom);
-}
-
 
 ?>
