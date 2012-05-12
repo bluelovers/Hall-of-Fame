4e44b01905ba5ce72193bf5f48ac51c47ce6c95b
bluelovers <codelovers@users.sourceforge.net>
2012-05-13 00:42:49 +0800

    HOF_Class_Skill (Step: 4)
    Step: 4
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/HOF/Class/Skill.php

--- hof/trust_path/HOF/Class/Skill.php
+++ hof/trust_path/HOF/Class/Skill.php
@@ -8,12 +8,168 @@
 class HOF_Class_Skill
 {
 
+	function __construct($no)
+	{
+		$data = HOF_Model_Data::getSkill($no, true);
+
+		$_source_data_ = $data;
+
+		parent::__construct((array)$data);
+
+		$this->id = $no;
+		$this->icon = $this->img;
+
+		$this->_source_data_ = $_source_data_;
+
+		return $this;
+	}
+
+	function exists()
+	{
+		return ($this->no) ? true : false;
+	}
+
+	function &newCopy($clone = false)
+	{
+		if ($clone)
+		{
+			$item = clone $this;
+		}
+		else
+		{
+			$item = new self($this->no);
+		}
+
+		return $item;
+	}
+
+	function &newInstance($no, $check = true)
+	{
+		if ($check && ($no instanceof self))
+		{
+			return $no;
+		}
+
+		return new self($no);
+	}
+
+	function id($over = false)
+	{
+		$var = $this->id;
+		$var = printf("[%04s]", $var);
+
+		return (string)$var;
+	}
+
+	function no()
+	{
+		$var = $this->no;
+		$var = printf("[%04s]", $var);
+
+		return (string)$var;
+	}
+
+	function __toString()
+	{
+		return $this->id();
+	}
+
+	/**
+	 * jQuery style attr
+	 */
+	function attr($attr, $value = null)
+	{
+		if (is_string($attr))
+		{
+			if ($value === null)
+			{
+				return $this->$attr;
+			}
+
+			$this->$attr = $value;
+		}
+		elseif (is_array($attr))
+		{
+			foreach ($attr as $k => $v)
+			{
+				$this->$k = $v;
+			}
+		}
+
+		return $this;
+	}
+
+	function icon($url = false, $true = false)
+	{
+		$icon = $true ? $this->img : $this->icon;
+
+		if ($url)
+		{
+			return HOF_Class_Icon::getImageUrl($icon, HOF_Class_Icon::IMG_SKILL);
+		}
+		else
+		{
+			return HOF_Class_Icon::getImage($icon, HOF_Class_Icon::IMG_SKILL);
+		}
+	}
+
+	function html($radio = false, $text = true)
+	{
+		$data = $this->toArray();
+		$data['img'] = $this->icon();
+		$data['name'] = $this->name();
+
+		return self::ShowItemDetail($data, $radio, $text);
+	}
+
+	function name()
+	{
+		$name = $this->name;
+
+		if (!$name)
+		{
+			$name = self::UNKNOW_NAME.'#'.$this->id();
+		}
+
+		return $name;
+	}
+
+	function __call($func, $args)
+	{
+		if (property_exists($this, $func) || isset($this[$func]))
+		{
+			$val = $this->$func;
+
+			return $val;
+		}
+		else
+		{
+			throw new BadMethodCallException('Call to undefined method '.get_class($this).'::'.$func.'()');
+		}
+	}
+
 	/**
 	 * 技の詳細を表示
 	 */
-	function ShowSkillDetail($skill, $radio = false)
+	function ShowSkillDetail($skill, $radio = false, $text = false)
 	{
 		if (!$skill) return false;
+
+		$output = array();
+
+		$output['skill'] = $skill;
+		$output['radio'] = $radio;
+
+		$content = HOF_Class_View::render(null, $output, 'layout/skill.detail');
+
+		if ($text)
+		{
+			return $content;
+		}
+
+		$content->output();
+
+		return;
 
 		if ($radio) print ('<input type="radio" name="newskill" value="' . $skill["no"] . '" class="vcent" />');
 
