e7a640a30fa6a7b3a475d8fa46fb77998ab9d9d3
bluelovers <codelovers@users.sourceforge.net>
2012-05-13 20:49:37 +0800

    HOF_Class_Char_Mon_Union (Step: 2)
    Step: 2
    
    Commit ChangedLog:
    
    [A ] new file:
    
    	hof/trust_path/HOF/Class/Char/Base.php

---
+++ hof/trust_path/HOF/Class/Char/Base.php
@@ -0,0 +1,89 @@
+<?php
+
+/**
+ * @author bluelovers
+ * @copyright 2012
+ */
+
+class HOF_Class_Char_Base extends HOF_Class_Array
+{
+	public $id;
+
+	/**
+	 * ステータス
+	 */
+	public $maxhp, $hp, $maxsp, $sp, $str, $int, $dex, $spd, $luk;
+
+	public $MAXHP, $HP, $MAXSP, $SP, $STR, $INT, $DEX, $SPD, $LUK;
+
+	/**
+	 * 単純なステータス補正(plus)
+	 */
+	public $P_MAXHP, $P_MAXSP, $P_STR, $P_INT, $P_DEX, $P_SPD, $P_LUK;
+	/**
+	 * 単純なステータス補正(multipication)
+	 */
+	public $M_MAXHP, $M_MAXSP;
+	/**
+	 * 特殊技能
+	 */
+	public $SPECIAL;
+
+	/**
+	 * 生存状態にする
+	 * 状態(0=生存 1=しぼー 2=毒状態)
+	 */
+	public $STATE = ALIVE;
+
+	/**
+	 * $atk=array(物理,魔法); $def=array(物理/,物理-,魔法/,魔法-);
+	 */
+	public $atk, $def;
+
+	/**
+	 * 行動までの時間
+	 */
+	public $delay;
+
+	/**
+	 * 行動(判定、使うスキル)
+	 */
+	public $pattern, $pattern_memo;
+
+	/**
+	 * (数値=詠唱中 false=待機中)
+	 * 詠唱完了時に使うスキル
+	 * @var bool
+	 */
+	public $expect = false;
+	/**
+	 * 詠唱完了時に使うスキルのタイプ(物理/魔法)
+	 */
+	public $expect_type;
+	/**
+	 * ↑のターゲット
+	 */
+	public $expect_target;
+
+	/**
+	 * 行動回数
+	 */
+	public $ActCount = 0;
+
+	/**
+	 * 決定した判断の回数=array()
+	 * @var array
+	 */
+	public $JdgCount = array();
+
+	/**
+	 * ファイルポインタが開かれていれば閉じる
+	 */
+	function fpclose()
+	{
+		HOF_Class_File::fpclose($this->fp);
+
+		unset($this->fp);
+	}
+
+}
