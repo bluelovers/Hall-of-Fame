0a8f05d59b38fc10a9edcfa762d740c6c95b64be
bluelovers <codelovers@users.sourceforge.net>
2012-05-14 06:41:12 +0800

    . (Step: 12)
    Step: 12
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/tpl/gamedata.monster.php

--- hof/trust_path/tpl/gamedata.monster.php
+++ hof/trust_path/tpl/gamedata.monster.php
@@ -1,4 +1,37 @@
+<?php $this->extend('gamedata.layout') ?>
 
-	<?php $this->extend('gamedata.layout') ?>
-
-	<?php include (GAME_DATA_MONSTER); ?>
+	<div style="margin:0 15px">
+		<h4>モンスター</h4>
+		<table class="align-center" style="width:740px" cellspacing="0">
+			<?php foreach($this->output->list as $data): ?>
+			<tr>
+				<td class="td6">Image</td>
+				<td class="td6">EXP</td>
+				<td class="td6">MONEY</td>
+				<td class="td6">HP</td>
+				<td class="td6">SP</td>
+				<td class="td6">STR</td>
+				<td class="td6">INT</td>
+				<td class="td6">DEX</td>
+				<td class="td6">SPD</td>
+				<td class="td6">LUK</td>
+			</tr>
+			</td>
+			
+				<td class="td7"><?php $data['char']->ShowCharWithLand($data['char']->land); ?></td>
+				<td class="td7"><?php e($data['monster']['exphold']) ?></td>
+				<td class="td7"><?php e($data['monster']['moneyhold']) ?></td>
+				<td class="td7"><?php e($data['monster']['maxhp']) ?></td>
+				<td class="td7"><?php e($data['monster']['maxsp']) ?></td>
+				<td class="td7"><?php e($data['monster']['str']) ?></td>
+				<td class="td7"><?php e($data['monster']['int']) ?></td>
+				<td class="td7"><?php e($data['monster']['dex']) ?></td>
+				<td class="td7"><?php e($data['monster']['spd']) ?></td>
+				<td class="td8"><?php e($data['monster']['luk']) ?></td>
+			</tr>
+			<tr>
+				<td class="td7" colspan="11"><?php e($data['monster']['info']['desc']) ?></td>
+			</tr>
+			<?php endforeach; ?>
+		</table>
+	</div>
