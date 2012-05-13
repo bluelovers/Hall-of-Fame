f9e33a9261e91b55cd374b5face66ee781b9de6f
bluelovers <codelovers@users.sourceforge.net>
2012-05-13 23:15:13 +0800

    change gender 1 = boy , 2 = girl (Step: 1)
    Step: 1
    
    Commit ChangedLog:
    
    [A ] new file:
    
    	hof/trust_path/HOF/Const/const.game.php

---
+++ hof/trust_path/HOF/Const/const.game.php
@@ -0,0 +1,14 @@
+<?php
+
+define('GENDER_UNKNOW', 0);
+define('GENDER_BOY', 1);
+define('GENDER_GIRL', 2);
+
+define("POSITION_FRONT", "front");
+define("POSITION_BACK", "back");
+
+define("STATE_ALIVE", 0);
+define("STATE_DEAD", 1);
+define("STATE_POISON", 2);
+
+define("JOB_UNDEFINED", 0);
