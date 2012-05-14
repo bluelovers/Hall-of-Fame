8bc32d931b3ce8e53db59e23341aa6b962f47662
bluelovers <codelovers@users.sourceforge.net>
2012-05-14 21:38:32 +0800

    Object Allow Extend Method Class (Step: 1)
    Step: 1
    
    Commit ChangedLog:
    
    [A ] new file:
    
    	hof/trust_path/HOF/Class/Base/Extend/Extend.php

---
+++ hof/trust_path/HOF/Class/Base/Extend/Extend.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @author bluelovers
+ * @copyright 2012
+ */
+
+class HOF_Class_Base_Extend_Extend implements HOF_Class_Base_Extend_RootInterface
+{
+
+	/**
+	 * jQuery style attr
+	 */
+
+	protected $_root_;
+
+	function __construct(&$root_obj)
+	{
+		$this->_root_ = &$root_obj;
+	}
+
+}
