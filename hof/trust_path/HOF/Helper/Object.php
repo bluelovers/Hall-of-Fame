443107a51d556b851dc02eea3b80af9f1fc166a4
bluelovers <codelovers@users.sourceforge.net>
2012-05-14 21:38:36 +0800

    Object Allow Extend Method Class (Step: 5)
    Step: 5
    
    Commit ChangedLog:
    
    [A ] new file:
    
    	hof/trust_path/HOF/Helper/Object.php

---
+++ hof/trust_path/HOF/Helper/Object.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * @author bluelovers
+ * @copyright 2012
+ */
+
+class HOF_Helper_Object
+{
+
+	function extend($root_obj, $class)
+	{
+		if (($root_obj instanceof HOF_Class_Base_Extend_RootInterface) || method_exists($root_obj, 'extend'))
+		{
+			return $root_obj->extend($class);
+		}
+		else
+		{
+			throw new RuntimeException('class not allow extend.');
+		}
+	}
+
+	/**
+	 * Get Class Public Methods
+	 *
+	 * @url http://hardforum.com/showthread.php?t=1480605
+	 */
+	function get_public_methods($class, $skip = array())
+	{
+		$methods = array();
+
+		foreach (get_class_methods($class) as $key => $method)
+		{
+			/* Get a reflection object for the class method */
+			$reflect = new ReflectionMethod($class, $method);
+
+			/* For private, use isPrivate().  For protected, use isProtected() */
+			if ($reflect->isPublic())
+			{
+				if (!in_array($method, $skip))
+				{
+					// Put the methods we care about into an array
+					$methods[] = $method;
+				}
+			}
+		}
+
+		return $methods;
+	}
+
+}
