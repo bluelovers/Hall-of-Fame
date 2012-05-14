598e30a73dc3ea0621331bc63c89ced57fa4d3eb
bluelovers <codelovers@users.sourceforge.net>
2012-05-14 23:41:15 +0800

    bugfix in ref argv (Step: 8)
    Step: 8
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/test/obj.extend.php

--- hof/trust_path/test/obj.extend.php
+++ hof/trust_path/test/obj.extend.php
@@ -12,12 +12,33 @@ class _temp_A extends HOF_Class_Base_Extend_Root
 
 }
 
-class _temp_B extends HOF_Class_Base_Extend_Extend
+class _temp_B implements HOF_Class_Base_Extend_ExtendInterface
 {
+
+	protected $_root_;
+
+	function __construct(&$root_obj)
+	{
+		$this->_root_ = &$root_obj;
+	}
 
 	function public_call()
 	{
 		echo __METHOD__ . "\n";
+	}
+
+	function public_call_ref(&$a, $b = 1)
+	{
+		echo __METHOD__ . "\n";
+		echo $a . "\n";
+		$a = 321;
+	}
+
+	function public_call_ref2($a, $b = 1)
+	{
+		echo __METHOD__ . "\n";
+		echo $a . "\n";
+		$a = 321;
 	}
 
 	protected function protected_call()
@@ -25,8 +46,32 @@ class _temp_B extends HOF_Class_Base_Extend_Extend
 		echo __METHOD__ . "\n";
 	}
 
+	static function static_call()
+	{
+		echo __METHOD__ . "\n";
+	}
+
 }
 
+$class = new ReflectionClass('_temp_B');
+$method = $class->getMethod('public_call_ref');
+$parameter = $method->getParameters();
+//$parameter = new ReflectionParameter(array($class, $method));
+
+//echo $parameter->__toString();
+
+foreach ($parameter as $param)
+{
+	var_dump($param);
+
+	if ($param->isPassedByReference())
+	{
+		echo $param->getName() . ' : isPassedByReference \n';
+	}
+}
+
+
+_e(__LINE__);
 
 $a = new _temp_A;
 
@@ -34,11 +79,56 @@ $a->extend('_temp_B');
 
 $a->public_call();
 
+
+_e(__LINE__);
+
+try
+{
+	$c = '123';
+
+	$a->public_call_ref($c);
+
+	_e($c);
+}
+catch (Exception $e)
+{
+	echo $e->getMessage() . "\n";
+}
+
+
+_e(__LINE__);
+
+try
+{
+	$c = '123';
+
+	$a->public_call_ref2(&$c);
+
+	_e($c);
+}
+catch (Exception $e)
+{
+	_e($e->getMessage());
+}
+
+_e(__LINE__);
+
 try
 {
 	$a->protected_call();
 }
 catch (Exception $e)
 {
-	echo $e->getMessage() . "\n";
+	_e($e->getMessage());
+}
+
+_e(__LINE__);
+
+try
+{
+	$a->static_call();
+}
+catch (Exception $e)
+{
+	_e($e->getMessage());
 }
