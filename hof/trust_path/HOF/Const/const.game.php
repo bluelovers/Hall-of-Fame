12143a086a5b2162215278037bb8837e13842111
bluelovers <codelovers@users.sourceforge.net>
2012-05-15 04:41:05 +0800

    . (Step: 4)
    Step: 4
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/HOF/Const/const.game.php

--- hof/trust_path/HOF/Const/const.game.php
+++ hof/trust_path/HOF/Const/const.game.php
@@ -4,11 +4,16 @@ define('GENDER_UNKNOW', 0);
 define('GENDER_BOY', 1);
 define('GENDER_GIRL', 2);
 
-define("POSITION_FRONT", "front");
-define("POSITION_BACK", "back");
+define('POSITION_FRONT', 'front');
+define('POSITION_BACK', 'back');
 
-define("STATE_ALIVE", 0);
-define("STATE_DEAD", 1);
-define("STATE_POISON", 2);
+define('STATE_ALIVE', 0);
+define('STATE_DEAD', 1);
+define('STATE_POISON', 2);
 
-define("JOB_UNDEFINED", 0);
+define('JOB_UNDEFINED', 0);
+
+/**
+ * キャラ画像がない場合表示される画像
+ */
+define('NO_IMAGE', 'noimage');
