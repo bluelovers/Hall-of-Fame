f6989f3e9d47f411eb3cc45b6fedf0acc7cbf666
bluelovers <codelovers@users.sourceforge.net>
2012-05-16 01:17:22 +0800

    . (Step: 2)
    Step: 2
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/tpl/char.char.php

--- hof/trust_path/tpl/char.char.php
+++ hof/trust_path/tpl/char.char.php
@@ -1,4 +1,16 @@
+<?php $this->extend('char/layout') ?>
 
-	<?php $this->extend('char/layout') ?>
-
+	<form action="?char=<?php e($this->output->char_id) ?>" method="post" style="padding:5px 0 0 15px">
+		<h4>Character Status<a href="?manual#charstat" target="_blank" class="a0">?</a></h4>
+		<?php $this->output->char->ShowCharDetail(); ?>
+		<?php if ($this->output->user_item["7500"]):?>
+		<!-- 改名 -->
+		<input type="submit" class="btn" name="rename" value="ChangeName">
+		<?php endif; ?>
+		<?php if ($this->output->user_item["7510"] || $this->output->user_item["7511"] || $this->output->user_item["7512"] || $this->output->user_item["7513"] || $this->output->user_item["7520"]): ?>
+		<!-- ステータスリセット系 -->
+		<input type="submit" class="btn" name="showreset" value="Reset">
+		<?php endif; ?>
+		<input type="submit" class="btn" name="byebye" value="Kick">
+	</form>
 	<?php e($this->output->content) ?>
