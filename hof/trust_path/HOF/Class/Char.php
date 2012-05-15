f286b829b704a6e02bdc7f6c3628cf3bfd418c88
bluelovers <codelovers@users.sourceforge.net>
2012-05-15 19:26:32 +0800

    . (Step: 2)
    Step: 2
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/HOF/Class/Char.php

--- hof/trust_path/HOF/Class/Char.php
+++ hof/trust_path/HOF/Class/Char.php
@@ -30,28 +30,53 @@ class HOF_Class_Char extends char
 
 	protected $_extends_method_invalids_ = array();
 
-	public function extend($extend)
+	public function extend_remove($extend, $idx = null)
 	{
-		$this->_extends_[$class] = null;
-
 		if (is_object($extend))
 		{
 			$class = get_class($extend);
-			$this->_extends_[$class]['obj'] = &$extend;
 		}
 		else
 		{
 			$class = $extend;
-			$this->_extends_[$class]['obj'] = null;
 		}
 
-		$this->_extends_[$class]['class'] = $class;
+		if (!$idx) $idx = $class;
+
+		foreach((array)$this->_extends_[$idx]['method'] as $method)
+		{
+			unset($this->_extends_method_[$method]);
+		}
+
+		unset($this->_extends_[$idx]);
+
+		return array($idx, $class);
+	}
+
+	public function extend($extend, $idx = null)
+	{
+		list($class, $idx) = $this->extend_remove($extend, $idx);
+
+		$this->_extends_[$idx]['idx'] = $idx;
+
+		if (is_object($extend))
+		{
+			$this->_extends_[$idx]['obj'] = &$extend;
+		}
+		else
+		{
+			$this->_extends_[$idx]['obj'] = null;
+		}
+
+		$this->_extends_[$idx]['class'] = $class;
 
 		$methods = HOF_Helper_Object::get_public_methods($class, $this->_extends_method_invalids_);
 
-		foreach ($methods as $v)
+		$this->_extends_[$idx]['method'] = $methods;
+
+		foreach ($methods as $method)
 		{
-			$this->_extends_method_[$v] = $class;
+			$this->_extends_method_[$method] = $idx;
 		}
 
 		return $this;
@@ -59,16 +84,22 @@ class HOF_Class_Char extends char
 
 	public function __call($func, $argv)
 	{
-		if (isset($this->_extends_method_[$func]) && !empty($this->_extends_method_[$func]))
+		if (!empty($this->_extends_method_[$func]))
 		{
-			$class = $this->_extends_method_[$func];
+			$idx = $this->_extends_method_[$func];
+			$class = $this->_extends_[$idx]['class'];
 
-			if (!is_object($this->_extends_[$class]['obj']))
+			if (empty($this->_extends_[$idx]['callback'][$func]))
 			{
-				$this->_extends_[$class]['obj'] = new $class(&$this);
+				if (!is_object($this->_extends_[$idx]['obj']))
+				{
+					$this->_extends_[$idx]['obj'] = new $class(&$this);
+				}
+
+				$this->_extends_[$idx]['callback'][$func] = array(&$this->_extends_[$idx]['obj'], $func);
 			}
 
-			return call_user_func_array(array($this->_extends_[$class]['obj'], $func), $argv);
+			return call_user_func_array($this->_extends_[$idx]['callback'][$func], $argv);
 		}
 		else
 		{
@@ -103,7 +134,7 @@ class HOF_Class_Char extends char
 
 	function __destruct()
 	{
-		@HOF_Class_Char_Pattern::getInstance($this)->__destruct();
+
 
 		$this->fpclose();
 	}
