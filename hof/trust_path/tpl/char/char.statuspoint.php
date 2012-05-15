c2681a003cb1477e53f03b7803d1c6972f144f56
bluelovers <codelovers@users.sourceforge.net>
2012-05-16 02:11:52 +0800

    . (Step: 4)
    Step: 4
    
    Commit ChangedLog:
    
    [A ] new file:
    
    	hof/trust_path/tpl/char/char.statuspoint.php

---
+++ hof/trust_path/tpl/char/char.statuspoint.php
@@ -0,0 +1,28 @@
+
+	<?php if (0 < $this->output->char->statuspoint): ?>
+
+	<!-- ステータス上昇 -->
+	<form action="?char=<?php e($this->output->char_id) ?>" method="post" style="padding:0 15px">
+		<h4>Status <a href="?manual#statup" target="_blank" class="a0">?</a></h4>
+		<?php $Stat = array(
+				"Str",
+				"Int",
+				"Dex",
+				"Spd",
+				"Luk"); ?>
+		Point :
+		<?php e($this->output->char->statuspoint) ?>
+		<br />
+		<?php foreach ($Stat as $val): ?>
+			<?php e($val) ?>:
+			<select name="up<?php e($val) ?>" class="vcent">
+				<?php for ($i = 0; $i < $this->output->char->statuspoint + 1; $i++) : ?>
+					<option value="<?php e($i) ?>">+<?php e($i) ?></option>
+				<?php endfor; ?>
+			</select>
+		<?php endforeach; ?>
+		<br />
+		<input type="submit" class="btn" name="stup" value="Increase Status">
+	</form>
+	
+	<?php endif; ?>
