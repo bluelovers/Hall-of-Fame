56c47b8c3eced1b799e6dd6018585fb4d9e82b81
bluelovers <codelovers@users.sourceforge.net>
2012-05-16 02:11:53 +0800

    . (Step: 5)
    Step: 5
    
    Commit ChangedLog:
    
    [A ] new file:
    
    	hof/trust_path/tpl/char/job_change.php

---
+++ hof/trust_path/tpl/char/job_change.php
@@ -0,0 +1,25 @@
+<!-- 転職 -->
+
+<?php if ($this->output->job_change_list): ?>
+</form>
+
+	<form action="?char=<?php e($this->output->char_id) ?>" method="post" style="padding:0 15px">
+		<h4>JobChange</h4>
+		<table>
+			<tbody>
+				<tr>
+					<?php foreach ((array)$this->output->job_change_list as $newjob): ?>
+					<td valign="bottom" style="padding:5px 30px;text-align:center"><label> <img src="<?php e($newjob->icon_url()) ?>" />
+							<br />
+							<input type="radio" value="<?php e($newjob->job()) ?>" name="job" />
+							<br />
+							<?php e($newjob->job_name()) ?>
+						</label></td>
+					<?php endforeach; ?>
+				</tr>
+			</tbody>
+		</table>
+		<input type="submit" class="btn" name="job_change" value="JobChange">
+		<input type="hidden" name="job_change" value="1">
+	</form>
+	<?php endif; ?>
