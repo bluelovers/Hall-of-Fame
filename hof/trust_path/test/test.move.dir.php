6b570a06ad81dba49a295d467e4f9d19122b1259
bluelovers <codelovers@users.sourceforge.net>
2012-05-17 00:55:58 +0800

    file trash mode for kepp safe and try log what del (Step: 1)
    Step: 1
    
    Commit ChangedLog:
    
    [A ] new file:
    
    	hof/trust_path/test/test.move.dir.php

---
+++ hof/trust_path/test/test.move.dir.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @author bluelovers
+ * @copyright 2012
+ */
+
+require_once ("./bootstrap.php");
+
+//$dir = USER . '../user_del/';
+//
+//HOF_Class_File::mkdir($dir);
+
+HOF_Class_File::rmdir(USER.'test5', true);
