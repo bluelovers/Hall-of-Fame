2cd9138a3d9cb6ef35c1667ab3d9c8c0168ebaab
bluelovers <codelovers@users.sourceforge.net>
2012-05-18 07:04:47 +0800

    . (Step: 10)
    Step: 10
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/tpl/rank.rank_all.php

--- hof/trust_path/tpl/rank.rank_all.php
+++ hof/trust_path/tpl/rank.rank_all.php
@@ -2,7 +2,7 @@
 	<?php $this->extend('rank/layout') ?>
 
 	<div style="margin:15px">
-		<h4>Ranking - <?php e(gc_date("Y年n月j日 G時i分s秒")) ?></h4>
+		<h4>Ranking - <?php e(HOF_Helper_Global::gc_date("Y年n月j日 G時i分s秒")) ?></h4>
 
 		<?php $this->callMethod('_ShowRanking', 1); ?>
 	</div>
