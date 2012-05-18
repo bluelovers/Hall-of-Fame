f10fbfc63ebca60f769524eedcd061c200369bd7
bluelovers <codelovers@users.sourceforge.net>
2012-05-19 03:29:16 +0800

    new icon cache system (Step: 2)
    Step: 2
    
    Commit ChangedLog:
    
    [A ] new file:
    
    	hof/trust_path/test/icon.1.php

---
+++ hof/trust_path/test/icon.1.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @author bluelovers
+ * @copyright 2012
+ */
+
+require_once ("./bootstrap.php");
+
+//$v['land'] = HOF_Class_Icon::getImageList(HOF_Class_Icon::IMG_LAND);
+
+$v[] = HOF_Class_Icon::getImage('bg_swamp2', HOF_Class_Icon::IMG_LAND);
+$v[] = HOF_Class_Icon::getImage('land_swamp2', HOF_Class_Icon::IMG_LAND);
+
+debug($v);
