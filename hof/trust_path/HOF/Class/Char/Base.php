7e69ddd8176ec8366cea6b5add7645915f18784a
bluelovers <codelovers@users.sourceforge.net>
2012-05-19 06:04:52 +0800

    .
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/HOF/Class/Char/Base.php

--- hof/trust_path/HOF/Class/Char/Base.php
+++ hof/trust_path/HOF/Class/Char/Base.php
@@ -5,7 +5,7 @@
  * @copyright 2012
  */
 
-class HOF_Class_Char_Base implements HOF_Class_Base_Extend_RootInterface
+class HOF_Class_Char_Base extends HOF_Class_Base_Extend_Root
 {
 	public $id;
 
@@ -78,88 +78,6 @@ class HOF_Class_Char_Base implements HOF_Class_Base_Extend_RootInterface
 	 * @var array
 	 */
 	public $JdgCount = array();
-
-	protected $_extends_ = array();
-	protected $_extends_method_ = array();
-
-	protected $_extends_method_invalids_ = array();
-
-	public function extend_remove($extend, $idx = null)
-	{
-		if (is_object($extend))
-		{
-			$class = get_class($extend);
-		}
-		else
-		{
-			$class = $extend;
-		}
-
-		if (!$idx) $idx = $class;
-
-		foreach ((array )$this->_extends_[$idx]['method'] as $method)
-		{
-			unset($this->_extends_method_[$method]);
-		}
-
-		unset($this->_extends_[$idx]);
-
-		return array($idx, $class);
-	}
-
-	public function extend($extend, $idx = null)
-	{
-		list($class, $idx) = $this->extend_remove($extend, $idx);
-
-		$this->_extends_[$idx]['idx'] = $idx;
-
-		if (is_object($extend))
-		{
-			$this->_extends_[$idx]['obj'] = &$extend;
-		}
-		else
-		{
-			$this->_extends_[$idx]['obj'] = null;
-		}
-
-		$this->_extends_[$idx]['class'] = $class;
-
-		$methods = HOF_Helper_Object::get_public_methods($class, $this->_extends_method_invalids_);
-
-		$this->_extends_[$idx]['method'] = $methods;
-
-		foreach ($methods as $method)
-		{
-			$this->_extends_method_[$method] = $idx;
-		}
-
-		return $this;
-	}
-
-	public function __call($func, $argv)
-	{
-		if (!empty($this->_extends_method_[$func]))
-		{
-			$idx = $this->_extends_method_[$func];
-			$class = $this->_extends_[$idx]['class'];
-
-			if (empty($this->_extends_[$idx]['callback'][$func]))
-			{
-				if (!is_object($this->_extends_[$idx]['obj']))
-				{
-					$this->_extends_[$idx]['obj'] = new $class(&$this);
-				}
-
-				$this->_extends_[$idx]['callback'][$func] = array(&$this->_extends_[$idx]['obj'], $func);
-			}
-
-			return call_user_func_array($this->_extends_[$idx]['callback'][$func], $argv);
-		}
-		else
-		{
-			throw new BadMethodCallException('Call to undefined method ' . get_class($this) . '::' . $func . '()');
-		}
-	}
 
 	/**
 	 * ファイルポインタが開かれていれば閉じる
