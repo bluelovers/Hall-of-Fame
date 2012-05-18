82320fef4d57cecfd19ea4431cab2601ea4c882e
bluelovers <codelovers@users.sourceforge.net>
2012-05-19 07:39:55 +0800

    . (Step: 3)
    Step: 3
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/HOF/Class/Char/Battle/Effect.php

--- hof/trust_path/HOF/Class/Char/Battle/Effect.php
+++ hof/trust_path/HOF/Class/Char/Battle/Effect.php
@@ -23,8 +23,8 @@ class HOF_Class_Char_Battle_Effect
 	{
 		$output = '';
 
-		if ($this->char->STATE === 1) $sub = " dmg";
-		elseif ($this->char->STATE === 2) $sub = " spdmg";
+		if ($this->char->STATE === STATE_DEAD) $sub = " dmg";
+		elseif ($this->char->STATE === STATE_POISON) $sub = " spdmg";
 
 		/**
 		 * 名前
@@ -34,14 +34,14 @@ class HOF_Class_Char_Battle_Effect
 		/**
 		 * チャージor詠唱
 		 */
-		if ($this->char->expect_type === 0) $output .= '<span class="charge">(charging)</span>' . "\n";
-		elseif ($this->char->expect_type === 1) $output .= '<span class="charge">(casting)</span>' . "\n";
+		if ($this->char->expect_type === EXPECT_CHARGING) $output .= '<span class="charge">(charging)</span>' . "\n";
+		elseif ($this->char->expect_type === EXPECT_CASTING) $output .= '<span class="charge">(casting)</span>' . "\n";
 
 		// HP,SP
 		$output .= "<div class=\"hpsp\">\n";
-		$sub = $this->char->STATE === 1 ? "dmg" : "recover";
+		$sub = $this->char->STATE === STATE_DEAD ? "dmg" : "recover";
 		$output .= "<span class=\"{$sub}\">HP : {$this->char->HP}/{$this->char->MAXHP}</span><br />\n"; //HP
-		$sub = $this->char->STATE === 1 ? "dmg" : "support";
+		$sub = $this->char->STATE === STATE_DEAD ? "dmg" : "support";
 		$output .= "<span class=\"{$sub}\">SP : {$this->char->SP}/{$this->char->MAXSP}</span>\n";
 		$output .= "</div>\n"; //SP
 
@@ -55,7 +55,7 @@ class HOF_Class_Char_Battle_Effect
 
 		$SelfDamage = ceil($this->char->MAXHP * ($rate / 100));
 		if ($this->char->POSITION != POSITION_FRONT) $SelfDamage *= 2;
-		print ("<span class=\"dmg\">" . $this->char->Name(bold) . " sacrifice ");
+		print ("<span class=\"dmg\">" . $this->char->Name('bold') . " sacrifice ");
 		print ("<span class=\"bold\">$SelfDamage</span> HP</span>\n");
 		$this->char->HpDamage($SelfDamage);
 		print ("</span><br />\n");
@@ -68,7 +68,7 @@ class HOF_Class_Char_Battle_Effect
 		if ($this->char->SPECIAL["HpRegen"])
 		{
 			$Regen = round($this->char->MAXHP * $this->char->SPECIAL["HpRegen"] / 100);
-			print ('<span class="recover">* </span>' . $this->char->Name(bold) . "<span class=\"recover\"> Auto Regenerate <span class=\"bold\">" . $Regen . " HP</span></span> ");
+			print ('<span class="recover">* </span>' . $this->char->Name('bold') . "<span class=\"recover\"> Auto Regenerate <span class=\"bold\">" . $Regen . " HP</span></span> ");
 			$this->char->HpRecover($Regen);
 			print ("<br />\n");
 		}
@@ -76,7 +76,7 @@ class HOF_Class_Char_Battle_Effect
 		if ($this->char->SPECIAL["SpRegen"])
 		{
 			$Regen = round($this->char->MAXSP * $this->char->SPECIAL["SpRegen"] / 100);
-			print ('<span class="support">* </span>' . $this->char->Name(bold) . "<span class=\"support\"> Auto Regenerate <span class=\"bold\">" . $Regen . " SP</span></span> ");
+			print ('<span class="support">* </span>' . $this->char->Name('bold') . "<span class=\"support\"> Auto Regenerate <span class=\"bold\">" . $Regen . " SP</span></span> ");
 			$this->char->SpRecover($Regen);
 			print ("<br />\n");
 		}
@@ -98,13 +98,13 @@ class HOF_Class_Char_Battle_Effect
 		{
 			if ($this->char->POSITION == POSITION_FRONT) return false;
 			$this->char->POSITION = POSITION_FRONT;
-			print ($this->char->Name(bold) . " moved to front.<br />\n");
+			print ($this->char->Name('bold') . " moved to front.<br />\n");
 		}
 		else
 		{
 			if ($this->char->POSITION != POSITION_FRONT) return false;
 			$this->char->POSITION = POSITION_BACK;
-			print ($this->char->Name(bold) . " moved to back.<br />\n");
+			print ($this->char->Name('bold') . " moved to back.<br />\n");
 		}
 	}
 
@@ -230,7 +230,7 @@ class HOF_Class_Char_Battle_Effect
 		if ($this->char->POSITION == POSITION_FRONT)
 		{
 			$this->char->POSITION = POSITION_BACK;
-			print ($this->char->Name(bold) . " knock backed!<br />\n");
+			print ($this->char->Name('bold') . " knock backed!<br />\n");
 		}
 	}
 
@@ -238,39 +238,39 @@ class HOF_Class_Char_Battle_Effect
 	function PlusSTR($no)
 	{
 		$this->char->STR += $no;
-		print ($this->char->Name(bold) . " STR rise {$no}<br />\n");
+		print ($this->char->Name('bold') . " STR rise {$no}<br />\n");
 	}
 	function PlusINT($no)
 	{
 		$this->char->INT += $no;
-		print ($this->char->Name(bold) . " INT rise {$no}<br />\n");
+		print ($this->char->Name('bold') . " INT rise {$no}<br />\n");
 	}
 	function PlusDEX($no)
 	{
 		$this->char->DEX += $no;
-		print ($this->char->Name(bold) . " DEX rise {$no}<br />\n");
+		print ($this->char->Name('bold') . " DEX rise {$no}<br />\n");
 	}
 	function PlusSPD($no)
 	{
 		$this->char->SPD += $no;
-		print ($this->char->Name(bold) . " SPD rise {$no}<br />\n");
+		print ($this->char->Name('bold') . " SPD rise {$no}<br />\n");
 	}
 	function PlusLUK($no)
 	{
 		$this->char->LUK += $no;
-		print ($this->char->Name(bold) . " LUK rise {$no}<br />\n");
+		print ($this->char->Name('bold') . " LUK rise {$no}<br />\n");
 	}
 
 	//	ステータス強化(%)
 	function UpMAXHP($no)
 	{
-		print ($this->char->Name(bold) . " MAXHP({$this->char->MAXHP}) extended to ");
+		print ($this->char->Name('bold') . " MAXHP({$this->char->MAXHP}) extended to ");
 		$this->char->MAXHP = round($this->char->MAXHP * (1 + $no / 100));
 		print ("{$this->char->MAXHP}<br />\n");
 	}
 	function UpMAXSP($no)
 	{
-		print ($this->char->Name(bold) . " MAXSP({$this->char->MAXSP}) extended to ");
+		print ($this->char->Name('bold') . " MAXSP({$this->char->MAXSP}) extended to ");
 		$this->char->MAXSP = round($this->char->MAXSP * (1 + $no / 100));
 		print ("{$this->char->MAXSP}<br />\n");
 	}
@@ -279,12 +279,12 @@ class HOF_Class_Char_Battle_Effect
 		$this->char->STR = round($this->char->STR * (1 + $no / 100));
 		if (($this->char->str * MAX_STATUS_MAXIMUM / 100) < $this->char->STR)
 		{
-			print ($this->char->Name(bold) . " STR rise to the maximum(" . MAX_STATUS_MAXIMUM . "%).<br />\n");
+			print ($this->char->Name('bold') . " STR rise to the maximum(" . MAX_STATUS_MAXIMUM . "%).<br />\n");
 			$this->char->STR = round($this->char->str * MAX_STATUS_MAXIMUM / 100);
 		}
 		else
 		{
-			print ($this->char->Name(bold) . " STR rise {$no}%<br />\n");
+			print ($this->char->Name('bold') . " STR rise {$no}%<br />\n");
 		}
 	}
 	function UpINT($no)
@@ -292,12 +292,12 @@ class HOF_Class_Char_Battle_Effect
 		$this->char->INT = round($this->char->INT * (1 + $no / 100));
 		if (($this->char->int * MAX_STATUS_MAXIMUM / 100) < $this->char->INT)
 		{
-			print ($this->char->Name(bold) . " INT rise to the maximum(" . MAX_STATUS_MAXIMUM . "%).<br />\n");
+			print ($this->char->Name('bold') . " INT rise to the maximum(" . MAX_STATUS_MAXIMUM . "%).<br />\n");
 			$this->char->INT = round($this->char->int * MAX_STATUS_MAXIMUM / 100);
 		}
 		else
 		{
-			print ($this->char->Name(bold) . " INT rise {$no}%<br />\n");
+			print ($this->char->Name('bold') . " INT rise {$no}%<br />\n");
 		}
 	}
 	function UpDEX($no)
@@ -305,12 +305,12 @@ class HOF_Class_Char_Battle_Effect
 		$this->char->DEX = round($this->char->DEX * (1 + $no / 100));
 		if (($this->char->dex * MAX_STATUS_MAXIMUM / 100) < $this->char->DEX)
 		{
-			print ($this->char->Name(bold) . " DEX rise to the maximum(" . MAX_STATUS_MAXIMUM . "%).<br />\n");
+			print ($this->char->Name('bold') . " DEX rise to the maximum(" . MAX_STATUS_MAXIMUM . "%).<br />\n");
 			$this->char->DEX = round($this->char->dex * MAX_STATUS_MAXIMUM / 100);
 		}
 		else
 		{
-			print ($this->char->Name(bold) . " DEX rise {$no}%<br />\n");
+			print ($this->char->Name('bold') . " DEX rise {$no}%<br />\n");
 		}
 	}
 	function UpSPD($no)
@@ -318,47 +318,47 @@ class HOF_Class_Char_Battle_Effect
 		$this->char->SPD = round($this->char->SPD * (1 + $no / 100));
 		if (($this->char->spd * MAX_STATUS_MAXIMUM / 100) < $this->char->SPD)
 		{
-			print ($this->char->Name(bold) . " SPD rise to the maximum(" . MAX_STATUS_MAXIMUM . "%).<br />\n");
+			print ($this->char->Name('bold') . " SPD rise to the maximum(" . MAX_STATUS_MAXIMUM . "%).<br />\n");
 			$this->char->SPD = round($this->char->spd * MAX_STATUS_MAXIMUM / 100);
 		}
 		else
 		{
-			print ($this->char->Name(bold) . " SPD rise {$no}%<br />\n");
+			print ($this->char->Name('bold') . " SPD rise {$no}%<br />\n");
 		}
 	}
 	function UpATK($no)
 	{
 		$this->char->atk["0"] = round($this->char->atk["0"] * (1 + $no / 100));
-		print ($this->char->Name(bold) . " ATK rise {$no}%<br />\n");
+		print ($this->char->Name('bold') . " ATK rise {$no}%<br />\n");
 	}
 	function UpMATK($no)
 	{
 		$this->char->atk["1"] = round($this->char->atk["1"] * (1 + $no / 100));
-		print ($this->char->Name(bold) . " MATK rise {$no}%<br />\n");
+		print ($this->char->Name('bold') . " MATK rise {$no}%<br />\n");
 	}
 	function UpDEF($no)
 	{
 		$up = floor((100 - $this->char->def["0"]) * ($no / 100));
 		$this->char->def["0"] += $up;
-		print ($this->char->Name(bold) . " DEF rise {$no}%<br />\n");
+		print ($this->char->Name('bold') . " DEF rise {$no}%<br />\n");
 	}
 	function UpMDEF($no)
 	{
 		$up = floor((100 - $this->char->def["2"]) * ($no / 100));
-		print ($this->char->Name(bold) . " MDEF rise {$no}%<br />\n");
+		print ($this->char->Name('bold') . " MDEF rise {$no}%<br />\n");
 		$this->char->def["2"] += $up;
 	}
 	//	ステータス弱体化(%)
 	function DownMAXHP($no)
 	{
-		print ($this->char->Name(bold) . " MAXHP({$this->char->MAXHP}) down to ");
+		print ($this->char->Name('bold') . " MAXHP({$this->char->MAXHP}) down to ");
 		$this->char->MAXHP = round($this->char->MAXHP * (1 - $no / 100));
 		if ($this->char->MAXHP < $this->char->HP) $this->char->HP = $this->char->MAXHP;
 		print ("{$this->char->MAXHP}<br />\n");
 	}
 	function DownMAXSP($no)
 	{
-		print ($this->char->Name(bold) . " MAXSP({$this->char->MAXSP}) down to ");
+		print ($this->char->Name('bold') . " MAXSP({$this->char->MAXSP}) down to ");
 		$this->char->MAXSP = round($this->char->MAXSP * (1 - $no / 100));
 		if ($this->char->MAXSP < $this->char->SP) $this->char->SP = $this->char->MAXSP;
 		print ("{$this->char->MAXSP}<br />\n");
@@ -366,52 +366,52 @@ class HOF_Class_Char_Battle_Effect
 	function DownSTR($no)
 	{
 		$this->char->STR = round($this->char->STR * (1 - $no / 100));
-		print ($this->char->Name(bold) . " STR down {$no}%<br />\n");
+		print ($this->char->Name('bold') . " STR down {$no}%<br />\n");
 	}
 	function DownINT($no)
 	{
 		$this->char->INT = round($this->char->INT * (1 - $no / 100));
-		print ($this->char->Name(bold) . " INT down {$no}%<br />\n");
+		print ($this->char->Name('bold') . " INT down {$no}%<br />\n");
 	}
 	function DownDEX($no)
 	{
 		$this->char->DEX = round($this->char->DEX * (1 - $no / 100));
-		print ($this->char->Name(bold) . " DEX down {$no}%<br />\n");
+		print ($this->char->Name('bold') . " DEX down {$no}%<br />\n");
 	}
 	function DownSPD($no)
 	{
 		$this->char->SPD = round($this->char->SPD * (1 - $no / 100));
-		print ($this->char->Name(bold) . " SPD down {$no}%<br />\n");
+		print ($this->char->Name('bold') . " SPD down {$no}%<br />\n");
 	}
 	function DownATK($no)
 	{
 		$this->char->atk["0"] = round($this->char->atk["0"] * (1 - $no / 100));
-		print ($this->char->Name(bold) . " ATK down {$no}%<br />\n");
+		print ($this->char->Name('bold') . " ATK down {$no}%<br />\n");
 	}
 	function DownMATK($no)
 	{
 		$this->char->atk["1"] = round($this->char->atk["1"] * (1 - $no / 100));
-		print ($this->char->Name(bold) . " MATK down {$no}%<br />\n");
+		print ($this->char->Name('bold') . " MATK down {$no}%<br />\n");
 	}
 	function DownDEF($no)
 	{
 		$this->char->def["0"] = round($this->char->def["0"] * (1 - $no / 100));
-		print ($this->char->Name(bold) . " DEF down {$no}%<br />\n");
+		print ($this->char->Name('bold') . " DEF down {$no}%<br />\n");
 	}
 	function DownMDEF($no)
 	{
 		$this->char->def["2"] = round($this->char->def["2"] * (1 - $no / 100));
-		print ($this->char->Name(bold) . " MDEF down {$no}%<br />\n");
+		print ($this->char->Name('bold') . " MDEF down {$no}%<br />\n");
 	}
 
 
 	//	毒ダメージ
 	function PoisonDamage($multiply = 1)
 	{
-		if ($this->char->STATE !== 2) return false;
+		if ($this->char->STATE !== STATE_POISON) return false;
 
 		$poison = $this->char->PoisonDamageFormula($multiply);
-		print ("<span class=\"spdmg\">" . $this->char->Name(bold) . " got ");
+		print ("<span class=\"spdmg\">" . $this->char->Name('bold') . " got ");
 		print ("<span class=\"bold\">$poison</span> damage by poison.\n");
 		$this->char->HpDamage2($poison);
 		print ("</span><br />\n");
@@ -428,14 +428,14 @@ class HOF_Class_Char_Battle_Effect
 	//	毒の状態 異常化 処理
 	function GetPoison($BePoison)
 	{
-		if ($this->char->STATE === 2) return false;
+		if ($this->char->STATE === STATE_POISON) return false;
 		if ($this->char->SPECIAL["PoisonResist"])
 		{
 			$prob = mt_rand(0, 99);
 			$BePoison *= (1 - $this->char->SPECIAL["PoisonResist"] / 100);
 			if ($prob < $BePoison)
 			{
-				$this->char->STATE = 2;
+				$this->char->STATE = STATE_POISON;
 				return true;
 			}
 			else
@@ -443,7 +443,7 @@ class HOF_Class_Char_Battle_Effect
 				return "BLOCK";
 			}
 		}
-		$this->char->STATE = 2;
+		$this->char->STATE = STATE_POISON2;
 		return true;
 	}
 
@@ -454,7 +454,7 @@ class HOF_Class_Char_Battle_Effect
 		$Add = round($Add);
 		$this->char->SPECIAL["PoisonResist"] += $Add;
 		print ('<span class="support">');
-		print ($this->char->Name(bold) . " got PoisonResist!(" . $this->char->SPECIAL["PoisonResist"] . "%)");
+		print ($this->char->Name('bold') . " got PoisonResist!(" . $this->char->SPECIAL["PoisonResist"] . "%)");
 		print ("</span><br />\n");
 	}
 
@@ -478,15 +478,15 @@ class HOF_Class_Char_Battle_Effect
 		if ($this->char->STATE === STATE_DEAD)
 		{
 			//死亡状態
-			if ($mes) print ($this->char->Name(bold) . ' <span class="recover">revived</span>!<br />' . "\n");
-			$this->char->STATE = 0;
+			if ($mes) print ($this->char->Name('bold') . ' <span class="recover">revived</span>!<br />' . "\n");
+			$this->char->STATE = STATE_ALIVE;
 			return true;
 		}
 		if ($this->char->STATE === STATE_POISON)
 		{
 			//毒状態
-			if ($mes) print ($this->char->Name(bold) . "'s <span class=\"spdmg\">poison</span> has cured.<br />\n");
-			$this->char->STATE = 0;
+			if ($mes) print ($this->char->Name('bold') . "'s <span class=\"spdmg\">poison</span> has cured.<br />\n");
+			$this->char->STATE = STATE_ALIVE;
 			return true;
 		}
 	}
