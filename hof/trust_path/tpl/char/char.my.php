78d062acd1311b1ed9eb102d3f577143b2f2da77
bluelovers <codelovers@users.sourceforge.net>
2012-05-20 03:45:27 +0800

    本次提交改動到系統判定(可能會有未知錯誤) (Step: 16)
    Step: 16
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/tpl/char/char.my.php

--- hof/trust_path/tpl/char/char.my.php
+++ hof/trust_path/tpl/char/char.my.php
@@ -2,7 +2,7 @@
 	<?php if ($this->output->chars): ?>
 
 	<div class="margin15">
-		<?php HOF_Class_Char_View::ShowCharacters($this->output->chars) ?>
+		<?php HOF_Class_Char_View::ShowCharacters($this->output->chars, INPUT_RADIO) ?>
 	</div>
 
 		<?php if (0): ?>
