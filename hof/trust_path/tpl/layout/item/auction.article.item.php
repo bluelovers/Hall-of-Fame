54da6fbc16f62bd345daf9642fd6f987c7d663e1
bluelovers <codelovers@users.sourceforge.net>
2012-05-19 23:49:11 +0800

    HOF_Class_View::render(null, $this->output, 'layout/item/auction.article.item')->output(); (Step: 1)
    Step: 1
    
    Commit ChangedLog:
    
    [A ] new file:
    
    	hof/trust_path/tpl/layout/item/auction.article.item.php

---
+++ hof/trust_path/tpl/layout/item/auction.article.item.php
@@ -0,0 +1,94 @@
+<?php if (empty($this->output->article_list)): ?>
+<?php HOF_Helper_Global::ShowResult('競売物無し(no auction)') ?>
+<?php else: ?>
+
+	総出品数: <?php e($this->output->article_count) ?>
+
+	<table style="width:725px;text-align:center" cellpadding="0" cellspacing="0" border="0">
+		<tr>
+			<td class="td9"><a href="<?php e($this->output->query) ?>&sort=no"<?php e($this->output->style_no) ?>>番号</a></td>
+			<td class="td9"><a href="<?php e($this->output->query) ?>&sort=no"<?php e($this->output->style_no) ?>>残り</a></td>
+			<td class="td9">
+				<a href="<?php e($this->output->query) ?>&sort=price"<?php e($this->output->style_price) ?>>価格</a>
+				&nbsp;<a href="<?php e($this->output->query) ?>&sort=rprice"<?php e($this->output->style_rprice) ?>>(昇)</a>
+			</td>
+			<td class="td9">Item</td>
+			<td class="td9"><a href="<?php e($this->output->query) ?>&sort=bid"<?php e($this->output->style_bid) ?>>入札数</a></td>
+			<td class="td9">入札者</td>
+			<td class="td9">出品者</td>
+		</tr>
+
+		<?php foreach ($this->output->article_list as $article_list): ?>
+		<tr>
+			<td class="td7" rowspan="2">
+				<!-- 競売番号 -->
+				<?php e($article_list["no"]) ?>
+			</td>
+
+			<td class="td7">
+				<!-- 終了時刻 -->
+				<?php e($article_list['end_left']) ?>
+			</td>
+
+			<td class="td7">
+				<!-- 現在入札価格 -->
+				<?php e(HOF_Helper_Global::MoneyFormat($article_list["price"])) ?>
+			</td>
+
+			<td class="td7">
+				<!-- アイテム -->
+				<?php e($article_list['item_show']) ?>
+			</td>
+
+			<td class="td7">
+				<!-- 合計入札数 -->
+				<?php e($article_list["TotalBid"]) ?>
+			</td>
+
+			<td class="td7">
+				<!-- 入札者 -->
+				<?php e($article_list['bidder_name']) ?>
+			</td>
+
+			<td class="td8">
+				<!-- 出品者 -->
+				<?php e($article_list['exhibitor_name']) ?>
+			</td>
+
+		</tr>
+		<tr>
+			<td colspan="6" style="text-align:left;" class="td8">
+
+				<!-- 入札フォーム -->
+				<?php if ($this->output->bidding): ?>
+				<form action="<?php e($this->output->query) ?>" method="post">
+					<a href="#" onclick="$('#Bid<?php e($article_list["no"]) ?>').toggle()" style="margin:0 10px">入札</a>
+
+					<span style="display:none" id="Bid<?php e($article_list["no"]) ?>"> &nbsp;
+						<input type="text" name="BidPrice" style="width:80px" class="text" value="<?php e($article_list['price_bid_min']) ?>">
+						<input type="submit" value="Bid" class="btn">
+						<input type="hidden" name="article_no" value="<?php e($article_list["no"]) ?>">
+					</span>
+				</form>
+				<?php endif; ?>
+
+				<?php e($article_list["comment"]) ?>
+			</td>
+		</tr>
+		<?php endforeach; ?>
+
+		<tr>
+			<td class="td9"><a href="<?php e($this->output->query) ?>&sort=no"<?php e($this->output->style_no) ?>>番号</a></td>
+			<td class="td9"><a href="<?php e($this->output->query) ?>&sort=no"<?php e($this->output->style_no) ?>>残り</a></td>
+			<td class="td9">
+				<a href="<?php e($this->output->query) ?>&sort=price"<?php e($this->output->style_price) ?>>価格</a>
+				&nbsp;<a href="<?php e($this->output->query) ?>&sort=rprice"<?php e($this->output->style_rprice) ?>>(昇)</a>
+			</td>
+			<td class="td9">Item</td>
+			<td class="td9"><a href="<?php e($this->output->query) ?>&sort=bid"<?php e($this->output->style_bid) ?>>入札数</a></td>
+			<td class="td9">入札者</td>
+			<td class="td9">出品者</td>
+		</tr>
+
+	</table>
+	<?php endif; ?>
