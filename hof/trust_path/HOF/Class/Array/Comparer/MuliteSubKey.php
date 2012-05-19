bb7dd33401386e52dd38110a2e3a8aca2aa0b1b4
bluelovers <codelovers@users.sourceforge.net>
2012-05-20 00:17:01 +0800

    usort($this->article_list, array(HOF_Class_Array_Comparer_MuliteSubKey::newInstance($key)->sort_desc($desc), 'compare')); (Step: 2)
    Step: 2
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/HOF/Class/Array/Comparer/MuliteSubKey.php

--- hof/trust_path/HOF/Class/Array/Comparer/MuliteSubKey.php
+++ hof/trust_path/HOF/Class/Array/Comparer/MuliteSubKey.php
@@ -9,21 +9,61 @@ class HOF_Class_Array_Comparer_MuliteSubKey
 {
 
 	var $keys;
+	var $sort_desc = false;
 
 	function __construct($keys)
 	{
-		$this->keys = func_get_args();
+		if (is_array($keys))
+		{
+			$this->keys = (array)$keys;
+		}
+		else
+		{
+			$this->keys = func_get_args();
+		}
+
+		return $this;
+	}
+
+	function newInstance($keys)
+	{
+		if (is_array($keys))
+		{
+			$keys = (array)$keys;
+		}
+		else
+		{
+			$keys = func_get_args();
+		}
+
+		return new self($keys);
+	}
+
+	function sort_desc($sort_desc = null)
+	{
+		if ($sort_desc !== null)
+		{
+			$this->sort_desc = $sort_desc;
+		}
+
+		return $this;
 	}
 
 	function compare($a, $b)
 	{
 		$i = 0;
-		$c = count($this->keys);
+		$c = count((array)$this->keys);
 
 		$cmp = 0;
 		while ($cmp == 0 && $i < $c)
 		{
 			$cmp = strcmp($a[$this->keys[$i]], $b[$this->keys[$i]]);
+
+			if ($this->sort_desc)
+			{
+				$cmp = 0 - $cmp;
+			}
+
 			$i++;
 		}
 
