d38c0492b7c9ec6031ef312efd36681fbb17ae67
bluelovers <codelovers@users.sourceforge.net>
2012-05-20 03:45:26 +0800

    本次提交改動到系統判定(可能會有未知錯誤) (Step: 15)
    Step: 15
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/tpl/battle/team.char.php

--- hof/trust_path/tpl/battle/team.char.php
+++ hof/trust_path/tpl/battle/team.char.php
@@ -5,14 +5,16 @@
 
 	<form action="<?php e($this->get('battle.target.from.action', INDEX . '?common=' . $this->get('battle.target.id'))) ?>" method="post">
 
-		<?php e(HOF_Class_Char_View::ShowCharacters(HOF::user()->char, "CHECKBOX", HOF::user()->party_memo)); ?>
+		<?php e(HOF_Class_Char_View::ShowCharacters(HOF::user()->char, INPUT_CHECKBOX, HOF::user()->party_memo)); ?>
 
 		<div style="margin:15px;text-align:center;overflow: visible;">
-			<input type="submit" class="btn" name="monster_battle" value="Battle !">
-			<input type="reset" class="btn" value="Reset">
-			<br>
+			<input type="submit" class="btn" name="monster_battle" value="Battle !"/>
+			<input type="reset" class="btn" value="Reset"/>
+			<br/>
+			<label>
 			Save this party:
-			<input type="checkbox" name="memory_party" value="1">
+			<input type="checkbox" name="memory_party" value="1"/>
+			</label>
 		</div>
 
 	</form>
