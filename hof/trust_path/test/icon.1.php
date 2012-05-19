c9767467c1282ebde9a278b3c01032ba40d4e788
bluelovers <codelovers@users.sourceforge.net>
2012-05-19 21:10:25 +0800

    png bg alpha (Step: 27)
    Step: 27
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/test/icon.1.php

--- hof/trust_path/test/icon.1.php
+++ hof/trust_path/test/icon.1.php
@@ -12,4 +12,6 @@ require_once ("./bootstrap.php");
 $v[] = HOF_Class_Icon::getImage('bg_swamp2', HOF_Class_Icon::IMG_LAND);
 $v[] = HOF_Class_Icon::getImage('land_swamp2', HOF_Class_Icon::IMG_LAND);
 
+$v[] = HOF_Class_Icon::getImage('we_sword026', HOF_Class_Icon::IMG_ITEM);
+
 debug($v);
