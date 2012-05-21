f7d162978c9e26e1eb06e353fa394ab0ecdd6b68
bluelovers <codelovers@users.sourceforge.net>
2012-05-21 08:59:55 +0800

    . (Step: 4)
    Step: 4
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/tpl/layout/layout.body.php

--- hof/trust_path/tpl/layout/layout.body.php
+++ hof/trust_path/tpl/layout/layout.body.php
@@ -3,7 +3,7 @@
 
 	<div id="main_frame">
 		<div id="title">
-			<img src="<?php e(HOF_Class_Icon::getImageUrl('title03', './static/image/')); ?>">
+			<img src="<?php e(HOF_Class_Icon::getImageUrl('title03', HOF_Class_Icon::IMG_IMAGE)); ?>">
 		</div>
 
 		<?php HOF_Model_Main::getInstance()->MyMenu(); ?>
