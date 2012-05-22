3c48293b4cbf40ed1a782dd3150b77d38966bbbf
bluelovers <codelovers@users.sourceforge.net>
2012-05-22 09:08:38 +0800

    change router call (Step: 14)
    Step: 14
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/HOF/Class/Battle/Style/Image.php

--- hof/trust_path/HOF/Class/Battle/Style/Image.php
+++ hof/trust_path/HOF/Class/Battle/Style/Image.php
@@ -5,6 +5,19 @@
  * @copyright 2012
  */
 
+/**
+ * 画像合成を非常にナンセンスな方法で行う。
+ * GDライブラリ→画像の水平反転が不可能。
+ * PECL ImageMagic→可能。しかしPEARの知識が無く断念。
+ *
+ * 従って画像合成する場合,反転済みの画像を別で用意する。
+ *
+ * sampleURL
+ * http://localhost/proj/hof/image.php?f11=mon_018&f12=mon_018&f13=mon_018&f14=mon_018&b11=mon_018&b12=mon_018&f21=mon_018&f22=mon_018&b21=mon_018&b22=mon_018&b23=mon_018&f23=mon_018&f24=mon_018&info=0
+ * 最後の[&info=0] は無くてもok
+ *
+ * ※※※ 魔法陣の表示に未対応！！！！！！！！！
+ */
 class HOF_Class_Battle_Style_Image
 {
 	var $image;
