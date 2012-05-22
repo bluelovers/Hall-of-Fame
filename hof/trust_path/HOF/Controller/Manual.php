2646b54b2bdbd927da35c945677c82f864b60bf2
bluelovers <codelovers@users.sourceforge.net>
2012-05-23 03:49:48 +0800

    fix bug (Step: 5)
    Step: 5
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/HOF/Controller/Manual.php

--- hof/trust_path/HOF/Controller/Manual.php
+++ hof/trust_path/HOF/Controller/Manual.php
@@ -8,4 +8,12 @@
 class HOF_Controller_Manual extends HOF_Class_Controller
 {
 
+	function _main_init()
+	{
+		if (empty($this->action) || $this->action == self::DEFAULT_ACTION)
+		{
+			$this->_main_setup('manual');
+		}
+	}
+
 }
