94c2c66b410b6ef49b22ccc6a3d77787f100c7ef
bluelovers <codelovers@users.sourceforge.net>
2012-05-23 02:20:24 +0800

    log fix to new router system (Step: 14)
    Step: 14
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/tpl/log/header.php

--- hof/trust_path/tpl/log/header.php
+++ hof/trust_path/tpl/log/header.php
@@ -1,10 +1,10 @@
 
 	<div style="margin:15px">
 
-		<a href="?log" <?php if ($this->output->idx == 'log') e(' class="a0"'); ?>>All</a>
-		<a href="?clog" <?php if ($this->output->idx == 'clog') e(' class="a0"'); ?>>Common</a>
-		<a href="?ulog" <?php if ($this->output->idx == 'ulog') e(' class="a0"'); ?>>Union</a>
-		<a href="?rlog" <?php if ($this->output->idx == 'rlog') e(' class="a0"'); ?>>Ranking</a>
+		<a href="<?php e(HOF::url($this->controller())) ?>" <?php if ($this->output->idx == 'log') e(' class="a0"'); ?>>All</a>
+		<a href="<?php e(HOF::url($this->controller(), null, 'log=clog')) ?>" <?php if ($this->output->idx == 'clog') e(' class="a0"'); ?>>Common</a>
+		<a href="<?php e(HOF::url($this->controller(), null, 'log=ulog')) ?>" <?php if ($this->output->idx == 'ulog') e(' class="a0"'); ?>>Union</a>
+		<a href="<?php e(HOF::url($this->controller(), null, 'log=rlog')) ?>" <?php if ($this->output->idx == 'rlog') e(' class="a0"'); ?>>Ranking</a>
 
 	</div>
 
