292b91e934ab2f912baa5dac7f87c26213edd882
bluelovers <codelovers@users.sourceforge.net>
2012-05-23 03:02:43 +0800

    set base href (Step: 36)
    Step: 36
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/tpl/shop/header.php

--- hof/trust_path/tpl/shop/header.php
+++ hof/trust_path/tpl/shop/header.php
@@ -5,8 +5,8 @@
 	<?php $this->set('npc.talk.no', $this->get('npc_no', 'ori_002')) ?>
 	<?php ob_start(); ?>
 				いらっしゃいませー<br />
-				<a href="?menu=buy">買う</a>/<a href="?menu=sell">売る</a><br />
-				<a href="?menu=work">アルバイト</a>
+				<a href="<?php e(BASE_URL) ?>?menu=buy">買う</a>/<a href="?menu=sell">売る</a><br />
+				<a href="<?php e(BASE_URL) ?>?menu=work">アルバイト</a>
 	<?php $content = ob_get_clean(); ?>
 
 	<?php e($this->slot('layout/npc.talk.1', $content)) ?>
