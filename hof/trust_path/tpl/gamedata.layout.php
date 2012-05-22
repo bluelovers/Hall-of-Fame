c719e2a680a1326243861a3bbd52c17b713e410f
bluelovers <codelovers@users.sourceforge.net>
2012-05-23 03:02:31 +0800

    set base href (Step: 24)
    Step: 24
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/tpl/gamedata.layout.php

--- hof/trust_path/tpl/gamedata.layout.php
+++ hof/trust_path/tpl/gamedata.layout.php
@@ -2,10 +2,10 @@
 	<div style="margin:15px">
 		<h4>GameData</h4>
 		<div style="margin:0 20px">
-			| <a href="?gamedata=job">職(Job)</a> |
-			<a href="?gamedata=item">アイテム(item)</a> |
-			<a href="?gamedata=judge">判定</a> |
-			<a href="?gamedata=monster">モンスター</a> |
+			| <a href="<?php e(BASE_URL) ?>?gamedata=job">職(Job)</a> |
+			<a href="<?php e(BASE_URL) ?>?gamedata=item">アイテム(item)</a> |
+			<a href="<?php e(BASE_URL) ?>?gamedata=judge">判定</a> |
+			<a href="<?php e(BASE_URL) ?>?gamedata=monster">モンスター</a> |
 		</div>
 	</div>
 
