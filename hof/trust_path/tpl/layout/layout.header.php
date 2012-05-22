a59c6a055cdab15ca82175d3c6276ee3d9effb66
bluelovers <codelovers@users.sourceforge.net>
2012-05-22 12:02:19 +0800

    . (Step: 10)
    Step: 10
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/tpl/layout/layout.header.php

--- hof/trust_path/tpl/layout/layout.header.php
+++ hof/trust_path/tpl/layout/layout.header.php
@@ -3,11 +3,13 @@
 
 	<title><?= TITLE ?></title>
 
-			<link rel="stylesheet" href="./static/style/basis.css" type="text/css">
-			<link rel="stylesheet" href="./static/style/style.css" type="text/css">
-			<script type="text/javascript" src="http://code.jquery.com/jquery-latest.pack.js"></script>
-			<script type="text/javascript" src="./static/js/jquery-core.js"></script>
-			<style>
+	<base href="<?php e(BASE_URL) ?>">
+
+	<link rel="stylesheet" href="<?php e(BASE_URL) ?>static/style/basis.css" type="text/css">
+	<link rel="stylesheet" href="<?php e(BASE_URL) ?>static/style/style.css" type="text/css">
+	<script type="text/javascript" src="http://code.jquery.com/jquery-latest.pack.js"></script>
+	<script type="text/javascript" src="<?php e(BASE_URL) ?>static/js/jquery-core.js"></script>
+	<style>
 
 .flip-h {
     -moz-transform: scaleX(-1);
