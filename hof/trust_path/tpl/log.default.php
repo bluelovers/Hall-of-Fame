f0c8b3d7abb13a9976dfad352c6b1f795f6921da
bluelovers <codelovers@users.sourceforge.net>
2012-05-23 02:20:21 +0800

    log fix to new router system (Step: 11)
    Step: 11
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/tpl/log.default.php

--- hof/trust_path/tpl/log.default.php
+++ hof/trust_path/tpl/log.default.php
@@ -5,56 +5,68 @@
 
 		<!-- common -->
 
-		<?php foreach($this->output->logs as $_k => $logs): ?>
+		<?php if (empty($this->output->logs)): ?>
+			log doesnt exists
+		<?php else: ?>
 
-			<h4>
+			<?php foreach($this->output->logs as $_k => $logs): ?>
 
-				<?php if ($_k == LOG_BATTLE_UNION): ?>
-					ユニオン戦 - (Union Battle Log)
-					<?php $_u = 'ulog'; ?>
-				<?php elseif ($_k == LOG_BATTLE_RANK): ?>
-					ランキング戦 - (Rank Battle Log)
-					<?php $_u = 'rlog'; ?>
+				<h4>
+
+					<?php if ($_k == LOG_BATTLE_UNION): ?>
+						ユニオン戦 - (Union Battle Log)
+						<?php $_u = 'ulog'; ?>
+					<?php elseif ($_k == LOG_BATTLE_RANK): ?>
+						ランキング戦 - (Rank Battle Log)
+						<?php $_u = 'rlog'; ?>
+					<?php else: ?>
+						最近の戦闘 - (Recent Battles)
+						<?php $_u = 'clog'; ?>
+					<?php endif; ?>
+
+					<?php if (!$this->output->full_log): ?>
+						<a href="<?php e(HOF::url($this->controller(), null, array('log' => $_u))) ?>">全表示</a>
+					<?php endif; ?>
+				</h4>
+
+				<?php if (empty($logs)): ?>
+					log doesnt exists
 				<?php else: ?>
-					最近の戦闘 - (Recent Battles)
-					<?php $_u = 'clog'; ?>
+
+					<?php foreach($logs as $log): ?>
+
+						[ <a href="<?php e(HOF::url($this->controller(), 'log', array('log' => $_u, 'no' => $log['time']))) ?>"><?php e($log['date']) ?></a> ]
+
+						<!-- 総ターン数 -->
+						<span class="bold"><?php e($log['act']) ?></span>turns
+
+						<?php if ($log['win'] === "0"): ?>
+							<span class="recover"><?php e($log['team'][0]) ?></span>
+						<?php elseif ($log['win'] === "1"): ?>
+							<span class="dmg"><?php e($log['team'][0]) ?></span>
+						<?php else: ?>
+							<?php e($log['team'][0]) ?>
+						<?php endif; ?>
+
+						(<?php e($log['number'][0]) ?>:<?php e($log['avelv'][0]) ?>)
+						vs
+						<?php if ($log['win'] === "0"): ?>
+							<span class="dmg"><?php e($log['team'][1]) ?></span>
+						<?php elseif ($log['win'] === "1"): ?>
+							<span class="recover"><?php e($log['team'][1]) ?></span>
+						<?php else: ?>
+							<?php e($log['team'][1]) ?>
+						<?php endif; ?>
+						(<?php e($log['number'][1]) ?>:<?php e($log['avelv'][1]) ?>)
+
+						<br />
+
+					<?php endforeach; ?>
+
 				<?php endif; ?>
-
-				<?php if (!$this->output->full_log): ?>
-					<a href="?<?php e($_u) ?>">全表示</a>
-				<?php endif; ?>
-			</h4>
-
-			<?php foreach($logs as $log): ?>
-
-				[ <a href="?<?php e($_u) ?>=<?php e($log['time']) ?>"><?php e($log['date']) ?></a> ]
-
-				<!-- 総ターン数 -->
-				<span class="bold"><?php e($log['act']) ?></span>turns
-
-				<?php if ($log['win'] === "0"): ?>
-					<span class="recover"><?php e($log['team'][0]) ?></span>
-				<?php elseif ($log['win'] === "1"): ?>
-					<span class="dmg"><?php e($log['team'][0]) ?></span>
-				<?php else: ?>
-					<?php e($log['team'][0]) ?>
-				<?php endif; ?>
-
-				(<?php e($log['number'][0]) ?>:<?php e($log['avelv'][0]) ?>)
-				vs
-				<?php if ($log['win'] === "0"): ?>
-					<span class="dmg"><?php e($log['team'][1]) ?></span>
-				<?php elseif ($log['win'] === "1"): ?>
-					<span class="recover"><?php e($log['team'][1]) ?></span>
-				<?php else: ?>
-					<?php e($log['team'][1]) ?>
-				<?php endif; ?>
-				(<?php e($log['number'][1]) ?>:<?php e($log['avelv'][1]) ?>)
-
-				<br />
 
 			<?php endforeach; ?>
 
-		<?php endforeach; ?>
+		<?php endif; ?>
 
 	</div>
