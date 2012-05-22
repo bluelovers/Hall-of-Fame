53433f023770feeaf140363663b944d608faa95a
bluelovers <codelovers@users.sourceforge.net>
2012-05-23 03:02:22 +0800

    set base href (Step: 16)
    Step: 16
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/tpl/char/char.statuspoint.php

--- hof/trust_path/tpl/char/char.statuspoint.php
+++ hof/trust_path/tpl/char/char.statuspoint.php
@@ -2,8 +2,8 @@
 	<?php if (0 < $this->output->char->statuspoint): ?>
 
 	<!-- ステータス上昇 -->
-	<form action="?char=<?php e($this->output->char_id) ?>" method="post" style="padding:0 15px">
-		<h4>Status <a href="?manual#statup" target="_blank" class="a0">?</a></h4>
+	<form action="<?php e(BASE_URL) ?>?char=<?php e($this->output->char_id) ?>" method="post" style="padding:0 15px">
+		<h4>Status <a href="<?php e(BASE_URL) ?>?manual#statup" target="_blank" class="a0">?</a></h4>
 		<?php $Stat = array(
 				"Str",
 				"Int",
@@ -24,5 +24,5 @@
 		<br />
 		<input type="submit" class="btn" name="stup" value="Increase Status">
 	</form>
-	
+
 	<?php endif; ?>
