e3afe930a222e7b5e9ce221eec42e88958b37ecb
bluelovers <codelovers@users.sourceforge.net>
2012-05-22 23:37:38 +0800

    . (Step: 4)
    Step: 4
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/tpl/auction/form.exhibit.php

--- hof/trust_path/tpl/auction/form.exhibit.php
+++ hof/trust_path/tpl/auction/form.exhibit.php
@@ -2,5 +2,3 @@
 	<form action="?menu=auction" method="post">
 		<input type="submit" value="Put Auction" name="ExhibitItemForm" class="btn" style="width:160px">
 	</form>
-
-	<a href="?menu=auction">更新</a>
