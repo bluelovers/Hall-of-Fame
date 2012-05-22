7ae52f0f2059897c50223991d685e4f1301b5cd4
bluelovers <codelovers@users.sourceforge.net>
2012-05-23 03:02:46 +0800

    set base href (Step: 39)
    Step: 39
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/tpl/town.default.php

--- hof/trust_path/tpl/town.default.php
+++ hof/trust_path/tpl/town.default.php
@@ -9,13 +9,13 @@
 					店(Shop)
 					<ul>
 						<li>
-							<a href="?menu=buy">買う(Buy)</a>
+							<a href="<?php e(BASE_URL) ?>?menu=buy">買う(Buy)</a>
 						</li>
 						<li>
-							<a href="?menu=sell">売る(Sell)</a>
+							<a href="<?php e(BASE_URL) ?>?menu=sell">売る(Sell)</a>
 						</li>
 						<li>
-							<a href="?menu=work">アルバイト</a>
+							<a href="<?php e(BASE_URL) ?>?menu=work">アルバイト</a>
 						</li>
 					</ul>
 				</li>
@@ -26,7 +26,7 @@
 
 				<li>
 					<p>
-						<a href="?recruit">人材斡旋所(Recruit)</a>
+						<a href="<?php e(BASE_URL) ?>?recruit">人材斡旋所(Recruit)</a>
 					</p>
 				</li>
 
@@ -38,10 +38,10 @@
 					鍛冶屋(Smithy)
 					<ul>
 						<li>
-							<a href="?menu=refine">精錬工房(Refine)</a>
+							<a href="<?php e(BASE_URL) ?>?menu=refine">精錬工房(Refine)</a>
 						</li>
 						<li>
-							<a href="?menu=create">製作工房(Create)</a>
+							<a href="<?php e(BASE_URL) ?>?menu=create">製作工房(Create)</a>
 						</li>
 					</ul>
 				</li>
@@ -51,7 +51,7 @@
 				<?php if ($this->output->list['Auction']): ?>
 
 				<li>
-					<a href="?menu=auction">オークション会場(Auction)
+					<a href="<?php e(BASE_URL) ?>?menu=auction">オークション会場(Auction)
 				</li>
 
 				<?php endif; ?>
@@ -59,14 +59,14 @@
 				<?php if ($this->output->list['colosseum']): ?>
 
 				<li>
-					<a href="?menu=rank">コロシアム(Colosseum)</a>
+					<a href="<?php e(BASE_URL) ?>?menu=rank">コロシアム(Colosseum)</a>
 				</li>
 
 				<?php endif; ?>
 			</ul>
 		</div>
 		<h4>広場</h4>
-		<form action="?town" method="post">
+		<form action="<?php e(BASE_URL) ?>?town" method="post">
 			<input type="text" maxlength="60" name="message" class="text" style="width:300px"/>
 			<input type="submit" value="post" class="btn" style="width:100px" />
 		</form>
