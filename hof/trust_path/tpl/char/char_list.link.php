961e18ff278f968d93c0c7acf49157e8fd340727
bluelovers <codelovers@users.sourceforge.net>
2012-05-23 03:02:23 +0800

    set base href (Step: 17)
    Step: 17
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/tpl/char/char_list.link.php

--- hof/trust_path/tpl/char/char_list.link.php
+++ hof/trust_path/tpl/char/char_list.link.php
@@ -2,6 +2,6 @@
 	<div  style="padding:15px">
 		|
 		<?php foreach ($this->output->char_list as $k => $v): ?>
-			<a href="?char=<?php e($k) ?>"><?php e($v) ?></a> |
+			<a href="<?php e(BASE_URL) ?>?char=<?php e($k) ?>"><?php e($v) ?></a> |
 		<?php endforeach; ?>
 	</div>
