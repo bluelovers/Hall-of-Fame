e1d9e4f581a9b6b7f1084e6ad3f7e4f44dd82fcd
bluelovers <codelovers@users.sourceforge.net>
2012-05-24 06:50:02 +0800

    . (Step: 11)
    Step: 11
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/tpl/char/char_list.link.php

--- hof/trust_path/tpl/char/char_list.link.php
+++ hof/trust_path/tpl/char/char_list.link.php
@@ -2,6 +2,6 @@
 	<div  style="padding:15px">
 		|
 		<?php foreach ($this->output->char_list as $k => $v): ?>
-			<a href="<?php e(HOF::url('char', null, array('char' => $k))) ?>"><?php e($v) ?></a> |
+			<a href="<?php e(HOF::url('char', 'char', array('char' => $k))) ?>"><?php e($v) ?></a> |
 		<?php endforeach; ?>
 	</div>
