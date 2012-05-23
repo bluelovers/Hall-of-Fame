e11f1d05e7751e0e2fe19c959a7d55e8d33f4ac7
bluelovers <codelovers@users.sourceforge.net>
2012-05-24 03:10:06 +0800

    . (Step: 5)
    Step: 5
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/tpl/char/char.statuspoint.php

--- hof/trust_path/tpl/char/char.statuspoint.php
+++ hof/trust_path/tpl/char/char.statuspoint.php
@@ -2,7 +2,7 @@
 	<?php if (0 < $this->output->char->statuspoint): ?>
 
 	<!-- ステータス上昇 -->
-	<form action="<?php e(HOF::url('char', null, array('char' => $this->output->char_id))) ?>" method="post" style="padding:0 15px">
+	<form action="<?php e($this->output->char_url) ?>" method="post" style="padding:0 15px">
 		<h4>Status <a href="<?php e(HOF::url('manual', 'manual', '#statup')) ?>" target="_blank" class="a0">?</a></h4>
 		<?php $Stat = array(
 				"Str",
