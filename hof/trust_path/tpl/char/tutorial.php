d7d30a33d6186f4f0c79154bd066dad0eff8967c
bluelovers <codelovers@users.sourceforge.net>
2012-05-24 02:58:49 +0800

    Char (Step: 10)
    Step: 10
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/tpl/char/tutorial.php

--- hof/trust_path/tpl/char/tutorial.php
+++ hof/trust_path/tpl/char/tutorial.php
@@ -1,3 +1,3 @@
 	<div style="margin:5px 15px">
-		<a href="<?php e(BASE_URL) ?>?tutorial">チュートリアル</a>- 戦闘の基本(登録後,1時間だけ表示されます)
+		<a href="<?php e(HOF::url('manual', 'tutorial')) ?>">チュートリアル</a>- 戦闘の基本(登録後,1時間だけ表示されます)
 	</div>
