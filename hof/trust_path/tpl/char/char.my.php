ac2b65e493a993dbc7d4888442f3aeb35a15bd81
bluelovers <codelovers@users.sourceforge.net>
2012-05-24 02:58:43 +0800

    Char (Step: 5)
    Step: 5
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/tpl/char/char.my.php

--- hof/trust_path/tpl/char/char.my.php
+++ hof/trust_path/tpl/char/char.my.php
@@ -5,20 +5,4 @@
 		<?php HOF_Class_Char_View::ShowCharacters($this->output->chars, INPUT_RADIO) ?>
 	</div>
 
-		<?php if (0): ?>
-
-	<table cellspacing="0" style="width:100%">
-		<tbody>
-			<tr>
-				<?php foreach ($this->output->chars as $val): ?>
-				<?php if ($i % CHAR_ROW == 0 && $i != 0): ?>
-			</tr>
-			<tr>
-				<?php endif; ?>
-				<td valign="bottom" style="width:<?php e($this->output->width) ?>%"><?php $val->ShowCharLink(); ?></td>
-				<?php $i++; ?>
-				<?php endforeach; ?>
-			</tr>
-		</tbody>
-	</table><?php endif; ?>
 	<?php endif; ?>
