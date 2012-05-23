6dfe43c721384a11507ca35b0d78ac4802acc3ac
bluelovers <codelovers@users.sourceforge.net>
2012-05-24 05:34:07 +0800

    . (Step: 2)
    Step: 2
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/HOF/Class/Data.php

--- hof/trust_path/HOF/Class/Data.php
+++ hof/trust_path/HOF/Class/Data.php
@@ -8,6 +8,9 @@
 class HOF_Class_Data extends HOF_Class_Array
 {
 
+	var $autocache = false;
+	var $data;
+
 	/**
 	 * $_key = strtolower($_key);
 	 * BASE_TRUST_PATH . '/HOF/Resource/'.ucfirst($_key).'/'.$_key.'.' . $no . '.yml'
@@ -28,26 +31,42 @@ class HOF_Class_Data extends HOF_Class_Array
 	{
 		$_key = strtolower($_key);
 
-		if (!isset($this->data[$_key][$no]))
+		if (!$this->autocache)
 		{
 			$data = HOF_Class_Yaml::load($this->_filename($_key, $no));
-			$this->data[$_key][$no] = $data;
 		}
+		else
+		{
+			if (!isset($this->data[$_key][$no]))
+			{
+				$data = HOF_Class_Yaml::load($this->_filename($_key, $no));
+				$this->data[$_key][$no] = $data;
+			}
 
-		$data = $this->data[$_key][$no];
+			$data = $this->data[$_key][$no];
+		}
 
 		return $data;
 	}
 
+	function _save($_key, $no, $data)
+	{
+		$_key = strtolower($_key);
+
+		return HOF_Class_Yaml::save($this->_filename($_key, $no), $data);
+	}
+
 	static function _load_list($_key, $sort = SORT_NUMERIC)
 	{
+		if ($sort === null) $sort = SORT_NUMERIC;
+
 		$list = array();
 
-		$regex = HOF_Class_Data::_filename($_key, '*');
+		$regex = self::_filename($_key, '*');
 
 		$regex = '/^' . str_replace('\*', '(.+)', preg_quote($regex, '/')) . '$/i';
 
-		foreach (glob(HOF_Class_Data::_filename($_key, '*')) as $file)
+		foreach (glob(self::_filename($_key, '*')) as $file)
 		{
 			$list[] = preg_replace($regex, '$1', $file);
 		}
