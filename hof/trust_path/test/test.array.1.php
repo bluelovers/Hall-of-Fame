66b42def1e95ab55596b1f4a0bbd158bb5520f38
bluelovers <codelovers@users.sourceforge.net>
2012-05-24 11:49:41 +0800

    ArrayObject deep (Step: 1)
    Step: 1
    
    Commit ChangedLog:
    
    [A ] new file:
    
    	hof/trust_path/test/test.array.1.php

---
+++ hof/trust_path/test/test.array.1.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @author bluelovers
+ * @copyright 2012
+ */
+
+require_once ("./bootstrap.php");
+
+$arr = array(
+	'a' => 1,
+	'b' => array(
+		1,
+		2,
+		3,
+		),
+
+	'c' => array('c2' => array('c3' => array('c4' => array(), ), ), ),
+	);
+
+$arr = new HOF_Class_Array($arr, 0, 4);
+
+debug(is_array($arr), count($arr));
+
+var_dump($arr);
+var_dump($arr['c']);
+
+$arr['d'] = array(789, array(999));
+
+var_dump($arr['d']);
