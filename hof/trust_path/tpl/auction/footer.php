06740e0e2bd74488474e328955b9f4eef7e93ded
bluelovers <codelovers@users.sourceforge.net>
2012-05-27 06:08:23 +0800

    . (Step: 5)
    Step: 5
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/tpl/auction/footer.php

--- hof/trust_path/tpl/auction/footer.php
+++ hof/trust_path/tpl/auction/footer.php
@@ -1,9 +1,3 @@
 	</div>
 
-	<a name="log"></a>
 
-	<h4>オークションログ(AuctionLog)</h4>
-
-	<div style="margin-left:20px">
-		<?php e(implode('<br/>', array_reverse($this->controller->ItemAuction->log))); ?>
-	</div>
