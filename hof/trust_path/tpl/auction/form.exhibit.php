f6b8f970d10873cec006d4edbd6e8c90d4474383
bluelovers <codelovers@users.sourceforge.net>
2012-05-26 16:55:14 +0800

    split auction (Step: 6)
    Step: 6
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/tpl/auction/form.exhibit.php

--- hof/trust_path/tpl/auction/form.exhibit.php
+++ hof/trust_path/tpl/auction/form.exhibit.php
@@ -1,4 +1,4 @@
 
-	<form action="<?php e(HOF::url('auction')) ?>" method="post">
+	<form action="<?php e(HOF::url('auction', 'exhibit')) ?>" method="post">
 		<input type="submit" value="Put Auction" name="ExhibitItemForm" class="btn" style="width:160px">
 	</form>
