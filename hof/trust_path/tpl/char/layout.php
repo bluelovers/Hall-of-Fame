c9081352f5e3d2e7aac68c1defc55b2ea8fe65d4
bluelovers <codelovers@users.sourceforge.net>
2012-05-27 17:17:29 +0800

    equip bugfix and char action split (Step: 24)
    Step: 24
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/tpl/char/layout.php

--- hof/trust_path/tpl/char/layout.php
+++ hof/trust_path/tpl/char/layout.php
@@ -1,10 +1,10 @@
 
 	<?php e($this->slot('char/header')) ?>
 
-	<?php e($this->slot('char/msg')) ?>
+
+
+	<?php e($this->slot('char/char.detail')) ?>
 
 	<?php e($this->content) ?>
-
-	<?php e($this->slot('char/job_change')) ?>
 
 	<?php e($this->slot('char/footer')) ?>
