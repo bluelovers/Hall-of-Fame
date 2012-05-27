172dcd4ba5f266fb9d2236b6c430f1fced3f51bf
bluelovers <codelovers@users.sourceforge.net>
2012-05-27 18:19:13 +0800

    . (Step: 3)
    Step: 3
    
    Commit ChangedLog:
    
    [A ] new file:
    
    	hof/trust_path/tpl/char.action.php

---
+++ hof/trust_path/tpl/char.action.php
@@ -0,0 +1,8 @@
+
+	<?php $this->extend('char/layout') ?>
+
+	<?php e($this->slot('char/char.judge')) ?>
+
+	<?php e($this->slot('char/char.position')) ?>
+
+	<?php e($this->slot('char/char.skill')) ?>
