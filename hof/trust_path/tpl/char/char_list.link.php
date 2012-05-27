8964da78483c5a3708332d051f27e498c0856ab9
bluelovers <codelovers@users.sourceforge.net>
2012-05-27 17:17:25 +0800

    equip bugfix and char action split (Step: 20)
    Step: 20
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/tpl/char/char_list.link.php

--- hof/trust_path/tpl/char/char_list.link.php
+++ hof/trust_path/tpl/char/char_list.link.php
@@ -1,7 +1,7 @@
 
-	<div  style="padding:15px">
-		|
+	<div>
+		CharList |
 		<?php foreach ($this->output->char_list as $k => $v): ?>
-			<a href="<?php e(HOF::url('char', 'char', array('char' => $k))) ?>"><?php e($v) ?></a> |
+			<a href="<?php e(HOF::url('char', $this->action, array('char' => $k))) ?>" <?php if ($k == $this->output->char_id) e(' class="a0"'); ?>><?php e($v) ?></a> |
 		<?php endforeach; ?>
 	</div>
