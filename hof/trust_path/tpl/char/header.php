1d840681f4a5056c9134d23b05ab9f05f5f29ee2
bluelovers <codelovers@users.sourceforge.net>
2012-05-27 17:17:27 +0800

    equip bugfix and char action split (Step: 22)
    Step: 22
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/tpl/char/header.php

--- hof/trust_path/tpl/char/header.php
+++ hof/trust_path/tpl/char/header.php
@@ -1,2 +1,25 @@
 
-	<?php e($this->slot('char/char_list.link')) ?>
+
+
+	<?php $this->set('npc.talk.title', 'Char') ?>
+	<?php $this->set('npc.talk.no', $this->get('npc_no', 'mon_267r')) ?>
+	<?php ob_start(); ?>
+
+		<?php e($this->slot('char/char_list.link')) ?>
+
+		<?php e($this->slot('char/msg')) ?>
+
+		<div>
+			ActionList /
+		<?php foreach(array('char', 'stup', 'action', 'equip', 'skill_learn', 'job_change') as $k): ?>
+
+			<a href="<?php e(HOF::url('char', $k, array('char' => $this->output->char_id))) ?>" <?php if ($k == $this->action) e(' class="a0"'); ?>><?php e(HOF::putintoClassParts($k)) ?></a> /
+
+		<?php endforeach; ?>
+		</div>
+
+	<?php $content = ob_get_clean(); ?>
+
+	<?php e($this->slot('layout/npc.talk.1', $content)) ?>
+
+	<div class="margin15">
