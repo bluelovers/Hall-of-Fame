eb1bbc8104f9f1c6882f9ab3949c6d618956cff2
bluelovers <codelovers@users.sourceforge.net>
2012-05-28 14:03:47 +0800

    . (Step: 34)
    Step: 34
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/tpl/char.equip.php

--- hof/trust_path/tpl/char.equip.php
+++ hof/trust_path/tpl/char.equip.php
@@ -1,3 +1,7 @@
 	<?php $this->extend('char/layout') ?>
 
+	<h4>Equipment<a href="<?php e(HOF::url('manual', 'manual', '#equip')) ?>" target="_blank" class="a0">?</a></h4>
+
+	<?php e($this->slot('char/char.equip')) ?>
+
 	<?php e($this->output->content) ?>
