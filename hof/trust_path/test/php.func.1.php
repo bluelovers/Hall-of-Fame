b8d9708096f22c473b44b0356ade155842af9253
bluelovers <codelovers@users.sourceforge.net>
2012-05-30 03:55:19 +0800

    $char->isSummon() (Step: 2)
    Step: 2
    
    Commit ChangedLog:
    
    [A ] new file:
    
    	hof/trust_path/test/php.func.1.php

---
+++ hof/trust_path/test/php.func.1.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @author bluelovers
+ * @copyright 2012
+ */
+
+require_once ("./bootstrap.php");
+
+$auth = "24\tLewis Carroll";
+$n = sscanf($auth, "%d\t%s %s", $id, $first, $last);
+$m = sscanf($auth, "%d\t%s %s");
+
+debug($n, $id, $first, $last);
+debug($m);
