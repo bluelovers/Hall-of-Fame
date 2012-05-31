586aa976ec98d4c3668f58988e3f4676b07b0983
bluelovers <codelovers@users.sourceforge.net>
2012-06-01 07:01:11 +0800

    . (Step: 7)
    Step: 7
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	hof/trust_path/HOF/Class/Battle/Team2.php

--- hof/trust_path/HOF/Class/Battle/Team2.php
+++ hof/trust_path/HOF/Class/Battle/Team2.php
@@ -11,28 +11,45 @@ class HOF_Class_Battle_Team2 extends HOF_Class_Array_Prop
 	public $team_name;
 	public $team_idx;
 
-	static $cache;
+	protected static $cache;
 
-	static $team_count = 0;
-	static $char_list = array();
+	protected static $team_count = 0;
+	protected static $char_list = array();
 
 	protected $temp;
+	protected $data;
 
-	public function __construct($chars = array())
+	public function __construct($chars = array(), $team_idx = null, $team_name = null)
 	{
-		$this->team_idx(self::$team_count++);
+		//$this->team_idx(self::$team_count++);
 
 		$this->option('prop', false);
 		//$this->setIteratorClass('HOF_Class_Battle_TeamIterator');
 
+		$this->team_idx($team_idx);
+		$this->team_name($team_name);
+
+		//$this->data = new HOF_Class_Array_Prop();
+
 		parent::__construct($chars);
+
+		$this->update();
 	}
 
 	/**
 	 * @return self
 	 */
-	public static function &newInstance($team = array())
+	public static function &newInstance($team = array(), $team_idx = null, $team_name = null)
 	{
+		if ($team instanceof self)
+		{
+
+			$team->team_idx($team_idx);
+			$team->team_name($team_name);
+
+			return $team;
+		}
+
 		return new self($team);
 	}
 
@@ -43,7 +60,8 @@ class HOF_Class_Battle_Team2 extends HOF_Class_Array_Prop
 
 	public function __clone()
 	{
-		$this->team_idx(self::$team_count++);
+		//$this->team_idx(self::$team_count++);
+		unset($this->team_idx);
 
 		foreach ($this as $k => $v)
 		{
@@ -67,11 +85,11 @@ class HOF_Class_Battle_Team2 extends HOF_Class_Array_Prop
 	/*
 	function exchangeArray($input)
 	{
-	$array = parent::exchangeArray($input);
+		$array = parent::exchangeArray($input);
 
-	$this->update();
+		$this->update();
 
-	return $array;
+		return $array;
 	}
 	*/
 
@@ -103,11 +121,21 @@ class HOF_Class_Battle_Team2 extends HOF_Class_Array_Prop
 		return $this->{__FUNCTION__ };
 	}
 
+	public function data($key, $val = null)
+	{
+		if ($val !== null)
+		{
+			$this->{__FUNCTION__ }[$key] = $val;
+		}
+
+		return $this->{__FUNCTION__ }[$key];
+	}
+
 	public function offsetSet($k, $char)
 	{
 		if (!$char instanceof HOF_Class_Char_Abstract)
 		{
-			throw new Exception('%s not a vaild Char', (string )$char);
+			throw new Exception(sprintf('%s not a vaild Char', (string )$char));
 		}
 
 		$char->team($this);
@@ -135,12 +163,16 @@ class HOF_Class_Battle_Team2 extends HOF_Class_Array_Prop
 		self::$cache['fixCharName'] = false;
 	}
 
-	public function pushNameList($cls = false)
+	public function pushNameList($nochk = false, $cls = false)
 	{
 		if ($cls || !self::$cache['ord'])
 		{
 			self::clsNameList();
+
+			$nochk = true;
 		}
+
+		if (!$nochk && $this->temp['pushNameList']) return true;
 
 		foreach ($this as $char)
 		{
@@ -175,7 +207,7 @@ class HOF_Class_Battle_Team2 extends HOF_Class_Array_Prop
 
 		$name = $entry->Name();
 
-		if ((bool)$over || (int)self::$cache['name_list'][$name] > 1 || (int)self::$cache['overlap'][$name])
+		if ((!$entry->isUnion() || (int)self::$cache['overlap'][$name]) && ((bool)$over || (int)self::$cache['name_list'][$name] > 1 || (int)self::$cache['overlap'][$name]))
 		{
 			$letter = chr(self::$cache['ord'] + (int)self::$cache['overlap'][$name]);
 
@@ -251,6 +283,64 @@ class HOF_Class_Battle_Team2 extends HOF_Class_Array_Prop
 	function _callback_CountTrueChars($char)
 	{
 		return (bool)!($char->isSummon());
+	}
+
+	public function pickList($amount = null, $pick_list = null)
+	{
+		if ($amount === null) $amount = (int)$this->data('amount');
+		if ($pick_list === null) $pick_list = $this->data('pick_list');
+
+		$list = array();
+
+		if ($amount > 0 && !empty($pick_list))
+		{
+			for ($i = 0; $i < $amount; $i++)
+			{
+				$list[] = $this->pick((array )$pick_list);
+			}
+		}
+
+		return $list;
+	}
+
+	/**
+	 * 出現する確率から敵を選んで返す
+	 */
+	public function pick($pick_list = null)
+	{
+		if ($pick_list === null) $pick_list = $this->data('pick_list');
+
+		/**
+		 * 確率の合計
+		 */
+		foreach ($pick_list as $val) $max += $val[0];
+
+		HOF_Helper_Math::rand_seed();
+
+		/**
+		 * 0～合計 の中で乱数を取る
+		 */
+		$pos = mt_rand(0, $max);
+
+		$list = HOF_Helper_Array::array_shuffle($pick_list);
+
+		foreach ($list as $no => $val)
+		{
+			/**
+			 * その時点での確率の合計
+			 */
+			$upp += $val[0];
+
+			/**
+			 * 合計より低ければ　敵が決定される
+			 */
+			if ($pos <= $upp)
+			{
+				return $no;
+			}
+		}
+
+		return array_rand($list);
 	}
 
 }
