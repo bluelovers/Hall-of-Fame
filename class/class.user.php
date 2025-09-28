e70cab712701b379b601f48efef4d5bc913725df
bluelovers <codelovers@users.sourceforge.net>
2025-09-29 00:26:35 +0800

    doc: docblock

--- class/class.user.php
+++ class/class.user.php
@@ -1,4 +1,9 @@
-<?php 
+<?php
+
+/**
+ * ユーザークラス
+ * User class handling user data and related operations.
+ */
 class user {
 
 	// ファイルポインタ
@@ -6,111 +11,140 @@ class user {
 	var $file;
 
 	var $id, $pass;
-	var $name, $last, $login ,$start;
+	var $name, $last, $login, $start;
 	var $money;
 	var $char;
 	var $time;
-	var $wtime;//總消費時間
-	var $ip;//IPアドレス
+	var $wtime; // 總消費時間
+	var $ip; // IPアドレス
 
 	var $party_memo;
-	var $party_rank;//ランキング用のパ一ティ
-	var $rank_set_time;//ランキングPT設定した時間
-	var $rank_btl_time;//次のランク戰に挑戰できる時間
+	var $party_rank; // ランキング用のパ一ティ
+	var $rank_set_time; // ランキングPT設定した時間
+	var $rank_btl_time; // 次のランク戦に挑戦できる時間
 	// ランキングの成績
 	// = "總戰鬥回數<>勝利數<>敗北數<>引き分け<>首位防衛";
 	var $rank_record;
-	var $union_btl_time;//次のUnion戰に挑戰できる時間
+	var $union_btl_time; // 次のUnion戦に挑戦できる時間
 
 	// OPTION
 	var $record_btl_log;
 	var $no_JS_itemlist;
 	var $UserColor;
 
-	// ユ一ザ一アイテム用の變數
+	// ユ一ザ一アイテム用の変数
 	var $fp_item;
 	var $item;
 
-//////////////////////////////////////////////////
-//	對像のIDのユ一ザ一クラスを作成
-	function user($id,$noExit=false) {
-		if($id)
-		{
-			$this->id	= $id;
-			if($data = $this->LoadData($noExit)) {
-				$this->DataUpDate($data);//timeとか增やす
+	/**
+	 * ユーザークラスコンストラクタ
+	 * User class constructor to initialize user data.
+	 *
+	 * @param int $id User ID
+	 * @param bool $noExit Prevent file exit on error
+	 */
+	function user($id, $noExit = false) {
+		if ($id) {
+			$this->id = $id;
+			if ($data = $this->LoadData($noExit)) {
+				$this->DataUpDate($data); // timeとか増やす
 				$this->SetData($data);
 			}
 		}
 	}
-//////////////////////////////////////////////////
-//	IPを變更
+
+	/**
+	 * IPを変更
+	 * Set user IP address.
+	 *
+	 * @param string $ip New IP address
+	 */
 	function SetIp($ip) {
 		$this->ip = $ip;
 	}
-//////////////////////////////////////////////////
-//	ユ一ザデ一タを讀む
-	function LoadData($noExit=false) {
-		$file	= USER.$this->id."/".DATA;
-		if(file_exists($file))
-		{
-			$this->file	= $file;
-			$this->fp	= FileLock($file,$noExit);
-			if(!$this->fp)
+
+	/**
+	 * ユーザーデータを読み込む
+	 * Load user data from file.
+	 *
+	 * @param bool $noExit Prevent file exit on error
+	 * @return mixed User data or false if file does not exist
+	 */
+	function LoadData($noExit = false) {
+		$file = USER . $this->id . "/" . DATA;
+		if (file_exists($file)) {
+			$this->file = $file;
+			$this->fp = FileLock($file, $noExit);
+			if (!$this->fp)
 				return false;
-			$data	= ParseFileFP($this->fp);
-			//$data	= ParseFile($file);// (2007/7/30 追加)
+			$data = ParseFileFP($this->fp);
+			// $data = ParseFile($file); // (2007/7/30 追加)
 			/*
-			$Array	= array("party_memo","party_rank");
-			foreach($Array as $val)
-			{
-				if(!$data["$val"]) continue;
-				$data["$val"]	= explode("<>",$data["$val"]);
+			$Array = array("party_memo", "party_rank");
+			foreach ($Array as $val) {
+				if (!$data["$val"]) continue;
+				$data["$val"] = explode("<>", $data["$val"]);
 			}
 			*/
 			return $data;
-		}
-			else
-		{
+		} else {
 			return false;
 		}
 	}
 
-//////////////////////////////////////////////////
-//	IDが結局のところ存在しているかたしかめる
+	/**
+	 * IDが結局のところ存在しているかどうか確認
+	 * Check if the user exists by checking the name.
+	 *
+	 * @return bool True if user exists, false otherwise
+	 */
 	function is_exist() {
-		if($this->name)
+		if ($this->name)
 			return true;
 		else
 			return false;
 	}
-//////////////////////////////////////////////////
-//	名前を返す
-	function Name($opt=false) {
-		if($this->name) {
-			if($opt)
-				return '<span class="'.$opt.'">'.$this->name.'</span>';
+
+	/**
+	 * 名前を返す
+	 * Return user's name with optional HTML class.
+	 *
+	 * @param mixed $opt Optional HTML class for the name
+	 * @return mixed User's name or false if not set
+	 */
+	function Name($opt = false) {
+		if ($this->name) {
+			if ($opt)
+				return '<span class="' . $opt . '">' . $this->name . '</span>';
 			else
 				return $this->name;
 		} else {
 			return false;
 		}
 	}
-//////////////////////////////////////////////////
-//	名前を變える
+
+	/**
+	 * 名前を変更
+	 * Change user's name.
+	 *
+	 * @param string $name New name for the user
+	 * @return bool True if name changed, false otherwise
+	 */
 	function ChangeName($name) {
-
-		if($this->name == $name)
+		if ($this->name == $name)
 			return false;
-
-		$this->name	= $name;
+		$this->name = $name;
 		return true;
 	}
-//////////////////////////////////////////////////
-//	Union戰鬥した時間をセット
+
+	/**
+	 * Union戦闘した時間をセット
+	 * Set the time for Union battle.
+	 */
 	function UnionSetTime() {
-		$this->union_btl_time	= time();
+		$this->union_btl_time = time();
 	}
+
 //////////////////////////////////////////////////
 //	UnionBattleができるかどうか確認する。
 	function CanUnionBattle() {
@@ -213,20 +247,24 @@ class user {
 		$this->rank_btl_time	= $time;
 	}
 
-//////////////////////////////////////////////////
-//	ランキング挑戰できるか？(無理なら殘り時間を返す)
+	/**
+	 * UnionBattleができるかどうか確認する。
+	 * Check if user can perform a Union battle.
+	 *
+	 * @return mixed True if able to battle, remaining time otherwise
+	 */
 	function CanRankBattle() {
-		$now	= time();
-		if($this->rank_btl_time <= $now) {
+		$now = time();
+		if ($this->rank_btl_time <= $now) {
 			return true;
-		} else if(!$this->rank_btl_time) {
+		} else if (!$this->rank_btl_time) {
 			return true;
 		} else {
-			$left	= $this->rank_btl_time - $now;
-			$hour		= floor($left/3600);
-			$minutes	= floor(($left%3600)/60);
-			$seconds	= floor(($left%3600)%60);
-			return array($hour,$minutes,$seconds);
+			$left = $this->rank_btl_time - $now;
+			$hour = floor($left / 3600);
+			$minutes = floor(($left % 3600) / 60);
+			$seconds = floor(($left % 3600) % 60);
+			return array($hour, $minutes, $seconds);
 		}
 	}
 
@@ -247,192 +285,231 @@ class user {
 		}
 	}
 
-//////////////////////////////////////////////////
-//	時間を消費する(總消費時間の加算)
+	/**
+	 * 時間を消費する(總消費時間の加算)
+	 * Waste time from user's available time.
+	 *
+	 * @param int $time Time to be wasted
+	 * @return bool True if successful, false otherwise
+	 */
 	function WasteTime($time) {
-		if($this->time < $time)
+		if ($this->time < $time)
 			return false;
-		$this->time		-= $time;
-		$this->wtime 	+= $time;
+		$this->time -= $time;
+		$this->wtime += $time;
 		return true;
 	}
-//////////////////////////////////////////////////
-//	キャラクタ一を所持してる數をかぞえる。
+
+	/**
+	 * キャラクターを所持している数をカウントする。
+	 * Count the number of characters owned by the user.
+	 *
+	 * @return int Number of characters
+	 */
 	function CharCount() {
-		$dir	= USER.$this->id;
-		$no		= 0;
-		foreach(glob("$dir/*") as $adr) {
-			$number	= basename($adr,".dat");
-			if(is_numeric($number)) {//キャラデ一タファイル
+		$dir = USER . $this->id;
+		$no = 0;
+		foreach (glob("$dir/*") as $adr) {
+			$number = basename($adr, ".dat");
+			if (is_numeric($number)) { // キャラクターデータファイル
 				$no++;
 			}
 		}
 		return $no;
 	}
-//////////////////////////////////////////////////
-//	全所持キャラクタ一をファイルから讀んで $this->char に格納
+
+	/**
+	 * 全所持キャラクターをファイルから読み込んで $this->char に格納。
+	 * Load all owned characters from files into $this->char.
+	 */
 	function CharDataLoadAll() {
-		$dir	= USER.$this->id;
-		$this->char	= array();//配列の初期化だけしておく
-		foreach(glob("$dir/*") as $adr) {
-			//print("substr:".substr($adr,-20,16)."<br>");//確認用
-			//$number	= substr($adr,-20,16);//↓1行と同じ結果
-			$number	= basename($adr,".dat");
-			if(is_numeric($number)) {//キャラデ一タファイル
-				//$chardata	= ParseFile($adr);// (2007/7/30 $adr -> $fp)
-				//$this->char[$number]	= new char($chardata);
-				$this->char[$number]	= new char($adr);
-				$this->char[$number]->SetUser($this->id);//キャラが誰のか設定する
+		$dir = USER . $this->id;
+		$this->char = array(); // 配列の初期化
+		foreach (glob("$dir/*") as $adr) {
+			$number = basename($adr, ".dat");
+			if (is_numeric($number)) { // キャラクターデータファイル
+				$this->char[$number] = new char($adr);
+				$this->char[$number]->SetUser($this->id); // キャラクターが誰かを設定する
 			}
 		}
 	}
-//////////////////////////////////////////////////
-//	指定の所持キャラクタ一をファイルから讀んで $this->char に格納後 "返す"。
-	function CharDataLoad($CharNo) {
-		// 既に讀んでる場合。
-		if($this->char[$CharNo])
-			return $this->char[$CharNo];
-		// 讀んで無い場合。
-		$file	= USER.$this->id."/".$CharNo.".dat";
-		// そんなキャラいない場合。
-		if(!file_exists($file))
-			return false;
 
-		// 居る場合。
-		//$chardata	= ParseFile($file);
-		//$this->char[$CharNo]	= new char($chardata);
-		$this->char[$CharNo]	= new char($file);
-		$this->char[$CharNo]->SetUser($this->id);//キャラが誰のか設定する
+	/**
+	 * 指定の所持キャラクターをファイルから読み込んで $this->char に格納後 "返す"。
+	 * Load a specific character from file into $this->char and return it.
+	 *
+	 * @param int $CharNo Character number
+	 * @return mixed Loaded character or false if not found
+	 */
+	function CharDataLoad($CharNo) {
+		// 既に読み込んだ場合。
+		if ($this->char[$CharNo])
+			return $this->char[$CharNo];
+		// 読み込んでいない場合。
+		$file = USER . $this->id . "/" . $CharNo . ".dat";
+		// そんなキャラクターがない場合。
+		if (!file_exists($file))
+			return false;
+		// 存在する場合。
+		$this->char[$CharNo] = new char($file);
+		$this->char[$CharNo]->SetUser($this->id); // キャラクターが誰かを設定する
 		return $this->char[$CharNo];
 	}
-//////////////////////////////////////////////////
-//	アイテムを追加
-	function AddItem($no,$amount=false) {
-		if(!isset($this->item))//どうしたもんか…
+
+	/**
+	 * アイテムを追加
+	 * Add item to user's inventory.
+	 *
+	 * @param int $no Item number
+	 * @param mixed $amount Optional amount of the item to add
+	 */
+	function AddItem($no, $amount = false) {
+		if (!isset($this->item)) // どうしたもんか…
 			$this->LoadUserItem();
-		if($amount)
-			$this->item[$no]	+= $amount;
+		if ($amount)
+			$this->item[$no] += $amount;
 		else
 			$this->item[$no]++;
 	}
 
-//////////////////////////////////////////////////
-//	アイテムを削除
-	function DeleteItem($no,$amount=false) {
-		if(!isset($this->item))//どうしたもんか…
+	/**
+	 * アイテムを削除
+	 * Remove item from user's inventory.
+	 *
+	 * @param int $no Item number
+	 * @param mixed $amount Optional amount of the item to remove
+	 * @return int Amount removed
+	 */
+	function DeleteItem($no, $amount = false) {
+		if (!isset($this->item)) // どうしたもんか…
 			$this->LoadUserItem();
 
-		// 減らす數。
-		if($this->item[$no] < $amount) {
-			$amount	= $this->item[$no];
-			if(!$amount)
+		// 減らす数。
+		if ($this->item[$no] < $amount) {
+			$amount = $this->item[$no];
+			if (!$amount)
 				$amount = 0;
 		}
-		if(!is_numeric($amount))
-			$amount	= 1;
+		if (!is_numeric($amount))
+			$amount = 1;
 
 		// 減らす。
-		$this->item[$no]	-= $amount;
-		if($this->item[$no] < 1)
+		$this->item[$no] -= $amount;
+		if ($this->item[$no] < 1)
 			unset($this->item[$no]);
 
 		return $amount;
 	}
 
-//////////////////////////////////////////////////
-//	アイテムデ一タを讀む
+	/**
+	 * アイテムデータを読み込む
+	 * Load user's item data.
+	 */
 	function LoadUserItem() {
-
-		// 2重に讀むのを防止。
-		if(isset($this->item))
+		// 2重に読み込むのを防ぐ。
+		if (isset($this->item))
 			return false;
 
-		$file	= USER.$this->id."/".ITEM;
+		$file = USER . $this->id . "/" . ITEM;
 
-		if(file_exists($file)) {
-			$this->fp_item	= FileLock($file);
-			$this->item	= ParseFileFP($this->fp_item);
-			if($this->item === false)
-				$this->item	= array();
+		if (file_exists($file)) {
+			$this->fp_item = FileLock($file);
+			$this->item = ParseFileFP($this->fp_item);
+			if ($this->item === false)
+				$this->item = array();
 		} else {
-			$this->item	= array();
+			$this->item = array();
 		}
 	}
 
-//////////////////////////////////////////////////
-//	アイテムデ一タを保存する
+	/**
+	 * アイテムデータを保存する
+	 * Save user's item data.
+	 *
+	 * @return bool True if successful, false otherwise
+	 */
 	function SaveUserItem() {
-		$dir	= USER.$this->id;
-		if(!file_exists($dir))
+		$dir = USER . $this->id;
+		if (!file_exists($dir))
 			return false;
 
-		$file	= USER.$this->id."/".ITEM;
+		$file = USER . $this->id . "/" . ITEM;
 
-		if(!is_array($this->item))
+		if (!is_array($this->item))
 			return false;
 
-		// アイテムのソ一ト
-		ksort($this->item,SORT_STRING);
+		// アイテムのソート
+		ksort($this->item, SORT_STRING);
 
-		foreach($this->item as $key => $val) {
-			$text	.= "$key=$val\n";
+		foreach ($this->item as $key => $val) {
+			$text .= "$key=$val\n";
 		}
 
-		if(file_exists($file) && $this->fp_item) {
-			WriteFileFP($this->fp_item,$text,1);//$textが空でも保存する
+		if (file_exists($file) && $this->fp_item) {
+			WriteFileFP($this->fp_item, $text, 1); // $textが空でも保存する
 			fclose($this->fp_item);
 			unset($this->fp_item);
 		} else {
 			// $textが空でも保存する
-			WriteFile($file,$text,1);
+			WriteFile($file, $text, 1);
 		}
 	}
 
-//////////////////////////////////////////////////
-//	時間を經過させる。(Timeの增加)
+	/**
+	 * 時間を経過させる。(Timeの増加)
+	 * Update user's time data.
+	 *
+	 * @param array &$data Reference to user data
+	 */
 	function DataUpDate(&$data) {
-		$now	= time();
-		$diff	= $now - $data["last"];
-		$data["last"]	= $now;
-		$gain	= $diff / (24*60*60) * TIME_GAIN_DAY;
-		$data["time"]	+= $gain;
-		if(MAX_TIME < $data["time"])
-			$data["time"]	= MAX_TIME;
+		$now = time();
+		$diff = $now - $data["last"];
+		$data["last"] = $now;
+		$gain = $diff / (24 * 60 * 60) * TIME_GAIN_DAY;
+		$data["time"] += $gain;
+		if (MAX_TIME < $data["time"])
+			$data["time"] = MAX_TIME;
 	}
 
-//////////////////////////////////////////////////
-//	デ一タをセットする。
-//	※?
+	/**
+	 * データをセットする。
+	 * Set user data from an array.
+	 *
+	 * @param array &$data Reference to user data
+	 */
 	function SetData(&$data) {
-
-		foreach($data as $key => $val) {
-			$this->{$key}	= $val;
+		foreach ($data as $key => $val) {
+			$this->{$key} = $val;
 		}
-		/*
-		$this->name	= $data["name"];
-		$this->login	= $data["login"];
-		$this->last	= $data["last"];
-		$this->start	= $data["start"];
-		*/
 	}
 
-//////////////////////////////////////////////////
-//	パスワ一ドを暗號化する
+	/**
+	 * パスワードを暗号化する
+	 * Encrypt user's password.
+	 *
+	 * @param string $pass User's password
+	 * @return string Encrypted password
+	 */
 	function CryptPassword($pass) {
-		return substr(crypt($pass,CRYPT_KEY),strlen(CRYPT_KEY));
+		return substr(crypt($pass, CRYPT_KEY), strlen(CRYPT_KEY));
 	}
 
-//////////////////////////////////////////////////
-//	名前を消す
+	/**
+	 * 名前を消す
+	 * Delete user's name.
+	 */
 	function DeleteName() {
-		$this->name	= NULL;
+		$this->name = NULL;
 	}
 
-//////////////////////////////////////////////////
-//	デ一タを保存する形式に變換する。(テキスト)
+	/**
+	 * データを保存する形式に変換する。(テキスト)
+	 * Convert user data to a text format for saving.
+	 *
+	 * @return string Formatted user data
+	 */
 	function DataSavingFormat() {
-
-		$Save	= array(
+		$Save = array(
 			"id",
 			"pass",
 			"ip",
@@ -453,127 +530,107 @@ class user {
 			"record_btl_log",
 			"no_JS_itemlist",
 			"UserColor",
-			);
-		foreach($Save as $val) {
-			if($this->{$val})
-				$text	.= "$val=".(is_array($this->{$val}) ? implode("<>",$this->{$val}) : $this->{$val})."\n";
+		);
+		foreach ($Save as $val) {
+			if ($this->{$val})
+				$text .= "$val=" . (is_array($this->{$val}) ? implode("<>", $this->{$val}) : $this->{$val}) . "\n";
 		}
-		
-
-		/*
-		$Save	= get_object_vars($this);
-		unset($Save["char"]);
-		unset($Save["item"]);
-		unset($Save["islogin"]);
-		foreach($Save as $key => $val) {
-			$text	.= "$key=".(is_array($val) ? implode("<>",$val) : $val)."\n";
-		}
-		*/
-
-		//print("<pre>".print_r($AAA,1)."</pre>");
-
 		return $text;
 	}
 
-//////////////////////////////////////////////////
-//	デ一タを保存する
+	/**
+	 * データを保存する
+	 * Save user data to file.
+	 *
+	 * @return bool True if successful, false otherwise
+	 */
 	function SaveData() {
-		$dir	= USER.$this->id;
-		$file	= USER.$this->id."/".DATA;
+		$dir = USER . $this->id;
+		$file = USER . $this->id . "/" . DATA;
 
-		if(file_exists($this->file) && $this->fp) {
-			//print("BBB");
-			//ftruncate($this->fp,0);
-			//rewind($this->fp);
-			//$fp	= fopen($file,"w+");
-			//flock($fp,LOCK_EX);
-			//fputs($this->fp,$this->DataSavingFormat());
-			WriteFileFP($this->fp,$this->DataSavingFormat());
+		if (file_exists($this->file) && $this->fp) {
+			WriteFileFP($this->fp, $this->DataSavingFormat());
 			fclose($this->fp);
 			unset($this->fp);
-			//WriteFile("./user/1234/data2.dat",$this->DataSavingFormat());
-			//WriteFile($file,$this->DataSavingFormat());
-			//WriteFileFP($this->fp,$this->DataSavingFormat());
-			//fclose($this->fp);
 		} else {
-			if(file_exists($file))
-				WriteFile($file,$this->DataSavingFormat());
+			if (file_exists($file))
+				WriteFile($file, $this->DataSavingFormat());
 		}
 	}
-/////////////////////////////////////////////////
-//	デ一タファイル兼キャラファイルのファイルポインタも全部閉じる
+
+	/**
+	 * データファイル兼キャラクターファイルのファイルポインタも全部閉じる
+	 * Close all file pointers used by user data.
+	 */
 	function fpCloseAll() {
-		// 基本デ一タ
-		if(is_resource($this->fp))
-		{
+		if (is_resource($this->fp)) {
 			fclose($this->fp);
 			unset($this->fp);
 		}
-
-		// アイテムデ一タ
-		if(is_resource($this->fp_item))
-		{
+		if (is_resource($this->fp_item)) {
 			fclose($this->fp_item);
 			unset($this->fp_item);
 		}
-
-		// キャラデ一タ
-		if($this->char)
-		{
-			foreach($this->char as $key => $var)
-			{
-				if(method_exists($this->char[$key],"fpclose"))
+		if ($this->char) {
+			foreach ($this->char as $key => $var) {
+				if (method_exists($this->char[$key], "fpclose"))
 					$this->char[$key]->fpclose();
 			}
 		}
-
 	}
-//////////////////////////////////////////////////
-//	ユ一ザ一の削除(全ファイル)
-	function DeleteUser($DeleteFromRank=true) {
-		//ランキングからまず消す。
-		if($DeleteFromRank) {
+
+	/**
+	 * ユーザーの削除(全ファイル)
+	 * Delete user and all related files.
+	 *
+	 * @param bool $DeleteFromRank True to delete from ranking
+	 */
+	function DeleteUser($DeleteFromRank = true) {
+		if ($DeleteFromRank) {
 			include_once(CLASS_RANKING);
-			$Ranking	= new Ranking();
-			if( $Ranking->DeleteRank($this->id) )
+			$Ranking = new Ranking();
+			if ($Ranking->DeleteRank($this->id))
 				$Ranking->SaveRanking();
 		}
-
-		$dir	= USER.$this->id;
-		$files	= glob("$dir/*");
+		$dir = USER . $this->id;
+		$files = glob("$dir/*");
 		$this->fpCloseAll();
-		foreach($files as $val)
+		foreach ($files as $val)
 			unlink($val);
 		rmdir($dir);
 	}
-//////////////////////////////////////////////////
-//	放棄されているかどうか確かめる
+
+	/**
+	 * 放棄されているかどうか確かめる
+	 * Check if the user is abandoned.
+	 *
+	 * @return bool True if abandoned, false otherwise
+	 */
 	function IsAbandoned() {
-		$now	= time();
-		// $this->login がおかしければ終了する。
-		if(strlen($this->login) !== 10) {
+		$now = time();
+		if (strlen($this->login) !== 10)
 			return false;
-		}
-		if( ($this->login + ABANDONED) < $now) {
+		if (($this->login + ABANDONED) < $now)
 			return true;
-		} else {
+		else
 			return false;
-		}
 	}
-//////////////////////////////////////////////////
-//	キャラデ一タを消す
+
+	/**
+	 * キャラクターデータを消す
+	 * Delete a character's data.
+	 *
+	 * @param int $no Character number
+	 */
 	function DeleteChar($no) {
-		$file	= USER.$this->id."/".$no.".dat";
-		if($this->char[$no]) {
+		$file = USER . $this->id . "/" . $no . ".dat";
+		if ($this->char[$no]) {
 			$this->char[$no]->fpclose();
 		}
-		if(file_exists($file))
+		if (file_exists($file))
 			unlink($file);
 	}
 
-//////////////////////////////////////////////////
-//	
-	//function Load
-
 }
+
 ?>
