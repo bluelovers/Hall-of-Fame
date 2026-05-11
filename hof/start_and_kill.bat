a0b66270121d2443c7b80e9c0cfa60436bfe5a08
bluelovers <codelovers@users.sourceforge.net>
2026-05-11 14:08:56 +0800

    build(hof): 添加 UTF-8 代码页支持到批处理脚本

--- hof/start_and_kill.bat
+++ hof/start_and_kill.bat
@@ -1,2 +1,3 @@
 @echo off
+chcp 65001 >nul
 powershell -ExecutionPolicy Bypass -File "%~dp0start_and_kill.ps1" %*
--- hof/taskill-port.bat
+++ hof/taskill-port.bat
@@ -1,2 +1,3 @@
 @echo off
+chcp 65001 >nul
 powershell -ExecutionPolicy Bypass -File "%~dp0taskill-port.ps1" %*
